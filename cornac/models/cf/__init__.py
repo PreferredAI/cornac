fabda7cb4803b8bc361cfafb451311e0890650f2
Andrew-DungLe <ddle.2015@smu.edu.sg>
2018-10-12 18:29:04 +0800

    added ibpr

--- cornac/models/cf/__init__.py
+++ cornac/models/cf/__init__.py
@@ -2,8 +2,9 @@
 from .recom_hpf import Hpf
 from .recom_pmf import Pmf
 from .recom_bpr import Bpr
-
+from .recom_ibpr import Ibpr
 
 __all__ = ['Hpf',
 		   'Pmf',
-		   'Bpr']
+		   'Bpr',
+		   'Ibpr']
