d8666047ef284db51e602844d8cdd173356bf9ac
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2018-12-11 23:56:32 +0800

    Rename function in reader

--- cornac/data/reader.py
+++ cornac/data/reader.py
@@ -7,7 +7,7 @@
 import itertools
 
 
-def to_triplets(path_to_data_file, u_col=0, i_col=1, r_col=2, sep='\t', skip_lines=0):
+def txt_to_triplets(path_to_data_file, u_col=0, i_col=1, r_col=2, sep='\t', skip_lines=0):
     max_index = max(u_col, i_col, r_col)
     triplet_data = []
 
