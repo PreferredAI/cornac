2b6ba95925f78c65f6191aea14498dfe5f71afcb
Aghiles <salahaghiles@gmail.com>
2018-12-19 17:26:44 +0800

    Updated fit function for ibpr

--- cornac/models/online_ibpr/recom_online_ibpr.py
+++ cornac/models/online_ibpr/recom_online_ibpr.py
@@ -34,6 +34,9 @@ class OnlineIBPR(Recommender):
     trainable: boolean, optional, default: True
         When False, the model is not trained and Cornac assumes that the model already \
         pre-trained (U and V are not None).
+        
+    verbose: boolean, optional, default: False
+        When True, some running logs are displayed.
 
     init_params: dictionary, optional, default: None
         List of initial parameters, e.g., init_params = {'U':U, 'V':V} \
@@ -51,8 +54,9 @@ class OnlineIBPR(Recommender):
       In Proceedings of the 2017 ACM on Conference on Information and Knowledge Management (pp. 1389-1398). ACM.
     """
 
-    def __init__(self, k=20, max_iter=100, learning_rate = 0.05, lamda = 0.001, batch_size = 100, name="online_ibpr",trainable = True,init_params = None):
-        Recommender.__init__(self, name=name, trainable = trainable)
+    def __init__(self, k=20, max_iter=100, learning_rate = 0.05, lamda = 0.001, batch_size = 100, name="online_ibpr", trainable = True, 
+                 verbose=False, init_params = None):
+        Recommender.__init__(self, name=name, trainable=trainable, verbose=verbose)
         self.k = k
         self.init_params = init_params
         self.max_iter = max_iter
@@ -64,20 +68,37 @@ class OnlineIBPR(Recommender):
         self.U = init_params['U']  # matrix of user factors
         self.V = init_params['V']  # matrix of item factors
 
+
+
     # fit the recommender model to the traning data
-    def fit(self, triplets):
+    def fit(self, train_set):
         """Fit the model to observations.
 
         Parameters
         ----------
-        triplets: collections of user's ordinal triplets (u, i, j) indicating u prefers i to j
+        train_set: object of type TrainSet, required
+            An object contraining the user-item preference in csr scipy sparse format,\
+            as well as some useful attributes such as mappings to the original user/item ids.\
+            Please refer to the class TrainSet in the "data" module for details.
         """
+        
+        Recommender.fit(self, train_set)
 
-        print('Learning...')
+        X = self.train_set.matrix        
+        #change the data to original user Id item Id and rating format
+        X = X.tocoo() # convert sparse matrix to COOrdiante format
+        triplets = np.ndarray(shape=(len(X.data), 3), dtype=float)
+        triplets[:, 0] = X.row
+        triplets[:, 1] = X.col
+        triplets[:, 2] = X.data
+        
+        if self.verbose:
+            print('Learning...')
         res = online_ibpr(triplets, k=self.k, n_epochs=self.max_iter,lamda = self.lamda, learning_rate= self.learning_rate, batch_size = self.batch_size, init_params=self.init_params)
-        self.U = res['U']
-        self.V = res['V']
-        print('Learning completed')
+        self.U = np.asarray(res['U'])
+        self.V = np.asarray(res['V'])
+        if self.verbose:
+            print('Learning completed')
 
 
 
