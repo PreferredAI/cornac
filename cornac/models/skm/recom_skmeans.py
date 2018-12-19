20d1e53505498cdd63e02eb789cf24541f02a5b1
Aghiles <salahaghiles@gmail.com>
2018-12-19 17:59:39 +0800

    Change Skmeans' fit function

--- cornac/models/skm/recom_skmeans.py
+++ cornac/models/skm/recom_skmeans.py
@@ -3,8 +3,11 @@
 """
 
 import numpy as np
+import scipy.sparse as sp
 from ..recommender import Recommender
 from .skmeans import *
+from ...exception import ScoreException
+
 
 
 class SKMeans(Recommender):
@@ -28,8 +31,8 @@ class SKMeans(Recommender):
     tol : float, optional, default: 1e-6
         Relative tolerance with regards to skmeans' criterion to declare convergence.
         
-    verbose: boolean, optional, default: True
-        When True, the skmeans criterion (likelihood) is displayed after each iteration.
+    verbose: boolean, optional, default: False
+        When True, some running logs are displayed.
 
     init_par: numpy 1d array, optional, default: None
         The initial object parition, 1d array contaning the cluster label (int type starting from 0) \
@@ -45,29 +48,37 @@ class SKMeans(Recommender):
     """
 
     def __init__(self, k=5, max_iter=100, name="Skmeans", trainable=True, tol=1e-6, verbose=True, init_par=None):
-        Recommender.__init__(self, name=name, trainable=trainable)
+        Recommender.__init__(self, name=name, trainable=trainable, verbose=verbose)
         self.k = k
         self.par = init_par
         self.max_iter = max_iter
         self.tol = tol
         self.verbose = verbose
-
         self.centroids = None  # matrix of cluster centroids
 
+
     # fit the recommender model to the traning data
-    def fit(self, X):
+    def fit(self, train_set):
         """Fit the model to observations.
 
         Parameters
         ----------
-        X: scipy sparse matrix, required
-            the user-item preference matrix (traning data), in a scipy sparse format\
-            (e.g., csc_matrix).
+        train_set: object of type TrainSet, required
+            An object contraining the user-item preference in csr scipy sparse format,\
+            as well as some useful attributes such as mappings to the original user/item ids.\
+            Please refer to the class TrainSet in the "data" module for details.
         """
+        
+        Recommender.fit(self, train_set)
+
+        X = self.train_set.matrix  
+        X = sp.csr_matrix(X)
+        
+        # Skmeans requires rows of X to have a unit L2 norm. We therefore need to make a copy of X as we should not modify the latter.
         X1 = X.copy()
         X1 = X1.multiply(sp.csc_matrix(1. / (np.sqrt(X1.multiply(X1).sum(1).A1) + 1e-20)).T)
+        
         if self.trainable:
-            # Skmeans requires rows of X to have a unit L2 norm. We therefore need to make a copy of X as we should not modify the latter.
             res = skmeans(X1, k=self.k, max_iter=self.max_iter, tol=self.tol, verbose=self.verbose, init_par=self.par)
             self.centroids = res['centroids']
             self.par = res['partition']
