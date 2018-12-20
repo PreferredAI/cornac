d9a4c3a3116a43564eb9e723a0d2028379c1d827
Aghiles <salahaghiles@gmail.com>
2018-12-20 12:20:52 +0800

    Update evaluation strategy

--- cornac/eval_strategies/cross_validation.py
+++ cornac/eval_strategies/cross_validation.py
@@ -34,9 +34,6 @@ class CrossValidation(BaseStrategy):
         The minimum value that is considered to be a good rating used for ranking, \
         e.g, if the ratings are in {1, ..., 5}, then rating_threshold = 4.
 
-    shuffle: bool, optional, default: True
-        Shuffle the data before splitting.
-
     exclude_unknowns: bool, optional, default: False
         Ignore unknown users and items (cold-start) during evaluation and testing
 
@@ -44,11 +41,10 @@ class CrossValidation(BaseStrategy):
         Output running log
     """
 
-    def __init__(self, data, n_folds=5, rating_threshold=1., partition=None, shuffle=True,
+    def __init__(self, data, data_format='UIR', n_folds=5, rating_threshold=1., partition=None,
                  exclude_unknowns=False, verbose=False):
-        BaseStrategy.__init__(self, rating_threshold=rating_threshold, exclude_unknowns=exclude_unknowns,
-                                    verbose=verbose)
-        self._data = data
+        BaseStrategy.__init__(self, data=data, data_format = data_format, rating_threshold=rating_threshold,
+                              exclude_unknowns=exclude_unknowns, verbose=verbose)
         self.n_folds = n_folds
         self.partition = partition
         self.current_fold = 0
@@ -56,25 +52,27 @@ class CrossValidation(BaseStrategy):
         self.n_ratings = data.shape[0]
 		
 
-    # Partition users into n_folds
+    # Partition ratings into n_folds
     def _get_partition(self):
 
         n_fold_partition = np.random.choice(self.n_folds, size=self.n_ratings, replace=True,
-                                            p=None)  # sample with replacement
+                                            p=None)
         
         while len(set(n_fold_partition)) != self.n_folds:  # just in case some fold is empty
             n_fold_partition = np.random.choice(self.n_folds, size=self.n_ratings, replace=True, p=None)
 
         return n_fold_partition
 
-    # This function is used to get the next train_test data
-    def _get_next_train_test_split(self):
+
+
+    def _get_next_train_test_sets(self):
 
         test_idx = np.where(self.partition == self.current_fold)[0]
         train_idx = np.where(self.partition != self.current_fold)[0]
 		
         train_data = safe_indexing(self._data, train_idx)
         test_data = safe_indexing(self._data, test_idx)
+        val_data = np.array([])
 
         if self._data_format == 'UIR':
             self.build_from_uir_format(train_data, val_data, test_data)
@@ -93,27 +91,23 @@ class CrossValidation(BaseStrategy):
             print('Total users = {}'.format(self.total_users))
             print('Total items = {}'.format(self.total_items))
 
-    def evaluate(self, model, metrics):
+    def evaluate(self, model, metrics,user_based):
 
+        per_fold_avg_res = {}
+        per_fold_user_res = {}
         if self.partition is None:
             self.partition = self._get_partition()
         elif len(self.partition) != self.n_ratings:
-            raise Exception('the partition length must be equal to the number of ratings')
+            raise Exception('The partition length must be equal to the number of ratings')
 
         for fold in range(self.n_folds):
-            print("fold:", self.current_fold)
-            self._get_next_train_test_split()
-            if self.current_fold == 1:
-                res_tot = self.current_split.evaluate(model=model, metrics=metrics)
-                resAvg = res_tot["ResAvg"]
-                print(resAvg)
-                resPerU = res_tot["ResPerUser"]
-            else:
-                res_tot = self.current_split.evaluate(model=model, metrics=metrics)
-                """ need to figure out how to average the resuls accoording"""
-                resAvg = np.vstack((resAvg, res_tot["ResAvg"]))
-                resPerU = resPerU + res_tot["ResPerUser"]
+            self._get_next_train_test_sets()
+            avg_res, per_user_res = BaseStrategy.evaluate(self, model, metrics, user_based)
+            fold_name = 'fold:'+str(self.current_fold)
+            per_fold_avg_res[fold_name] = avg_res
+            per_fold_user_res[fold_name] = per_user_res
 
+        """
         avg_resAvg = resAvg.mean(
             0)  # we are averaging the average results across the n_folds, another possibility is to make it per-user?
         std_resAvg = resAvg.std(0, ddof=1)
@@ -124,7 +118,6 @@ class CrossValidation(BaseStrategy):
             resPerU[:, len(metrics)].todense().A1, ">", 0), :] / resPerU[which_(resPerU[:, len(metrics)].todense().A1,
                                                                                 ">", 0), len(
             metrics)].todense().reshape(n_processed_u, 1)
+        """
 
-        # This is a temporary solution, we just return a single structure containing all the results, (may be consider returning an object of class result instead)
-        res_tot = {"ResAvg": avg_resAvg[0:len(metrics)], "ResStd": std_resAvg[0:len(metrics)], "ResPerUser": resPerU}
-        return res_tot
+        return per_fold_avg_res, per_fold_user_res
