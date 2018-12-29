1f3cff7c24553e49c6a117061423ec307ac66edf
Aghiles <salahaghiles@gmail.com>
2018-12-29 20:46:48 +0800

    Change hpf to pf

--- cornac/models/hpf/cpp/cpp_hpf.h
+++ cornac/models/hpf/cpp/cpp_hpf.h
@@ -20,4 +20,4 @@ typedef std::vector<int> iVec;
 
 
 
-void hpf_cpp(Mat const&X, int const&g, Mat &G_s, Mat &G_r, Mat &L_s, Mat &L_r, dVec &K_r, dVec &T_r, int maxiter);
+void pf_cpp(Mat const&X, int const&g, Mat &G_s, Mat &G_r, Mat &L_s, Mat &L_r, dVec &K_r, dVec &T_r, int maxiter);
