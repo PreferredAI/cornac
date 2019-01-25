199ab9181f8b6387cc8748ccf8ee3e5c9df087fb
Andrew-DungLe <ddle.2015@smu.edu.sg>
2019-01-25 21:16:26 +0800

    add example to run ibpr

--- examples/ibpr_example.py
+++ examples/ibpr_example.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 
 """
-Example to run Probabilistic Matrix Factorization (PMF) model with Ratio Split evaluation strategy
 
-@author: Quoc-Tuan Truong <tuantq.vnu@gmail.com>
+@author: Le Duy Dung <ddle.2015@smu.edu.sg>
 """
 
 import cornac
 from cornac.datasets import MovieLens1M
 from cornac.eval_methods import RatioSplit
-from cornac.models import BPR
+from cornac.models import IBPR
 
-# Load the MovieLens 100K dataset
+# Load the MovieLens 1M dataset
 ml_1m = MovieLens1M.load_data()
 
 # Instantiate an evaluation method.
-ratio_split = RatioSplit(data=ml_1m, test_size=0.2, rating_threshold=1.0, exclude_unknowns=False)
+ratio_split = RatioSplit(data=ml_1m, test_size=0.2, rating_threshold=1.0, exclude_unknowns=True)
 
-# Instantiate a PMF recommender model.
-bpr = BPR(k=50, init_params={'U': None, 'V': None})
+# Instantiate a IBPR recommender model.
+ibpr = IBPR(k=10, init_params={'U': None, 'V': None})
 
 # Instantiate evaluation metrics.
 rec_20 = cornac.metrics.Recall(k=20)
@@ -26,7 +25,7 @@ pre_20 = cornac.metrics.Precision(k=20)
 
 # Instantiate and then run an experiment.
 exp = cornac.Experiment(eval_method=ratio_split,
-                        models=[bpr],
+                        models=[ibpr],
                         metrics=[rec_20, pre_20],
                         user_based=True)
 exp.run()
