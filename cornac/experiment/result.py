cfa086d960bb7e1d6f657b21a8d176a7b1da7c0c
Aghiles <salahaghiles@gmail.com>
2019-01-29 19:58:54 +0800

    Organize per fold average results

--- cornac/experiment/result.py
+++ cornac/experiment/result.py
@@ -20,12 +20,18 @@ class SingleModelResult:
         self.per_user = metric_user_results
 
     def _organize_avg_res(self, model_name, metric_names):
-        self.avg = [self.avg.get(mt_name, np.nan) for mt_name in metric_names]
-        self.avg = np.asarray(self.avg)
-        self.avg = self.avg.reshape(1, len(metric_names))
-        self.avg = pd.DataFrame(data=self.avg, index=np.asarray([model_name]), columns=np.asarray(metric_names))
+        #self.avg = [self.avg.get(mt_name, np.nan) for mt_name in metric_names]
+        #self.avg = np.asarray(self.avg)
+        #self.avg = self.avg.reshape(1, len(metric_names))
+        #self.avg = pd.DataFrame(data=self.avg, index=np.asarray([model_name]), columns=np.asarray(metric_names))
+        self.avg = self._get_data_frame(avg_res=self.avg, model_name=model_name, metric_names=metric_names)
 
-
+    def _get_data_frame(self, avg_res, model_name, metric_names):
+        avg_res = [avg_res.get(mt_name, np.nan) for mt_name in metric_names]
+        avg_res = np.asarray(avg_res)
+        avg_res = avg_res.reshape(1, len(metric_names))
+        avg_res = pd.DataFrame(data=avg_res, index=np.asarray([model_name]), columns=np.asarray(metric_names))
+        return avg_res
 
 
 class Result:
@@ -77,6 +83,14 @@ class CVSingleModelResult(SingleModelResult):
             for mt in self.per_fold_avg[f]:
                 self.avg[mt] += self.per_fold_avg[f][mt]/len(self.per_fold_avg)
 
+    def _organize_avg_res(self, model_name, metric_names):
+        # global avg
+        self.avg = self._get_data_frame(avg_res=self.avg, model_name=model_name, metric_names=metric_names)
+        # per_fold avg
+        for f in self.per_fold_avg:
+            self.per_fold_avg[f] = self._get_data_frame(avg_res=self.per_fold_avg[f], model_name= model_name,
+                                                        metric_names=metric_names)
+
 
 class CVResult(Result):
     """ Cross Validation Result Class
@@ -84,10 +98,12 @@ class CVResult(Result):
     Parameters
     ----------
     """
-    def __init__(self, per_model_results = {}, avg_results=None):
+    def __init__(self, per_model_results = {}, n_folds, avg_results=None):
         #self.per_model = per_model_results
         self.avg = avg_results
         self.per_fold_avg = {}
+        for f in range(n_folds):
+            self.per_fold_avg[f] = None
 
 
     def _add_model_res(self, res, model_name):
@@ -95,4 +111,8 @@ class CVResult(Result):
             self.avg = res.avg
         else:
             self.avg = self.avg.append(res.avg)
-
+        for f in res.per_fold_avg:
+            if self.per_fold_avg[f] is None:
+                self.per_fold_avg[f] = res.per_fold_avg[f]
+            else:
+                self.per_fold_avg[f] = self.per_fold_avg[f].append(res.per_fold_avg[f])
