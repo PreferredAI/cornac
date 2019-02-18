0ae304e414cc9725029a444da987de7e3bdcfe41
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2019-02-18 16:01:53 +0800

    Update doc for MovieLens

--- cornac/datasets/movielens.py
+++ cornac/datasets/movielens.py
@@ -11,15 +11,33 @@ from ..data.reader import Reader
 
 class MovieLens:
 
-    def __init__(self):
-        super().__init__()
+    @property
+    def base_url(self):
+        """Return the base url of the MovieLens datasets"""
+        return 'http://files.grouplens.org/datasets/movielens'
 
 
 class MovieLens100K(MovieLens):
 
     @staticmethod
     def load_data(format='UIR', verbose=False):
-        download_item = DownloadItem(url='http://files.grouplens.org/datasets/movielens/ml-100k/u.data',
+        """Load the MovieLens 100K dataset
+
+        Parameters
+        ----------
+        format: str, default: 'UIR'
+            Data format to be returned.
+
+        verbose: bool, default: False
+            The verbosity flag.
+
+        Returns
+        -------
+        data: array-like
+            Data in the form of a list of tuples depending on the given data format.
+
+        """
+        download_item = DownloadItem(url='{}/ml-100k/u.data'.format(MovieLens.base_url),
                                      relative_path='ml-100k/u.data')
         fpath = download_item.maybe_download(verbose)
 
@@ -32,7 +50,23 @@ class MovieLens1M(MovieLens):
 
     @staticmethod
     def load_data(format='UIR', verbose=False):
-        download_item = DownloadItem(url='http://files.grouplens.org/datasets/movielens/ml-1m.zip',
+        """Load the MovieLens 1M dataset
+
+        Parameters
+        ----------
+        format: str, default: 'UIR'
+            Data format to be returned.
+
+        verbose: bool, default: False
+            The verbosity flag.
+
+        Returns
+        -------
+        data: array-like
+            Data in the form of a list of tuples depending on the given data format.
+
+        """
+        download_item = DownloadItem(url='{}/ml-1m.zip'.format(MovieLens),
                                      relative_path='ml-1m/ratings.dat', unzip=True)
         fpath = download_item.maybe_download(verbose)
 
