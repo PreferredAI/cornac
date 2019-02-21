13ab352bcde98c77dcb82db9876aa36e09c34126
Tuan Truong <tuantq.vnu@gmail.com>
2019-02-22 01:20:30 +0800

    Add TextModule

---
+++ cornac/data/text.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Quoc-Tuan Truong <tuantq.vnu@gmail.com>
+"""
+
+class TextModule:
+    """Text module
+
+    """
+
+    def __init__(self):
+        pass
+
+    def build(self, ordered_ids):
+        pass
