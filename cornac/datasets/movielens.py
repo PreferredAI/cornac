b6cd262f5ed17b76ef269ab24e95a0e484a56b13
Tuan Truong <tuantq.vnu@gmail.com>
2019-02-22 01:00:34 +0800

    Refactor code

--- cornac/datasets/movielens.py
+++ cornac/datasets/movielens.py
@@ -4,16 +4,19 @@
 @author: Quoc-Tuan Truong <tuantq.vnu@gmail.com>
 """
 
-from ..utils.download import DownloadItem
+from ..utils import validate_format
+from ..utils import cache
 from ..data import reader
 
+VALID_DATA_FORMATS = ['UIR', 'UIRT']
 
-def load_100k(format='UIR', verbose=False):
+
+def load_100k(data_format='UIR'):
     """Load the MovieLens 100K dataset
 
     Parameters
     ----------
-    format: str, default: 'UIR'
+    data_format: str, default: 'UIR'
         Data format to be returned.
 
     verbose: bool, default: False
@@ -25,18 +28,19 @@ def load_100k(format='UIR', verbose=False):
         Data in the form of a list of tuples depending on the given data format.
 
     """
-    fpath = DownloadItem(url='http://files.grouplens.org/datasets/movielens/ml-100k/u.data',
-                         relative_path='ml-100k/u.data').maybe_download(verbose)
-    if format == 'UIR':
+    data_format = validate_format(data_format, VALID_DATA_FORMATS)
+    fpath = cache(url='http://files.grouplens.org/datasets/movielens/ml-100k/u.data',
+                  relative_path='ml-100k/u.data')
+    if data_format == 'UIR':
         return reader.read_uir(fpath)
 
 
-def load_1m(format='UIR', verbose=False):
+def load_1m(data_format='UIR'):
     """Load the MovieLens 1M dataset
 
     Parameters
     ----------
-    format: str, default: 'UIR'
+    data_format: str, default: 'UIR'
         Data format to be returned.
 
     verbose: bool, default: False
@@ -48,7 +52,8 @@ def load_1m(format='UIR', verbose=False):
         Data in the form of a list of tuples depending on the given data format.
 
     """
-    fpath = DownloadItem(url='http://files.grouplens.org/datasets/movielens/ml-1m.zip',
-                         relative_path='ml-1m/ratings.dat', unzip=True).maybe_download(verbose)
-    if format == 'UIR':
+    data_format = validate_format(data_format, VALID_DATA_FORMATS)
+    fpath = cache(url='http://files.grouplens.org/datasets/movielens/ml-1m.zip',
+                  relative_path='ml-1m/ratings.dat', unzip=True)
+    if data_format == 'UIR':
         return reader.read_uir(fpath, sep='::')
