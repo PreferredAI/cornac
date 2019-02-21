d08885c8eab1b729421a7058e0f153d8ba7ee273
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2019-02-22 00:59:51 +0800

    Refactor code

--- cornac/data/__init__.py
+++ cornac/data/__init__.py
@@ -1,7 +1,18 @@
-from .trainset import TrainSet, MatrixTrainSet
+from .text import TextModule
+from .image import ImageModule
+from .graph import GraphModule
+from .trainset import TrainSet
+from .trainset import MatrixTrainSet
+from .trainset import MultimodalTrainSet
 from .testset import TestSet
+from .testset import MultimodalTestSet
 from . import reader
 
-__all__ = ['TrainSet',
+__all__ = ['TextModule',
+           'ImageModule',
+           'GraphModule',
+           'TrainSet',
            'MatrixTrainSet',
-           'TestSet']
+           'MultimodalTrainSet',
+           'TestSet',
+           'MultimodalTestSet']
