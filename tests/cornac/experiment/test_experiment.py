39f9478edd0e880823cdf6dab729df07e09dab1d
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2019-02-24 02:44:32 +0800

    Add tests for Experiment

--- tests/cornac/experiment/test_experiment.py
+++ tests/cornac/experiment/test_experiment.py
@@ -4,9 +4,8 @@
 @author: Quoc-Tuan Truong <tuantq.vnu@gmail.com>
 """
 
-import os
 from cornac.data import reader
-from cornac.eval_methods import RatioSplit
+from cornac.eval_methods import RatioSplit, CrossValidation
 from cornac.models import PMF
 from cornac.metrics import MAE, RMSE, Recall, FMeasure
 from cornac.experiment.experiment import Experiment
@@ -39,3 +38,13 @@ def test_with_ratio_split():
         Experiment(None, [PMF(1, 0)], None)
     except ValueError:
         assert True
+
+
+def test_with_cross_validation():
+    data_file = './tests/data.txt'
+    data = reader.read_uir(data_file)
+    exp = Experiment(eval_method=CrossValidation(data),
+                     models=[PMF(1, 0)],
+                     metrics=[MAE(), RMSE(), Recall(1), FMeasure(1)],
+                     verbose=True)
+    exp.run()
