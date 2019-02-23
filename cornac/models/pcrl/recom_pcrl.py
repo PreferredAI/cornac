4f1f4b6d47b6172660bd665c51540d4698fac625
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2019-02-24 03:34:33 +0800

    Update score function of PCRL model

--- cornac/models/pcrl/recom_pcrl.py
+++ cornac/models/pcrl/recom_pcrl.py
@@ -3,13 +3,12 @@
 @author: Aghiles Salah <asalah@smu.edu.sg>
 """
 
-
-
 import numpy as np
 from ..recommender import Recommender
 from .pcrl import PCRL_
 
-#Recommender class for Probabilistic Collaborative Representation Learning (PCRL)
+
+# Recommender class for Probabilistic Collaborative Representation Learning (PCRL)
 class PCRL(Recommender):
     """Probabilistic Collaborative Representation Learning.
 
@@ -62,25 +61,25 @@ class PCRL(Recommender):
     ----------
     * Salah, Aghiles, and Hady W. Lauw. Probabilistic Collaborative Representation Learning for Personalized Item Recommendation. \
     In UAI 2018.
-    """    
+    """
 
-    def __init__(self, k=100, z_dims = [300], max_iter=300, batch_size = 300,learning_rate = 0.001,aux_info = None, name = "pcrl", trainable = True,w_determinist = True, init_params = {'G_s':None, 'G_r':None, 'L_s':None, 'L_r':None}):
+    def __init__(self, k=100, z_dims=[300], max_iter=300, batch_size=300, learning_rate=0.001, aux_info=None,
+                 name="pcrl", trainable=True, w_determinist=True,
+                 init_params={'G_s': None, 'G_r': None, 'L_s': None, 'L_r': None}):
 
-        Recommender.__init__(self, name=name, trainable = trainable)
+        Recommender.__init__(self, name=name, trainable=trainable)
 
-        self.aux_info = aux_info 
+        self.aux_info = aux_info
         self.k = k
-        self.z_dims = z_dims                #the dimension of the second hidden layer (we consider a 2-layers PCRL)
+        self.z_dims = z_dims  # the dimension of the second hidden layer (we consider a 2-layers PCRL)
         self.max_iter = max_iter
         self.batch_size = batch_size
         self.learning_rate = learning_rate
         self.init_params = init_params
         self.w_determinist = w_determinist
 
-        
-        
-    #fit the recommender model to the traning data    
-    def fit(self,X):
+    # fit the recommender model to the traning data
+    def fit(self, X):
         """Fit the model to observations.
 
         Parameters
@@ -90,68 +89,41 @@ class PCRL(Recommender):
             (e.g., csc_matrix).
         """
         if self.trainable:
-            #intanciate pcrl
-            pcrl_ = PCRL_(cf_data = X, aux_data = self.aux_info, k =self.k, z_dims = self.z_dims, n_epoch=self.max_iter, batch_size = self.batch_size, learning_rate = self.learning_rate, B = 1, w_determinist = self.w_determinist, init_params = self.init_params)
+            # intanciate pcrl
+            pcrl_ = PCRL_(cf_data=X, aux_data=self.aux_info, k=self.k, z_dims=self.z_dims, n_epoch=self.max_iter,
+                          batch_size=self.batch_size, learning_rate=self.learning_rate, B=1,
+                          w_determinist=self.w_determinist, init_params=self.init_params)
             pcrl_.learn()
-                        
-            self.Theta = np.array(pcrl_.Gs)/np.array(pcrl_.Gr)
-            self.Beta = np.array(pcrl_.Ls)/np.array(pcrl_.Lr)
+
+            self.Theta = np.array(pcrl_.Gs) / np.array(pcrl_.Gr)
+            self.Beta = np.array(pcrl_.Ls) / np.array(pcrl_.Lr)
         else:
             print('%s is trained already (trainable = False)' % (self.name))
-        
-   
-    
 
-      
-    def score(self, user_index, item_indexes = None):
-        """Predict the scores/ratings of a user for a list of items.
+    def score(self, user_id, item_id=None):
+        """Predict the scores/ratings of a user for an item.
 
         Parameters
         ----------
-        user_index: int, required
-            The index of the user for whom to perform score predictions.
-            
-        item_indexes: 1d array, optional, default: None
-            A list of item indexes for which to predict the rating score.\
-            When "None", score prediction is performed for all test items of the given user. 
+        user_id: int, required
+            The index of the user for whom to perform score prediction.
+
+        item_id: int, optional, default: None
+            The index of the item for that to perform score prediction.
+            If None, scores for all known items will be returned.
 
         Returns
         -------
-        Numpy 1d array 
-            Array containing the predicted values for the items of interest
+        res : A scalar or a Numpy array
+            Relative scores that the user gives to the item or to all known items
+
         """
-        
-        if item_indexes is None:
-            user_pred = self.Beta*self.Theta[user_index,:].T
+
+        if item_id is None:
+            user_pred = self.Beta * self.Theta[user_id, :].T
         else:
-            user_pred = self.Beta[item_indexes,:]*self.Theta[user_index,:].T
-        #transform user_pred to a flatten array
-        user_pred = np.array(user_pred,dtype='float64').flatten()
-        
+            user_pred = self.Beta[item_id, :] * self.Theta[user_id, :].T
+        # transform user_pred to a flatten array
+        user_pred = np.array(user_pred, dtype='float64').flatten()
+
         return user_pred
-        
-        
-    
-    def rank(self, user_index, known_items = None):
-        """Rank all test items for a given user.
-
-        Parameters
-        ----------
-        user_index: int, required
-            The index of the user for whom to perform item raking.
-        known_items: 1d array, optional, default: None
-            A list of item indices already known by the user
-
-        Returns
-        -------
-        Numpy 1d array 
-            Array of item indices sorted (in decreasing order) relative to some user preference scores. 
-        """  
-        
-        u_pref_score = np.array(self.score(user_index))
-        if known_items is not None:
-            u_pref_score[known_items] = None
-            
-        rank_item_list = (-u_pref_score).argsort()  # ordering the items (in decreasing order) according to the preference score
-
-        return rank_item_list
