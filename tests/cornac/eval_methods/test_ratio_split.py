8f15f356798c6c74cc152d0bafed2d3c127c9292
Tuan Truong <tuantq.vnu@gmail.com>
2019-02-24 02:31:06 +0800

    Add tests for RatioSplit

--- tests/cornac/eval_methods/test_ratio_split.py
+++ tests/cornac/eval_methods/test_ratio_split.py
@@ -5,6 +5,9 @@
 """
 
 from cornac.eval_methods import RatioSplit
+from cornac.data import reader
+from cornac.models import MF
+from cornac.metrics import MAE, Recall
 
 def test_validate_size():
     train_size, val_size, test_size = RatioSplit.validate_size(0.1, 0.2, 10)
@@ -33,6 +36,11 @@ def test_validate_size():
         assert True
 
     try:
+        RatioSplit.validate_size(1, -0.2, 10)
+    except ValueError:
+        assert True
+
+    try:
         RatioSplit.validate_size(11, 0.2, 10)
     except ValueError:
         assert True
@@ -46,3 +54,40 @@ def test_validate_size():
         RatioSplit.validate_size(3, 8, 10)
     except ValueError:
         assert True
+
+
+def test_splits():
+    data_file = './tests/data.txt'
+    data = reader.read_uir(data_file)
+
+    ratio_split = RatioSplit(data, test_size=0.1, val_size=0.1, seed=123, verbose=True)
+    ratio_split.split()
+    assert ratio_split._split_ran
+
+    ratio_split.split()
+
+
+def test_evaluate():
+    data_file = './tests/data.txt'
+    data = reader.read_uir(data_file)
+
+    ratio_split = RatioSplit(data, exclude_unknowns=True, verbose=True)
+    ratio_split.evaluate(MF(), [MAE(), Recall()], user_based=False)
+
+    ratio_split = RatioSplit(data, exclude_unknowns=False, verbose=True)
+    ratio_split.evaluate(MF(), [MAE(), Recall()], user_based=False)
+
+    users = []
+    items = []
+    for u, i, r in data:
+        users.append(u)
+        items.append(i)
+    for u in users:
+        for i in items:
+            data.append((u, i, 5))
+
+    ratio_split = RatioSplit(data, exclude_unknowns=True, verbose=True)
+    ratio_split.evaluate(MF(), [MAE(), Recall()], user_based=True)
+
+    ratio_split = RatioSplit(data, exclude_unknowns=False, verbose=True)
+    ratio_split.evaluate(MF(), [MAE(), Recall()], user_based=True)
