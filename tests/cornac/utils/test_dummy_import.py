906a0a13223e37b62a425d83f052b2329b24a329
Tuan Truong <tuantq.vnu@gmail.com>
2019-02-24 01:46:30 +0800

    Add test for dummy_import

---
+++ tests/cornac/utils/test_dummy_import.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Quoc-Tuan Truong <tuantq.vnu@gmail.com>
+"""
+
+from cornac.utils import tryimport
+
+def test_tryimport():
+    dummy = tryimport('this_module_could_not_exist_bla_bla')
+    try:
+        dummy.some_attribute
+    except ModuleNotFoundError:
+        assert True
