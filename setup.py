3c11f812af283f3ed5cee71ad982346e87d25286
Tuan Truong <tuantq.vnu@gmail.com>
2019-02-26 19:25:33 +0800

    Add Cython implementation of BPR model

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import Extension, setup, find_packages
 import numpy
+import sys
+import platform
+import glob
+import os
 
 try:
     from Cython.Build import cythonize
@@ -11,6 +15,50 @@ else:
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
+
+USE_OPENMP = True
+
+
+def extract_gcc_binaries():
+    """Try to find GCC on OSX for OpenMP support."""
+    patterns = ['/opt/local/bin/g++-mp-[0-9].[0-9]',
+                '/opt/local/bin/g++-mp-[0-9]',
+                '/usr/local/bin/g++-[0-9].[0-9]',
+                '/usr/local/bin/g++-[0-9]']
+    if 'darwin' in platform.platform().lower():
+        gcc_binaries = []
+        for pattern in patterns:
+            gcc_binaries += glob.glob(pattern)
+        gcc_binaries.sort()
+        if gcc_binaries:
+            _, gcc = os.path.split(gcc_binaries[-1])
+            return gcc
+        else:
+            return None
+    else:
+        return None
+
+
+if sys.platform.startswith("win"):
+    # compile args from
+    # https://msdn.microsoft.com/en-us/library/fwkeyyhe.aspx
+    compile_args = ['/O2', '/openmp']
+    link_args = []
+else:
+    gcc = extract_gcc_binaries()
+    if gcc is not None:
+        rpath = '/usr/local/opt/gcc/lib/gcc/' + gcc[-1] + '/'
+        link_args = ['-Wl,-rpath,' + rpath]
+    else:
+        link_args = []
+
+    compile_args = ['-Wno-unused-function', '-Wno-maybe-uninitialized', '-O3', '-ffast-math']
+    if USE_OPENMP:
+        compile_args.append("-fopenmp")
+        link_args.append("-fopenmp")
+
+    compile_args.append("-std=c++11")
+    link_args.append("-std=c++11")
 
 ext = '.pyx' if USE_CYTHON else '.cpp'
 
@@ -39,9 +87,32 @@ extensions = [
               language='c++'),
     Extension(name='recom_mf',
               sources=['cornac/models/mf/recom_mf' + ext],
-              include_dirs=[numpy.get_include()],
-              language='c++')
+              language='c++'),
+    Extension(name='recom_bpr',
+              sources=['cornac/models/bpr/recom_bpr' + ext],
+              language='c++',
+              extra_compile_args=compile_args, extra_link_args=link_args)
 ]
+
+
+def set_gcc():
+    """Try to use GCC on OSX for OpenMP support."""
+    # For macports and homebrew
+    if 'darwin' in platform.platform().lower():
+        gcc = extract_gcc_binaries()
+
+        if gcc is not None:
+            os.environ["CC"] = gcc
+            os.environ["CXX"] = gcc
+
+        else:
+            global use_openmp
+            use_openmp = False
+            print('No GCC available. Install gcc from Homebrew '
+                  'using brew install gcc.')
+
+
+set_gcc()
 
 cmdclass = {}
 
