89183e512de25ecc8ae44161d96213ca79a47a5d
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2019-02-27 20:13:45 +0800

    Add small subsample of Netflix dataset

--- cornac/datasets/netflix.py
+++ cornac/datasets/netflix.py
@@ -14,7 +14,10 @@ VALID_DATA_FORMATS = ['UIR', 'UIRT']
 
 
 def load_data(data_format='UIR'):
-    """Load the Netflix dataset
+    """Load the Netflix entire dataset
+        - Number of ratings: 100,480,507
+        - Number of users:       480,189
+        - Number of items:        17,770
 
     Parameters
     ----------
@@ -30,5 +33,30 @@ def load_data(data_format='UIR'):
     data_format = validate_format(data_format, VALID_DATA_FORMATS)
     fpath = cache(url='https://static.preferred.ai/cornac/datasets/netflix/data.zip',
                   unzip=True, relative_path='netflix/data.csv')
+    if data_format == 'UIR':
+        return reader.read_uir(fpath, sep=',')
+
+
+def load_data_small(data_format='UIR'):
+    """Load a small subset of the Netflix dataset. We draw this subsample such that
+    every user has at least 10 items and each item has at least 10 users.
+        - Number of ratings: 607,803
+        - Number of users:    10,000
+        - Number of items:     5,000
+
+    Parameters
+    ----------
+    data_format: str, default: 'UIR'
+        Data format to be returned.
+
+    Returns
+    -------
+    data: array-like
+        Data in the form of a list of tuples depending on the given data format.
+
+    """
+    data_format = validate_format(data_format, VALID_DATA_FORMATS)
+    fpath = cache(url='https://static.preferred.ai/cornac/datasets/netflix/data_small.zip',
+                  unzip=True, relative_path='netflix/data_small.csv')
     if data_format == 'UIR':
         return reader.read_uir(fpath, sep=',')
