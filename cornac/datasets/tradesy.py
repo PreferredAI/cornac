4a2fa79ea8029de52a60d6bd9d1cc8ef7c94cb56
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2019-02-27 18:54:46 +0800

    Update Tradesy dataset

--- cornac/datasets/tradesy.py
+++ cornac/datasets/tradesy.py
@@ -4,10 +4,10 @@
 @author: Quoc-Tuan Truong <tuantq.vnu@gmail.com>
 
 Original data: http://jmcauley.ucsd.edu/data/tradesy/
-This data is used in the VBPR paper:
-- Number of users: 92835
-- Number of items: 166526
-- Number of feedback: 394421 (410186 is reported but there are duplicates)
+This data is used in the VBPR paper. After cleaning the data we have:
+- Number of feedback: 394,421 (410,186 is reported but there are duplicates)
+- Number of users:     19,243 (19,823 is reported due to duplicates)
+- Number of items:    165,906 (166,521 is reported due to duplicates)
 
 """
 
@@ -40,7 +40,7 @@ def load_feature():
         Item-feature dictionary. Each feature vector is a Numpy array of size 4096.
 
     """
-    fpath = cache(url='https://static.preferred.ai/cornac/datasets/tradesy/item_feature.pkl',
-                  relative_path='tradesy/item_feature.pkl')
+    fpath = cache(url='https://static.preferred.ai/cornac/datasets/tradesy/item_feature.zip',
+                  unzip=True, relative_path='tradesy/item_feature.pkl')
     with open(fpath, 'rb') as f:
         return pickle.load(f)
