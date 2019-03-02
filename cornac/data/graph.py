170f0b1be71ede74c72e7d4ffed4759f0df57d8b
Aghiles <salahaghiles@gmail.com>
2019-03-02 15:25:22 +0800

    Add build_sparse_matrix

--- cornac/data/graph.py
+++ cornac/data/graph.py
@@ -5,6 +5,7 @@
 """
 
 from . import Module
+import scipy.sparse as sp
 
 class GraphModule(Module):
     """Graph module
@@ -14,6 +15,7 @@ class GraphModule(Module):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.raw_data = kwargs.get('data', None)
+        self.matrix = None
         self.map_data = []
         
         
@@ -23,9 +25,16 @@ class GraphModule(Module):
         for i, j, val in self.raw_data:
             self.map_data.append([ordered_ids[i], ordered_ids[j], val])
         self.raw_data.clear()
-        
+       
 
+    def _build_sparse_matrix(self, triplet):
+        n_rows = max(triplet[:,0]) + 1
+        n_cols = max(triplet[:,1]) + 1
+        self.matrix = sp.csc_matrix((triplet[:,2], (triplet[:,0], triplet[:,1])), shape=(len(n_rows), len(n_cols)))
+
+        
 
     def build(self, ordered_ids):
         self._build_triplet(self,ordered_ids) 
+        self._build__build_sparse_matrix(map_data)
         
