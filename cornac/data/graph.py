08fe24ce5ab6284b136406dcfef1e1c615844fb5
Aghiles <salahaghiles@gmail.com>
2019-03-02 15:26:43 +0800

    Change build_sparse_matrix in graph module

--- cornac/data/graph.py
+++ cornac/data/graph.py
@@ -9,7 +9,6 @@ import scipy.sparse as sp
 
 class GraphModule(Module):
     """Graph module
-
     """
 
     def __init__(self, **kwargs):
@@ -22,12 +21,16 @@ class GraphModule(Module):
     def _build_triplet(self, ordered_ids):
         """Build adjacency matrix in sparse triplet format using maped ids
         """
+        
         for i, j, val in self.raw_data:
             self.map_data.append([ordered_ids[i], ordered_ids[j], val])
         self.raw_data.clear()
        
 
     def _build_sparse_matrix(self, triplet):
+        """Build sparse adjacency matrix
+        """
+        
         n_rows = max(triplet[:,0]) + 1
         n_cols = max(triplet[:,1]) + 1
         self.matrix = sp.csc_matrix((triplet[:,2], (triplet[:,0], triplet[:,1])), shape=(len(n_rows), len(n_cols)))
@@ -36,5 +39,5 @@ class GraphModule(Module):
 
     def build(self, ordered_ids):
         self._build_triplet(self,ordered_ids) 
-        self._build__build_sparse_matrix(map_data)
+        self._build__build_sparse_matrix(self.map_data)
         
