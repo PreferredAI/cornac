558434a1cefecae2fa789b7a19fcb11f467a0112
Aghiles <salahaghiles@gmail.com>
2019-03-03 16:34:35 +0800

    reformat code

--- cornac/data/graph.py
+++ cornac/data/graph.py
@@ -8,6 +8,7 @@ from . import Module
 import scipy.sparse as sp
 import numpy as np
 
+
 class GraphModule(Module):
     """Graph module
     """
@@ -17,47 +18,42 @@ class GraphModule(Module):
         self.raw_data = kwargs.get('data', None)
         self.matrix = None
         self.map_data = []
-        
-        
+
     def _build_triplet(self, ordered_ids):
         """Build adjacency matrix in sparse triplet format using maped ids
         """
-        
+
         for i, j, val in self.raw_data:
             self.map_data.append([ordered_ids[i], ordered_ids[j], val])
         self.map_data = np.asanyarray(self.map_data)
         self.raw_data = None
-       
 
     def _build_sparse_matrix(self, triplet):
         """Build sparse adjacency matrix
         """
-        
-        n_rows = max(triplet[:,0]) + 1
-        n_cols = max(triplet[:,1]) + 1
-        self.matrix = sp.csc_matrix((triplet[:,2], (triplet[:,0], triplet[:,1])), shape=(n_rows,n_cols))
 
+        n_rows = max(triplet[:, 0]) + 1
+        n_cols = max(triplet[:, 1]) + 1
+        self.matrix = sp.csc_matrix((triplet[:, 2], (triplet[:, 0], triplet[:, 1])), shape=(n_rows, n_cols))
 
     def get_train_triplet(self, train_row_ids, train_col_ids):
         """Get the training tuples
         """
         train_triplet = []
-        #this makes operations much more efficient
+        # this makes operations much more efficient
         train_row_ids = np.asanyarray(list(train_row_ids))
         train_col_ids = np.asanyarray(list(train_col_ids))
         for i, j, val in self.map_data:
             if (i not in train_row_ids) or (j not in train_col_ids):
-                continue      
-            train_triplet.append([i,j, val])
-                
-        return  np.asarray(train_triplet)  
-        
+                continue
+            train_triplet.append([i, j, val])
+
+        return np.asarray(train_triplet)
 
     def build(self, ordered_ids):
-        self._build_triplet(ordered_ids) 
+        self._build_triplet(ordered_ids)
         self._build_sparse_matrix(self.map_data)
-        
-        
+
     def batch(self, batch_ids):
 
         """Collaborative Context Poisson Factorization.
@@ -67,10 +63,5 @@ class GraphModule(Module):
         batch_ids: array, required
             An array conting the ids of rows to be returned from the sparse adjacency matrix.        
         """
-        
+
         return self.matrix[batch_ids]
-    
-    
-    
-    
-    
