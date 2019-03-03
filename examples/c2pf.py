37fb2b75bc601f26418a233e684787e06bf8d930
Aghiles <salahaghiles@gmail.com>
2019-03-03 16:33:25 +0800

    Change C2PF example and reformat code

--- examples/c2pf.py
+++ examples/c2pf.py
@@ -2,6 +2,36 @@
 """
 Fit to and evaluate C2PF on the office Amzon dataset
 
-@author: Aghiles Salah
+@author: Aghiles Salah <asalah@smu.edu.sg>
 """
 
+from cornac.data import GraphModule
+from cornac.eval_methods import RatioSplit
+from cornac.experiment import Experiment
+from cornac import metrics
+from cornac.models import C2PF
+
+import numpy as np
+
+# load office rating data in triplet format (uid, iid, rating)
+office_ratings = np.loadtxt("path to office ratings")
+
+# load office item context information in triplet format (item_id, context_iterm_id, value), see C2PF paper for details
+office_context = np.loadtxt("path to office content data")
+
+item_graph_module = GraphModule(data=office_context)
+
+ratio_split = RatioSplit(data=office_ratings, test_size=0.2, rating_threshold=3.5, shuffle=True, exclude_unknowns=True,
+                         verbose=True, item_graph=item_graph_module)
+
+rec_c2pf = C2PF(k=100, max_iter=1, variant="c2pf")
+
+# Evaluation metrics
+nDgc = metrics.NDCG(k=-1)
+mrr = metrics.MRR()
+rec = metrics.Recall(k=20)
+pre = metrics.Precision(k=20)
+
+# Instantiate and run your experiment
+exp = Experiment(ratio_split, [rec_c2pf], metrics=[nDgc, mrr, rec, pre], user_based=True)
+exp.run()
