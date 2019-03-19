a0c603da7ed8b7387ed49bf1f448ae9cd1cdc704
Aghiles <salahaghiles@gmail.com>
2019-03-19 12:32:55 +0800

    add load_rating() for amazon_office dataset

--- cornac/datasets/amazon_office.py
+++ cornac/datasets/amazon_office.py
@@ -13,3 +13,22 @@ from ..data import reader
 VALID_DATA_FORMATS = ['UIR', 'UIRT']
 
 
+def load_rating(data_format='UIR'):
+    """Load the user-item ratings
+
+    Parameters
+    ----------
+    data_format: str, default: 'UIR'
+        Data format to be returned.
+
+    Returns
+    -------
+    data: array-like
+        Data in the form of a list of tuples depending on the specified data format.
+    """
+
+    data_format = validate_format(data_format, VALID_DATA_FORMATS)
+    fpath = cache(url='',
+                  relative_path='amazon_office/rating.txt')
+    if data_format == 'UIR':
+        return reader.read_uir(fpath)
