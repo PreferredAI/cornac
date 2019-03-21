7fac16ba50264569982ead414800ebad73ff7cbe
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2019-03-21 21:27:38 +0800

    Change tests of eval_methods to unittest.TestCase

--- tests/cornac/eval_methods/test_cross_validation.py
+++ tests/cornac/eval_methods/test_cross_validation.py
@@ -4,52 +4,46 @@
 @author: Aghiles Salah <asalah@smu.edu.sg>
 """
 
+import unittest
 from cornac.eval_methods import CrossValidation
 from cornac.data import reader
 import numpy as np
 
 
-def test_partition_data():
-    data = reader.read_uir('./tests/data.txt')
+class TestCrossValidation(unittest.TestCase):
 
-    nfolds = 5
-    cv = CrossValidation(data=data, n_folds=nfolds)
+    def setUp(self):
+        self.data = reader.read_uir('./tests/data.txt')
+        self.n_folds = 5
+        self.cv = CrossValidation(data=self.data, n_folds=self.n_folds)
 
-    ref_set = set(range(nfolds))
-    res_set = set(cv.partition)
-    fold_sizes = np.unique(cv.partition, return_counts=True)[1]
+    def test_partition_data(self):
+        ref_set = set(range(self.n_folds))
+        res_set = set(self.cv.partition)
+        fold_sizes = np.unique(self.cv.partition, return_counts=True)[1]
 
-    assert len(data) == len(cv.partition)
-    assert res_set == ref_set
-    assert np.all(fold_sizes == 2)
+        self.assertEqual(len(self.data), len(self.cv.partition))
+        self.assertEqual(res_set, ref_set)
+        np.testing.assert_array_equal(fold_sizes, 2)
+
+    def test_validate_partition(self):
+        try:
+            self.cv._validate_partition([0, 0, 1, 1])
+        except:
+            assert True
+
+        try:
+            self.cv._validate_partition([0, 0, 1, 1, 2, 2, 2, 2, 3, 3])
+        except:
+            assert True
+
+    def test_get_train_test_sets_next_fold(self):
+        for n in range(self.cv.n_folds):
+            self.cv._get_train_test()
+            self.assertEqual(self.cv.current_fold, n)
+            self.assertSequenceEqual(self.cv.train_set.matrix.shape, (8, 8))
+            self.cv._next_fold()
 
 
-def test_validate_partition():
-    data = reader.read_uir('./tests/data.txt')
-
-    nfolds = 5
-    cv = CrossValidation(data=data, n_folds=nfolds)
-
-    try:
-        cv._validate_partition([0, 0, 1, 1])
-    except:
-        assert True
-
-    try:
-        cv._validate_partition([0, 0, 1, 1, 2, 2, 2, 2, 3, 3])
-    except:
-        assert True
-
-
-def test_get_train_test_sets_next_fold():
-    data = reader.read_uir('./tests/data.txt')
-
-    nfolds = 5
-    cv = CrossValidation(data=data, n_folds=nfolds)
-    
-    for n in range(cv.n_folds):
-        cv._get_train_test()
-        assert cv.current_fold == n
-        assert cv.train_set.matrix.shape == (8, 8)
-        cv._next_fold()
-        
+if __name__ == '__main__':
+    unittest.main()
