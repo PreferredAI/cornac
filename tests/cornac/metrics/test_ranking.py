5f78abb11132840dc25cffda82133921e354f5c0
Tuan Truong <tuantq.vnu@gmail.com>
2019-03-21 22:38:39 +0800

    Change tests to unittest.TestCase, improve test coverage (#63)
    * Fix indentations
    
    * Change _add_special_tokens into staticmethod
    
    * Change all tests of data module to unittest.TestCase
    
    * Remove unused code
    
    * Update README.md
    
    * Add test for read_ui function
    
    * Change tests of all datasets to unittest.TestCase
    
    * Do not repeatedly showing split message in RatioSplit
    
    * Change tests of eval_methods to unittest.TestCase
    
    * Change tests of experiment to unittest.TestCase
    
    * Change tests of metrics to unittest.TestCase
    
    * Change tests of utils to unittest.TestCase
    
    * Improve test coverage
    
    * Fix indentations in test_trainset.py

--- tests/cornac/metrics/test_ranking.py
+++ tests/cornac/metrics/test_ranking.py
@@ -4,8 +4,8 @@
 @author: Quoc-Tuan Truong <tuantq.vnu@gmail.com>
 """
 
+import unittest
 import numpy as np
-
 from cornac.metrics.ranking import RankingMetric
 from cornac.metrics.ranking import MeasureAtK
 from cornac.metrics import NDCG
@@ -16,201 +16,200 @@ from cornac.metrics import Recall
 from cornac.metrics import FMeasure
 from cornac.metrics import AUC
 
-def test_ranking_metric():
-    metric = RankingMetric()
 
-    assert metric.type == 'ranking'
-    assert metric.name is None
-    assert metric.k == -1
+class TestRanking(unittest.TestCase):
 
-    try:
-        metric.compute()
-    except NotImplementedError:
-        assert True
+    def test_ranking_metric(self):
+        metric = RankingMetric()
+
+        self.assertEqual(metric.type, 'ranking')
+        self.assertIsNone(metric.name)
+        self.assertEqual(metric.k, -1)
+
+        try:
+            metric.compute()
+        except NotImplementedError:
+            assert True
+
+    def test_ndcg(self):
+        ndcg = NDCG()
+
+        self.assertEqual(ndcg.type, 'ranking')
+        self.assertEqual(ndcg.name, 'NDCG@-1')
+
+        self.assertEqual(1, ndcg.compute(np.asarray([1]), np.asarray([0])))
+
+        ground_truth = np.asarray([1, 0, 1])  # [1, 3]
+        rec_list = np.asarray([0, 2, 1])  # [1, 3, 2]
+        self.assertEqual(1, ndcg.compute(ground_truth, rec_list))
+
+        ndcg_2 = NDCG(k=2)
+        self.assertEqual(ndcg_2.k, 2)
+
+        ground_truth = np.asarray([0, 0, 1])  # [3]
+        rec_list = np.asarray([1, 2, 0])  # [2, 3, 1]
+        self.assertEqual(0.63, float('{:.2f}'.format(ndcg_2.compute(ground_truth, rec_list))))
+
+    def test_ncrr(self):
+        ncrr = NCRR()
+
+        self.assertEqual(ncrr.type, 'ranking')
+        self.assertEqual(ncrr.name, 'NCRR@-1')
+
+        self.assertEqual(1, ncrr.compute(np.asarray([1]), np.asarray([0])))
+
+        ground_truth = np.asarray([1, 0, 1])  # [1, 3]
+        rec_list = np.asarray([0, 2, 1])  # [1, 3, 2]
+        self.assertEqual(1, ncrr.compute(ground_truth, rec_list))
+
+        ground_truth = np.asarray([1, 0, 1])  # [1, 3]
+        rec_list = np.asarray([1, 2, 0])  # [2, 3, 1]
+        self.assertEqual(((1 / 3 + 1 / 2) / (1 + 1 / 2)), ncrr.compute(ground_truth, rec_list))
+
+        ncrr_2 = NCRR(k=2)
+        self.assertEqual(ncrr_2.k, 2)
+
+        ground_truth = np.asarray([0, 0, 1])  # [3]
+        rec_list = np.asarray([1, 2, 0])  # [2, 3, 1]
+        self.assertEqual(0.5, ncrr_2.compute(ground_truth, rec_list))
+
+    def test_mrr(self):
+        mrr = MRR()
+
+        self.assertEqual(mrr.type, 'ranking')
+        self.assertEqual(mrr.name, 'MRR')
+
+        self.assertEqual(1, mrr.compute(np.asarray([1]), np.asarray([0])))
+
+        ground_truth = np.asarray([1, 0, 1])  # [1, 3]
+        rec_list = np.asarray([0, 2, 1])  # [1, 3, 2]
+        self.assertEqual(1, mrr.compute(ground_truth, rec_list))
+
+        ground_truth = np.asarray([1, 0, 1])  # [1, 3]
+        rec_list = np.asarray([1, 2, 0])  # [2, 3, 1]
+        self.assertEqual(1 / 2, mrr.compute(ground_truth, rec_list))
+
+        ground_truth = np.asarray([1, 0, 1])  # [1, 3]
+        rec_list = np.asarray([1])  # [2]
+        try:
+            mrr.compute(ground_truth, rec_list)
+        except ValueError:
+            assert True
+
+    def test_measure_at_k(self):
+        measure_at_k = MeasureAtK()
+
+        self.assertEqual(measure_at_k.type, 'ranking')
+        assert measure_at_k.name is None
+        self.assertEqual(measure_at_k.k, -1)
+
+        tp, tp_fn, tp_fp = measure_at_k.compute(np.asarray([1]), np.asarray([0]))
+        self.assertEqual(1, tp)
+        self.assertEqual(1, tp_fn)
+        self.assertEqual(1, tp_fp)
+
+        ground_truth = np.asarray([1, 0, 1])  # [1, 0, 1]
+        rec_list = np.asarray([0, 2, 1])  # [1, 1, 1]
+        tp, tp_fn, tp_fp = measure_at_k.compute(ground_truth, rec_list)
+        self.assertEqual(2, tp)
+        self.assertEqual(2, tp_fn)
+        self.assertEqual(3, tp_fp)
+
+    def test_precision(self):
+        prec = Precision()
+
+        self.assertEqual(prec.type, 'ranking')
+        self.assertEqual(prec.name, 'Precision@-1')
+
+        self.assertEqual(1, prec.compute(np.asarray([1]), np.asarray([0])))
+
+        ground_truth = np.asarray([1, 0, 1])  # [1, 0, 1]
+        rec_list = np.asarray([0, 2, 1])  # [1, 1, 1]
+        self.assertEqual((2 / 3), prec.compute(ground_truth, rec_list))
+
+        ground_truth = np.asarray([0, 0, 1])  # [0, 0, 1]
+        rec_list = np.asarray([1, 2, 0])  # [1, 1, 1]
+        self.assertEqual((1 / 3), prec.compute(ground_truth, rec_list))
+
+        prec_2 = Precision(k=2)
+        self.assertEqual(prec_2.k, 2)
+
+        ground_truth = np.asarray([0, 0, 1])  # [0, 0, 1]
+        rec_list = np.asarray([1, 2, 0])  # [1, 1, 1]
+        self.assertEqual(0.5, prec_2.compute(ground_truth, rec_list))
+
+    def test_recall(self):
+        rec = Recall()
+
+        self.assertEqual(rec.type, 'ranking')
+        self.assertEqual(rec.name, 'Recall@-1')
+
+        self.assertEqual(1, rec.compute(np.asarray([1]), np.asarray([0])))
+
+        ground_truth = np.asarray([1, 0, 1])  # [1, 0, 1]
+        rec_list = np.asarray([0, 2, 1])  # [1, 1, 1]
+        self.assertEqual(1, rec.compute(ground_truth, rec_list))
+
+        ground_truth = np.asarray([0, 0, 1])  # [0, 0, 1]
+        rec_list = np.asarray([1, 2, 0])  # [1, 1, 1]
+        self.assertEqual(1, rec.compute(ground_truth, rec_list))
+
+        rec_2 = Recall(k=2)
+        self.assertEqual(rec_2.k, 2)
+
+        ground_truth = np.asarray([0, 0, 1])  # [0, 0, 1]
+        rec_list = np.asarray([1, 2, 0])  # [1, 1, 1]
+        self.assertEqual(1, rec_2.compute(ground_truth, rec_list))
+
+    def test_f_measure(self):
+        f1 = FMeasure()
+
+        self.assertEqual(f1.type, 'ranking')
+        self.assertEqual(f1.name, 'F1@-1')
+
+        self.assertEqual(1, f1.compute(np.asarray([1]), np.asarray([0])))
+
+        ground_truth = np.asarray([1, 0, 1])  # [1, 0, 1]
+        rec_list = np.asarray([0, 2, 1])  # [1, 1, 1]
+        self.assertEqual((4 / 5), f1.compute(ground_truth, rec_list))
+
+        ground_truth = np.asarray([0, 0, 1])  # [0, 0, 1]
+        rec_list = np.asarray([1, 2, 0])  # [1, 1, 1]
+        self.assertEqual((1 / 2), f1.compute(ground_truth, rec_list))
+
+        f1_2 = FMeasure(k=2)
+        self.assertEqual(f1_2.k, 2)
+
+        ground_truth = np.asarray([0, 0, 1])  # [0, 0, 1]
+        rec_list = np.asarray([1, 2, 0])  # [1, 1, 1]
+        self.assertEqual((2 / 3), f1_2.compute(ground_truth, rec_list))
+
+        ground_truth = np.asarray([1, 0, 0])  # [1, 0, 0]
+        rec_list = np.asarray([1, 2])  # [0, 1, 1]
+        self.assertEqual(0, f1_2.compute(ground_truth, rec_list))
+
+    def test_auc(self):
+        auc = AUC()
+
+        self.assertEqual(auc.type, 'ranking')
+        self.assertEqual(auc.name, 'AUC')
+
+        gt_pos = np.array([0, 0, 1, 1])
+        pd_scores = np.array([0.1, 0.4, 0.35, 0.8])
+        auc_score = auc.compute(pd_scores, gt_pos)
+        self.assertEqual(0.75, auc_score)
+
+        gt_pos = np.array([0, 1, 0, 1])
+        pd_scores = np.array([0.1, 0.4, 0.35, 0.8])
+        auc_score = auc.compute(pd_scores, gt_pos)
+        self.assertEqual(1.0, auc_score)
+
+        gt_pos = np.array([0, 0, 1, 0])
+        gt_neg = np.array([1, 1, 0, 0])
+        pd_scores = np.array([0.1, 0.4, 0.35, 0.8])
+        auc_score = auc.compute(pd_scores, gt_pos, gt_neg)
+        self.assertEqual(0.5, auc_score)
 
 
-def test_ndcg():
-    ndcg = NDCG()
-
-    assert ndcg.type == 'ranking'
-    assert ndcg.name == 'NDCG@-1'
-
-    assert 1 == ndcg.compute(np.asarray([1]), np.asarray([0]))
-
-    ground_truth = np.asarray([1, 0, 1]) # [1, 3]
-    rec_list = np.asarray([0, 2, 1]) # [1, 3, 2]
-    assert 1 == ndcg.compute(ground_truth, rec_list)
-
-    ndcg_2 = NDCG(k=2)
-    assert ndcg_2.k == 2
-
-    ground_truth = np.asarray([0, 0, 1]) # [3]
-    rec_list = np.asarray([1, 2, 0]) # [2, 3, 1]
-    assert 0.63 == float('{:.2f}'.format(ndcg_2.compute(ground_truth, rec_list)))
-
-
-def test_ncrr():
-    ncrr = NCRR()
-
-    assert ncrr.type == 'ranking'
-    assert ncrr.name == 'NCRR@-1'
-
-    assert 1 == ncrr.compute(np.asarray([1]), np.asarray([0]))
-
-    ground_truth = np.asarray([1, 0, 1]) # [1, 3]
-    rec_list = np.asarray([0, 2, 1]) # [1, 3, 2]
-    assert 1 == ncrr.compute(ground_truth, rec_list)
-
-    ground_truth = np.asarray([1, 0, 1]) # [1, 3]
-    rec_list = np.asarray([1, 2, 0]) # [2, 3, 1]
-    assert ((1/3 + 1/2) / (1 + 1/2)) == ncrr.compute(ground_truth, rec_list)
-
-    ncrr_2 = NCRR(k=2)
-    assert ncrr_2.k == 2
-
-    ground_truth = np.asarray([0, 0, 1]) # [3]
-    rec_list = np.asarray([1, 2, 0]) # [2, 3, 1]
-    assert 0.5 == ncrr_2.compute(ground_truth, rec_list)
-
-
-def test_mrr():
-    mrr = MRR()
-
-    assert mrr.type == 'ranking'
-    assert mrr.name == 'MRR'
-
-    assert 1 == mrr.compute(np.asarray([1]), np.asarray([0]))
-
-    ground_truth = np.asarray([1, 0, 1]) # [1, 3]
-    rec_list = np.asarray([0, 2, 1]) # [1, 3, 2]
-    assert 1 == mrr.compute(ground_truth, rec_list)
-
-    ground_truth = np.asarray([1, 0, 1]) # [1, 3]
-    rec_list = np.asarray([1, 2, 0]) # [2, 3, 1]
-    assert 1/2 == mrr.compute(ground_truth, rec_list)
-
-    ground_truth = np.asarray([1, 0, 1]) # [1, 3]
-    rec_list = np.asarray([1]) # [2]
-    try:
-        mrr.compute(ground_truth, rec_list)
-    except ValueError:
-        assert True
-
-
-def test_measure_at_k():
-    measure_at_k = MeasureAtK()
-
-    assert measure_at_k.type == 'ranking'
-    assert measure_at_k.name is None
-    assert measure_at_k.k == -1
-
-    tp, tp_fn, tp_fp = measure_at_k.compute(np.asarray([1]), np.asarray([0]))
-    assert 1 == tp
-    assert 1 == tp_fn
-    assert 1 == tp_fp
-
-    ground_truth = np.asarray([1, 0, 1]) # [1, 0, 1]
-    rec_list = np.asarray([0, 2, 1]) # [1, 1, 1]
-    tp, tp_fn, tp_fp = measure_at_k.compute(ground_truth, rec_list)
-    assert 2 == tp
-    assert 2 == tp_fn
-    assert 3 == tp_fp
-
-
-def test_precision():
-    prec = Precision()
-
-    assert prec.type == 'ranking'
-    assert prec.name == 'Precision@-1'
-
-    assert 1 == prec.compute(np.asarray([1]), np.asarray([0]))
-
-    ground_truth = np.asarray([1, 0, 1]) # [1, 0, 1]
-    rec_list = np.asarray([0, 2, 1]) # [1, 1, 1]
-    assert (2/3) == prec.compute(ground_truth, rec_list)
-
-    ground_truth = np.asarray([0, 0, 1]) # [0, 0, 1]
-    rec_list = np.asarray([1, 2, 0]) # [1, 1, 1]
-    assert (1/3) == prec.compute(ground_truth, rec_list)
-
-    prec_2 = Precision(k=2)
-    assert prec_2.k == 2
-
-    ground_truth = np.asarray([0, 0, 1]) # [0, 0, 1]
-    rec_list = np.asarray([1, 2, 0]) # [1, 1, 1]
-    assert 0.5 == prec_2.compute(ground_truth, rec_list)
-
-
-def test_recall():
-    rec = Recall()
-
-    assert rec.type == 'ranking'
-    assert rec.name == 'Recall@-1'
-
-    assert 1 == rec.compute(np.asarray([1]), np.asarray([0]))
-
-    ground_truth = np.asarray([1, 0, 1]) # [1, 0, 1]
-    rec_list = np.asarray([0, 2, 1]) # [1, 1, 1]
-    assert 1 == rec.compute(ground_truth, rec_list)
-
-    ground_truth = np.asarray([0, 0, 1]) # [0, 0, 1]
-    rec_list = np.asarray([1, 2, 0]) # [1, 1, 1]
-    assert 1 == rec.compute(ground_truth, rec_list)
-
-    rec_2 = Recall(k=2)
-    assert rec_2.k == 2
-
-    ground_truth = np.asarray([0, 0, 1]) # [0, 0, 1]
-    rec_list = np.asarray([1, 2, 0]) # [1, 1, 1]
-    assert 1 == rec_2.compute(ground_truth, rec_list)
-
-
-def test_f_measure():
-    f1 = FMeasure()
-
-    assert f1.type == 'ranking'
-    assert f1.name == 'F1@-1'
-
-    assert 1 == f1.compute(np.asarray([1]), np.asarray([0]))
-
-    ground_truth = np.asarray([1, 0, 1]) # [1, 0, 1]
-    rec_list = np.asarray([0, 2, 1]) # [1, 1, 1]
-    assert (4/5) == f1.compute(ground_truth, rec_list)
-
-    ground_truth = np.asarray([0, 0, 1]) # [0, 0, 1]
-    rec_list = np.asarray([1, 2, 0]) # [1, 1, 1]
-    assert (1/2) == f1.compute(ground_truth, rec_list)
-
-    f1_2 = FMeasure(k=2)
-    assert f1_2.k == 2
-
-    ground_truth = np.asarray([0, 0, 1]) # [0, 0, 1]
-    rec_list = np.asarray([1, 2, 0]) # [1, 1, 1]
-    assert (2/3) == f1_2.compute(ground_truth, rec_list)
-
-    ground_truth = np.asarray([1, 0, 0]) # [1, 0, 0]
-    rec_list = np.asarray([1, 2]) # [0, 1, 1]
-    assert 0 == f1_2.compute(ground_truth, rec_list)
-
-
-def test_auc():
-    auc = AUC()
-
-    assert auc.type == 'ranking'
-    assert auc.name == 'AUC'
-
-    gt_pos = np.array([0, 0, 1, 1])
-    pd_scores = np.array([0.1, 0.4, 0.35, 0.8])
-    auc_score = auc.compute(pd_scores, gt_pos)
-    assert 0.75 == auc_score
-
-    gt_pos = np.array([0, 1, 0, 1])
-    pd_scores = np.array([0.1, 0.4, 0.35, 0.8])
-    auc_score = auc.compute(pd_scores, gt_pos)
-    assert 1.0 == auc_score
-
-    gt_pos = np.array([0, 0, 1, 0])
-    gt_neg = np.array([1, 1, 0, 0])
-    pd_scores = np.array([0.1, 0.4, 0.35, 0.8])
-    auc_score = auc.compute(pd_scores, gt_pos, gt_neg)
-    assert 0.5 == auc_score
+if __name__ == '__main__':
+    unittest.main()
--- tests/cornac/utils/test_common.py
+++ tests/cornac/utils/test_common.py
@@ -4,7 +4,9 @@
 @author: Quoc-Tuan Truong <tuantq.vnu@gmail.com>
 """
 
+import unittest
 import numpy as np
+import numpy.testing as npt
 from cornac.utils.common import sigmoid
 from cornac.utils.common import safe_indexing
 from cornac.utils.common import validate_format
@@ -14,63 +16,62 @@ from cornac.utils.common import excepts
 from cornac.utils.common import intersects
 
 
-def test_sigmoid():
-    assert 0 == sigmoid(-np.inf)
-    assert 0.5 == sigmoid(0)
-    assert 1 == sigmoid(np.inf)
+class TestCommon(unittest.TestCase):
 
-    assert 0.5 > sigmoid(-0.1)
-    assert 0.5 < sigmoid(0.1)
+    def test_sigmoid(self):
+        self.assertEqual(0, sigmoid(-np.inf))
+        self.assertEqual(0.5, sigmoid(0))
+        self.assertEqual(1, sigmoid(np.inf))
+
+        self.assertGreater(0.5, sigmoid(-0.1))
+        self.assertGreater(sigmoid(0.1), 0.5)
+
+    def test_scale(self):
+        self.assertEqual(1, scale(0, 1, 5, 0, 1))
+        self.assertEqual(3, scale(0.5, 1, 5, 0, 1))
+        self.assertEqual(5, scale(1, 1, 5, 0, 1))
+
+        npt.assert_array_equal(scale(np.asarray([0, 0.25, 0.5, 0.75, 1]), 1, 5),
+                               np.asarray([1, 2, 3, 4, 5]))
+
+    def test_clip(self):
+        self.assertEqual(1, clip(0, 1, 5))
+        self.assertEqual(3, clip(3, 1, 5))
+        self.assertEqual(5, clip(6, 1, 5))
+
+        npt.assert_array_equal(clip(np.asarray([0, 3, 6]), 1, 5),
+                               np.asarray([1, 3, 5]))
+
+    def test_intersects(self):
+        self.assertEqual(0, len(intersects(np.asarray([1]), np.asarray(2))))
+        self.assertEqual(1, len(intersects(np.asarray([2]), np.asarray(2))))
+
+        npt.assert_array_equal(intersects(np.asarray([3, 2, 1]), np.asarray([1, 2])),
+                               np.asarray([2, 1]))
+
+    def test_excepts(self):
+        self.assertEqual(1, len(excepts(np.asarray([1]), np.asarray(2))))
+        self.assertEqual(0, len(excepts(np.asarray([2]), np.asarray(2))))
+
+        npt.assert_array_equal(excepts(np.asarray([3, 2, 1]), np.asarray([1, 2])),
+                               np.asarray([3]))
+
+    def test_safe_indexing(self):
+        npt.assert_array_equal(safe_indexing(np.asarray([3, 2, 1]), np.asarray([1, 2])),
+                               np.asarray([2, 1]))
+        npt.assert_array_equal(safe_indexing(np.asarray([3, 2, 1]), [1, 2]),
+                               np.asarray([2, 1]))
+        self.assertListEqual(safe_indexing([3, 2, 1], [1, 2]), [2, 1])
+
+    def test_validate_format(self):
+        self.assertEqual('UIR', validate_format('UIR', ['UIR']))
+        self.assertEqual('UIRT', validate_format('UIRT', ['UIRT']))
+
+        try:
+            validate_format('iur', ['UIR'])
+        except ValueError:
+            assert True
 
 
-def test_scale():
-    assert 1 == scale(0, 1, 5, 0, 1)
-    assert 3 == scale(0.5, 1, 5, 0, 1)
-    assert 5 == scale(1, 1, 5, 0, 1)
-
-    assert all([a == b for a, b in zip(scale(np.asarray([0, 0.25, 0.5, 0.75, 1]), 1, 5),
-                                       np.asarray([1, 2, 3, 4, 5]))])
-
-
-def test_clip():
-    assert 1 == clip(0, 1, 5)
-    assert 3 == clip(3, 1, 5)
-    assert 5 == clip(6, 1, 5)
-
-    assert all([a == b for a, b in zip(clip(np.asarray([0, 3, 6]), 1, 5),
-                                       np.asarray([1, 3, 5]))])
-
-
-def test_intersects():
-    assert 0 == len(intersects(np.asarray([1]), np.asarray(2)))
-    assert 1 == len(intersects(np.asarray([2]), np.asarray(2)))
-
-    assert all([a == b for a, b in zip(intersects(np.asarray([3, 2, 1]), np.asarray([1, 2])),
-                                       np.asarray([2, 1]))])
-
-
-def test_excepts():
-    assert 1 == len(excepts(np.asarray([1]), np.asarray(2)))
-    assert 0 == len(excepts(np.asarray([2]), np.asarray(2)))
-
-    assert all([a == b for a, b in zip(excepts(np.asarray([3, 2, 1]), np.asarray([1, 2])),
-                                       np.asarray([3]))])
-
-
-def test_safe_indexing():
-    assert all([a == b for a, b in zip(safe_indexing(np.asarray([3, 2, 1]), np.asarray([1, 2])),
-                                       np.asarray([2, 1]))])
-    assert all([a == b for a, b in zip(safe_indexing(np.asarray([3, 2, 1]), [1, 2]),
-                                       np.asarray([2, 1]))])
-    assert all([a == b for a, b in zip(safe_indexing([3, 2, 1], [1, 2]),
-                                       [2, 1])])
-
-
-def test_validate_format():
-    assert 'UIR' == validate_format('UIR', ['UIR'])
-    assert 'UIRT' == validate_format('UIRT', ['UIRT'])
-
-    try:
-        validate_format('iur', ['UIR'])
-    except ValueError:
-        assert True
+if __name__ == '__main__':
+    unittest.main()
