dc18ca3d5f7a71f2beb67cd4e8a9ac2e8db9c92a
Aghiles <salahaghiles@gmail.com>
2019-03-22 17:03:41 +0800

    add test_amazon_office()

--- tests/cornac/datasets/test_amazon_office.py
+++ tests/cornac/datasets/test_amazon_office.py
@@ -12,6 +12,14 @@ from cornac.datasets import amazon_office as office
 
 class TestAmazonOffice(unittest.TestCase):
 
+    def test_amazon_office(self):
+        random.seed(time.time())
+        if random.random() > 0.8:
+            ratings = office.load_rating()
+            contexts = office.load_context()
+            self.assertEqual(len(ratings), 53282)
+            self.assertEqual(len(contexts), 108466)
+
 
 if __name__ == '__main__':
     unittest.main()
