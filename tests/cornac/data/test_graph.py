0ae86592cad1dd00d27acf71268e5fd081f7ef0e
Aghiles <salahaghiles@gmail.com>
2019-03-22 17:44:53 +0800

    format code

--- tests/cornac/data/test_graph.py
+++ tests/cornac/data/test_graph.py
@@ -12,15 +12,15 @@ from cornac.data import reader
 class TestGraphModule(unittest.TestCase):
 
     def load_data(self):
-        self.data = reader.read_uir('./tests/graph_data.txt',sep=' ')
+        self.data = reader.read_uir('./tests/graph_data.txt', sep=' ')
 
     def test_init(self):
         gmd = GraphModule(data=data)
-        #gmd.build(global_id_map=None)
+        # gmd.build(global_id_map=None)
         self.assertEqual(len(gmd.raw_data), 12)
         self.assertEqual(len(gmd.map_data), 0)
         self.assertIsNone(gmd.matrix)
 
+
 if __name__ == '__main__':
     unittest.main()
-
