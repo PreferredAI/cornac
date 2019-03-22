9bd66a0e66cb210dcf3b218dd7eb3ca1bbc07fe1
Aghiles <salahaghiles@gmail.com>
2019-03-22 17:57:18 +0800

    Change TestGraphModule

--- tests/cornac/data/test_graph.py
+++ tests/cornac/data/test_graph.py
@@ -11,11 +11,10 @@ from cornac.data import reader
 
 class TestGraphModule(unittest.TestCase):
 
-    def load_data(self):
-        self.data = reader.read_uir('./tests/graph_data.txt', sep=' ')
-
     def test_init(self):
-        gmd = GraphModule(data=self.data)
+
+        data = reader.read_uir('./tests/graph_data.txt', sep=' ')
+        gmd = GraphModule(data=data)
         # gmd.build(global_id_map=None)
         self.assertEqual(len(gmd.raw_data), 12)
         self.assertEqual(len(gmd.map_data), 0)
