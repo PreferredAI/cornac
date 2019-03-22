f03f05b5b7e14790e3902a6405ea90e6d9c09d5e
Aghiles <salahaghiles@gmail.com>
2019-03-22 16:52:59 +0800

    Create TestAmazonOffice

---
+++ tests/cornac/datasets/test_amazon_office.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Aghiles Salah <asalah@smu.edu.sg>
+"""
+
+import unittest
+import random
+import time
+from cornac.datasets import amazon_office as office
+
+
+class TestAmazonOffice(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
