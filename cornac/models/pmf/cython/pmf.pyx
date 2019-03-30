8daeebae19fc6213aa397aa68f31ba1485a302f1
Aghiles <salahaghiles@gmail.com>
2019-03-30 19:56:16 +0800

    Improve effiency of pmf_non_linear cython code

--- cornac/models/pmf/cython/pmf.pyx
+++ cornac/models/pmf/cython/pmf.pyx
@@ -4,6 +4,7 @@
 """
 import numpy as np
 from libc.math cimport exp
+from libc.math cimport sqrt
 
 
 #Sigmoid function
@@ -116,17 +117,22 @@ def pmf(X,n_X,d_X,k, n_epochs = 100, lamda = 0.01,learning_rate=0.001, init_para
 
 
 #PMF (Gaussian non-linear model version using sigmoid function)  SGD_RMSProp optimizer
-def pmf_non_linear(X,n_X,d_X,k, n_epochs = 100, lamda = 0.001,learning_rate=0.001,gamma = 0.9, init_params = None):
+def pmf_non_linear(double[:,:]X, int n_X, int d_X, int k, int n_epochs = 100, double lamda = 0.001, double learning_rate=0.001, double gamma = 0.9, init_params = None):
   
     #some useful variables
-    loss = np.full(n_epochs, 0.0)
-    n = n_X
-    d = d_X
-    cache_u = np.zeros((n,k))
-    cache_v = np.zeros((d,k))
-    grad_u = np.zeros((n,k))
-    grad_v = np.zeros((d,k))
-    eps = 1e-8
+    cdef:
+        double[:] loss = np.full(n_epochs, 0.0)
+        int n = n_X
+        int d = d_X
+        double[:,:] U
+        double[:,:] V
+        double[:,:] cache_u = np.zeros((n,k))
+        double[:,:] cache_v = np.zeros((d,k))
+        double[:,:] grad_u = np.zeros((n,k))
+        double[:,:] grad_v = np.zeros((d,k))
+        double eps = 1e-8
+        int u_, i_
+        double val, s, norm_u, norm_v
   
     #Parameter initialization
     #User factors
@@ -146,21 +152,36 @@ def pmf_non_linear(X,n_X,d_X,k, n_epochs = 100, lamda = 0.001,learning_rate=0.00
     for epoch in range(n_epochs):
         for u_, i_, val in X:
             u_, i_ = int(u_), int(i_)
-            sg = sigmoid(np.dot(U[u_,:], V[i_,:].T))
+            
+            s = 0.0
+            for j in range(k):
+                s+= U[u_,j]*V[i_,j]
+            sg = sigmoid(s)
+            #sg = sigmoid(np.dot(U[u_,:], V[i_,:].T))
             e = (val - sg)      #Error for the obseved rating u, i, val
             we= e*sg*(1.-sg)   #Weighted error for the obseved rating u, i, val
-            grad_u[u_,:] = we * V[i_,:]- lamda * U[u_, :]
-            cache_u[u_,:] = gamma * cache_u[u_,:] + (1 - gamma) * (grad_u[u_,:]*grad_u[u_,:])
-            U[u_,:] += learning_rate * (grad_u[u_,:]/(np.sqrt(cache_u[u_,:])+eps) ) # Update the user factor, better to reweight the L2 regularization terms acoording the number of ratings per-user 
-            #update item factors
-            grad_v[i_,:] = we * U[u_,:] - lamda * V[i_, :]
-            cache_v[i_,:] = gamma * cache_v[i_,:] + (1 - gamma) * (grad_v[i_,:]*grad_v[i_,:])            
-            V[i_,:] += learning_rate * (grad_v[i_,:]/(np.sqrt(cache_v[i_,:])+eps) )  # Update item factor             
-            loss[epoch]+= e*e  + lamda * (np.dot(U[u_, :].T, U[u_, :]) + np.dot(V[i_, :].T, V[i_, :]))
+            # update user factors
+            for j in range(k):
+                grad_u[u_,j] = we * V[i_,j]- lamda * U[u_,j]
+                cache_u[u_,j] = gamma * cache_u[u_,j] + (1 - gamma) * (grad_u[u_,j]*grad_u[u_,j])
+                U[u_,j] += learning_rate * (grad_u[u_,j]/(sqrt(cache_u[u_,j])+eps)) # Update the user factor, better to reweight the L2 regularization terms acoording the number of ratings per-user 
+            
+            # update item factors
+            for j in range(k):
+                grad_v[i_,j] = we * U[u_,j] - lamda * V[i_, j]
+                cache_v[i_,j] = gamma * cache_v[i_,j] + (1 - gamma) * (grad_v[i_,j]*grad_v[i_,j])            
+                V[i_,j] += learning_rate * (grad_v[i_,j]/(sqrt(cache_v[i_,j]) + eps))  
+                
+            norm_u = 0.0
+            norm_v = 0.0
+            for j in range(k):
+                norm_u += U[u_,j]*U[u_,j]
+                norm_v += V[i_,j]*V[i_,j]
+            
+            #loss[epoch]+= e*e  + lamda * (np.dot(U[u_, :].T, U[u_, :]) + np.dot(V[i_, :].T, V[i_, :]))
+            loss[epoch]+= e*e  + lamda * (norm_u + norm_v)
+
         print('epoch %i, loss: %f' % (epoch, loss[epoch]))
     res = {'U':U,'V':V,'loss': loss}
     
     return res
-
-
-
