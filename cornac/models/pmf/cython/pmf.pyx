65d80f77b98cb43c11ee5eca766df6daec543d70
Aghiles <salahaghiles@gmail.com>
2019-03-31 16:56:29 +0800

    Clean pmf.pyx

--- cornac/models/pmf/cython/pmf.pyx
+++ cornac/models/pmf/cython/pmf.pyx
@@ -53,9 +53,7 @@ def pmf_linear(int[:] uid, int[:] iid, float[:] rat, int n_users, int n_items, i
   
     #Optimization
     for epoch in range(n_epochs):
-        #for u_, i_, val in X:
         for r in range(nnz):
-            #u_, i_ = integral(u_), integral(i_)
             u_, i_, val = uid[r], iid[r], rat[r]
             s = 0.0
             for j in range(k):
@@ -80,7 +78,6 @@ def pmf_linear(int[:] uid, int[:] iid, float[:] rat, int n_users, int n_items, i
                 norm_u += U[u_,j]*U[u_,j]
                 norm_v += V[i_,j]*V[i_,j]
             
-            #loss[epoch]+= e*e  + lamda * (np.dot(U[u_, :].T, U[u_, :]) + np.dot(V[i_, :].T, V[i_, :]))
             loss[epoch]+= e*e  + lamda * (norm_u + norm_v)
 
         print('epoch %i, loss: %f' % (epoch, loss[epoch]))   
@@ -88,54 +85,6 @@ def pmf_linear(int[:] uid, int[:] iid, float[:] rat, int n_users, int n_items, i
     res = {'U':U,'V':V,'loss': loss}
     
     return res
-
-
-
-"""
-#PMF (Gaussian linear-model version) SGD optimizer
-def pmf(X,n_X,d_X,k, n_epochs = 100, lamda = 0.01,learning_rate=0.001, init_params = None):
-  
-    print("I am pmf")
-    ### data preparation
-    loss = np.full(n_epochs, 0.0)
-    n = n_X
-    d = d_X
-
-    cache_u = np.zeros((n,k))
-    cache_v = np.zeros((d,k))
-    grad_u = np.zeros((n,k))
-    grad_v = np.zeros((d,k))
-    double gamma = 0.9
-    double eps = 1e-8
-    #Parameter initialization
-    #User factors
-    if init_params['U'] is None:
-        U = np.random.normal(loc=0.0, scale=0.1, size=n*k).reshape(n,k)
-    else:
-        U = init_params['U']
-    
-    #Item factors
-    if init_params['V'] is None:
-        V = np.random.normal(loc=0.0, scale=0.1, size=d*k).reshape(d,k)
-    else:
-        V = init_params['V']
-  
-    #Optimization
-    
-    for epoch in range(n_epochs):
-        for u_, i_, val in X:
-            u_, i_ = int(u_), int(i_)
-            e = val - np.dot(U[u_,:], V[i_,:].T)  # Error for the obseved rating u, i, val
-            U[u_,:] += learning_rate * ( e * V[i_,:]- lamda * U[u_, :]) # Update the user factor, this update  in not exactly faire need to reweight the L2 regularization terms acoording the number of ratings per-user 
-            V[i_,:] += learning_rate * ( e * U[u_,:]- lamda * V[i_, :])  # Update item factor 
-            loss[epoch]+= e*e  + lamda * (np.dot(U[u_, :].T, U[u_, :]) + np.dot(V[i_, :].T, V[i_, :]))
-        print("loss:",loss[epoch])    
- 
-    res = {'U':U,'V':V,'loss': loss}
-    
-    return res
-"""
-
 
 
 #PMF (Gaussian non-linear model version using sigmoid function)  SGD_RMSProp optimizer
@@ -157,14 +106,13 @@ def pmf_non_linear(int[:] uid, int[:] iid, float[:] rat, int n_users, int n_item
         int u_, i_, j, epoch
         double val, s, e, norm_u, norm_v
   
-    #Parameter initialization
-    #User factors
+    # Initialize user factors
     if init_params['U'] is None:
         U = np.random.normal(loc=0.0, scale=0.001, size=n*k).reshape(n,k)
     else:
         U = init_params['U']
     
-    #Item factors
+    # Initialize item factors
     if init_params['V'] is None:
         V = np.random.normal(loc=0.0, scale=0.001, size=d*k).reshape(d,k)
     else:
@@ -172,16 +120,13 @@ def pmf_non_linear(int[:] uid, int[:] iid, float[:] rat, int n_users, int n_item
   
     #Optimization
     for epoch in range(n_epochs):
-        #for u_, i_, val in X:
         for r in range(nnz):
-            #u_, i_ = integral(u_), integral(i_)
             u_, i_, val = uid[r], iid[r], rat[r]
             
             s = 0.0
             for j in range(k):
                 s+= U[u_,j]*V[i_,j]
             sg = sigmoid(s)
-            #sg = sigmoid(np.dot(U[u_,:], V[i_,:].T))
             e = (val - sg)     #Error for the obseved rating u, i, val
             we= e*sg*(1.-sg)   #Weighted error for the obseved rating u, i, val
             
@@ -203,7 +148,6 @@ def pmf_non_linear(int[:] uid, int[:] iid, float[:] rat, int n_users, int n_item
                 norm_u += U[u_,j]*U[u_,j]
                 norm_v += V[i_,j]*V[i_,j]
             
-            #loss[epoch]+= e*e  + lamda * (np.dot(U[u_, :].T, U[u_, :]) + np.dot(V[i_, :].T, V[i_, :]))
             loss[epoch]+= e*e  + lamda * (norm_u + norm_v)
 
         print('epoch %i, loss: %f' % (epoch, loss[epoch]))
