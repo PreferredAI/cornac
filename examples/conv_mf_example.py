83fe0cd3e87953f8967713f17843c6a82d8310ae
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-03-31 21:57:02 +0800

    Update movielens dataset (#90)
    * load_plot() returns list of plots and list of movie ids
    
    * Update example of ConvMF according to change in movielens dataset
    
    * Update test for movielens

--- examples/conv_mf_example.py
+++ examples/conv_mf_example.py
@@ -9,22 +9,26 @@ Example for Convolutional Matrix Factorization
 import cornac
 from cornac.datasets import movielens
 from cornac.eval_methods import RatioSplit
-from cornac.data.text import TextModule
+from cornac.data import TextModule
 from cornac.data.text import BaseTokenizer
 
-item_text = movielens.load_plot()
+plots, movie_ids = movielens.load_plot()
 ml_1m = movielens.load_1m()
+
 # remove items without plot
-ml_1m = [(u, i, r) for (u, i, r) in ml_1m if i in item_text.keys()]
+ids = set(movie_ids)
+ml_1m = [(u, i, r) for (u, i, r) in ml_1m if i in ids]
 
 # build text module
-item_text_module = TextModule(id_text=item_text, tokenizer=BaseTokenizer('\t'),
+item_text_module = TextModule(corpus=plots, ids=movie_ids,
+                              tokenizer=BaseTokenizer('\t'),
                               max_vocab=8000, max_doc_freq=0.5, stop_words='english')
 
 ratio_split = RatioSplit(data=ml_1m, test_size=0.2, exclude_unknowns=True,
                          item_text=item_text_module, verbose=True)
 convmf = cornac.models.ConvMF(n_epochs=50)
 rmse = cornac.metrics.RMSE()
+
 exp = cornac.Experiment(eval_method=ratio_split,
                         models=[convmf],
                         metrics=[rmse],
