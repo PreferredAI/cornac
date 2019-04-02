27f6c3b3e6e5407cbb6fb23dc5423858403136ca
Tuan Truong <tuantq.vnu@gmail.com>
2019-04-03 00:25:59 +0800

    Support more types of archive in download utils  (#99)

--- tests/cornac/utils/test_download.py
+++ tests/cornac/utils/test_download.py
@@ -12,19 +12,33 @@ from cornac.utils.download import cache
 class TestDownload(unittest.TestCase):
 
     def test_download_normal_file(self):
-        fpath = cache(url='https://static.preferred.ai/cornac/hello_world.txt')
+        fpath = cache(url='https://static.preferred.ai/cornac/tests/hello_world.txt')
         self.assertTrue(os.path.exists(fpath))
         with open(fpath, 'r') as f:
             self.assertEqual("I'm Cornac!", f.read().strip())
 
-        cache(url='https://static.preferred.ai/cornac/hello_world.txt')
+        cache(url='https://static.preferred.ai/cornac/tests/hello_world.txt')
 
     def test_download_zip_file(self):
-        fpath = cache(url='https://static.preferred.ai/cornac/dummy.zip',
+        fpath = cache(url='https://static.preferred.ai/cornac/tests/dummy.zip',
                       unzip=True, relative_path='dummy/hello_world.txt')
         self.assertTrue(os.path.exists(fpath))
         with open(fpath, 'r') as f:
             self.assertEqual("I'm Cornac!", f.read().strip())
+
+    def test_download_gzip_file(self):
+        fpath = cache(url='https://static.preferred.ai/cornac/tests/dummy.tar.gz',
+                      unzip=True, relative_path='dummy/gz.txt')
+        self.assertTrue(os.path.exists(fpath))
+        with open(fpath, 'r') as f:
+            self.assertEqual('gz', f.read().strip())
+
+    def test_downloa_bzip2_file(self):
+        fpath = cache(url='https://static.preferred.ai/cornac/tests/dummy.tar.bz2',
+                      unzip=True, relative_path='dummy/bz2.txt')
+        self.assertTrue(os.path.exists(fpath))
+        with open(fpath, 'r') as f:
+            self.assertEqual('bz2', f.read().strip())
 
 
 if __name__ == '__main__':
