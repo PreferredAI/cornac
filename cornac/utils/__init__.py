bc64cbcf67fa99230a298e2e73e5772fec37acf7
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-04-03 00:19:57 +0800

    Remove tryimport from utils (#98)

--- cornac/utils/__init__.py
+++ cornac/utils/__init__.py
@@ -1,11 +1,9 @@
 from .common import validate_format
 from .common import estimate_batches
 from .download import cache
-from .dummy_import import tryimport
 from cornac.utils.fast_dot import fast_dot
 
 __all__ = ['validate_format',
            'estimate_batches',
            'cache',
-           'tryimport',
            'fast_dot']
