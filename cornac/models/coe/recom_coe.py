cb1b59e03cf62462f3a30a18937c1e1db5d82375
Tuan Truong <tuantq.vnu@gmail.com>
2019-04-03 00:19:57 +0800

    Remove tryimport from utils (#98)

--- cornac/models/coe/recom_coe.py
+++ cornac/models/coe/recom_coe.py
@@ -3,11 +3,9 @@
 @author: Dung D. Le (Andrew) <ddle.2015@smu.edu.sg>
 """
 
-import numpy as np
-from  .coe import *
+from .coe import *
 from ..recommender import Recommender
 from ...exception import ScoreException
-
 
 
 class COE(Recommender):
@@ -56,23 +54,19 @@ class COE(Recommender):
      In Proceedings of the 2016 SIAM International Conference on Data Mining (pp. 396-404). Society for Industrial and Applied Mathematics.
     """
 
-
-
-    def __init__(self, k=20, max_iter=100, learning_rate = 0.05, lamda = 0.001, batch_size = 1000, name="coe",trainable = True, 
-                 verbose=False, init_params = None):
+    def __init__(self, k=20, max_iter=100, learning_rate=0.05, lamda=0.001, batch_size=1000, name="coe", trainable=True,
+                 verbose=False, init_params=None):
         Recommender.__init__(self, name=name, trainable=trainable, verbose=verbose)
         self.k = k
         self.init_params = init_params
         self.max_iter = max_iter
         self.name = name
         self.learning_rate = learning_rate
-        self.lamda = lamda   
-        self.batch_size = batch_size 
-        
+        self.lamda = lamda
+        self.batch_size = batch_size
+
         self.U = init_params['U']  # matrix of user factors
         self.V = init_params['V']  # matrix of item factors
-
-
 
     # fit the recommender model to the traning data
     def fit(self, train_set):
@@ -85,22 +79,23 @@ class COE(Recommender):
             as well as some useful attributes such as mappings to the original user/item ids.\
             Please refer to the class TrainSet in the "data" module for details.
         """
-        
+
         Recommender.fit(self, train_set)
 
-        X = self.train_set.matrix   
+        X = self.train_set.matrix
 
         if self.verbose:
             print('Learning...')
-        res = coe(X, k=self.k, n_epochs=self.max_iter,lamda = self.lamda, learning_rate= self.learning_rate, batch_size = self.batch_size, init_params=self.init_params)
+        res = coe(X, k=self.k, n_epochs=self.max_iter, lamda=self.lamda, learning_rate=self.learning_rate,
+                  batch_size=self.batch_size, init_params=self.init_params)
         self.U = np.asarray(res['U'])
         self.V = np.asarray(res['V'])
-        
+
         if self.verbose:
             print('Learning completed')
 
-    #get prefiction for a single user (predictions for one user at a time for efficiency purposes)
-    #predictions are not stored for the same efficiency reasons"""
+    # get prefiction for a single user (predictions for one user at a time for efficiency purposes)
+    # predictions are not stored for the same efficiency reasons"""
 
     def score(self, user_id, item_id=None):
         """Predict the scores/ratings of a user for an item.
@@ -130,5 +125,5 @@ class COE(Recommender):
             if self.train_set.is_unk_user(user_id) or self.train_set.is_unk_item(item_id):
                 raise ScoreException("Can't make score prediction for (user_id=%d, item_id=%d)" % (user_id, item_id))
 
-            user_pred = np.sum(np.abs(self.V[item_id,:] - self.U[user_id, :])**2,axis=-1)**(1./2)
+            user_pred = np.sum(np.abs(self.V[item_id, :] - self.U[user_id, :]) ** 2, axis=-1) ** (1. / 2)
             return user_pred
