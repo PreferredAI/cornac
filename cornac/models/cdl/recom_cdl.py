59b5529adaccc4a7e9227e2153a1813db086efc0
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-04-08 20:18:00 +0800

    Refactor CDL model to train with sparse batches (#110)

--- cornac/models/cdl/recom_cdl.py
+++ cornac/models/cdl/recom_cdl.py
@@ -5,8 +5,8 @@
 """
 
 from ..recommender import Recommender
-from .cdl import *
 from ...exception import ScoreException
+import numpy as np
 
 
 class CDL(Recommender):
@@ -14,17 +14,24 @@ class CDL(Recommender):
 
     Parameters
     ----------
+    name: string, default: 'CDL'
+        The name of the recommender model.
+
     k: int, optional, default: 50
         The dimension of the latent factors.
 
     max_iter: int, optional, default: 100
         Maximum number of iterations or the number of epochs for SGD.
 
-    autoencoder_structure：array, optional, default: [200]
-        The number of neurons of encoder/ decoder layer for SDAE
+    autoencoder_structure: list, default: None
+        The number of neurons of encoder/decoder layer for SDAE.
+        For example, autoencoder_structure = [200], the SDAE structure will be [vocab_size, 200, k, 200, vocab_size]
 
     learning_rate: float, optional, default: 0.001
         The learning rate for AdamOptimizer.
+
+    vocab_size: int, default: 8000
+        The size of text input for the SDAE.
 
     lambda_u: float, optional, default: 0.1
         The regularization parameter for users.
@@ -44,17 +51,14 @@ class CDL(Recommender):
     b: float, optional, default: 0.01
         The confidence of unseen ratings.
 
-    autoencoder_corruption: float, optional, default: 0.3
-        The corruption ratio for SDAE.
+    corruption_rate: float, optional, default: 0.3
+        The corruption ratio for input text of the SDAE.
 
     dropout_rate: float, optional, default: 0.1
         The probability that each element is removed in dropout of SDAE.
 
     batch_size: int, optional, default: 100
         The batch size for SGD.
-
-    name: string, optional, default: 'CDL'
-        The name of the recommender model.
 
     trainable: boolean, optional, default: True
         When False, the model is not trained and Cornac assumes that the model already 
@@ -67,16 +71,20 @@ class CDL(Recommender):
         V: ndarray, shape (n_items,k)
             The item latent factors, optional initialization via init_params.
 
+    seed: int, optional, default: None
+        Random seed for weight initialization.
+
     References
     ----------
     * Hao Wang, Naiyan Wang, Dit-Yan Yeung. CDL: Collaborative Deep Learning for Recommender Systems. In : SIGKDD. 2015. p. 1235-1244.
     """
 
-    def __init__(self, k=50, autoencoder_structure=None, lambda_u=0.1, lambda_v=100,
-                 lambda_w=0.1, lambda_n=1000, a=1, b=0.01, autoencoder_corruption=0.3, learning_rate=0.001,
-                 dropout_rate=0.1, batch_size=128, max_iter=100, name="CDL", trainable=True, verbose=True,
-                 init_params=None):
-        Recommender.__init__(self, name=name, trainable=trainable, verbose=verbose)
+    def __init__(self, name='CDL',
+                 k=50, autoencoder_structure=None, lambda_u=0.1, lambda_v=100, lambda_w=0.1, lambda_n=1000,
+                 a=1, b=0.01, corruption_rate=0.3, learning_rate=0.001, vocab_size=8000,
+                 dropout_rate=0.1, batch_size=128, max_iter=100, trainable=True, verbose=True,
+                 init_params=None, seed=None):
+        super().__init__(name=name, trainable=trainable, verbose=verbose)
         self.k = k
         self.lambda_u = lambda_u
         self.lambda_v = lambda_v
@@ -84,17 +92,19 @@ class CDL(Recommender):
         self.lambda_n = lambda_n
         self.a = a
         self.b = b
-        self.autoencoder_corruption = autoencoder_corruption
+        self.corruption_rate = corruption_rate
         self.dropout_rate = dropout_rate
         self.learning_rate = learning_rate
+        self.vocab_size = vocab_size
         self.name = name
         self.init_params = init_params
         self.max_iter = max_iter
         self.autoencoder_structure = autoencoder_structure
         self.batch_size = batch_size
         self.verbose = verbose
+        self.init_params = {} if not init_params else init_params
+        self.seed = seed
 
-    # fit the recommender model to the traning data
     def fit(self, train_set):
         """Fit the model to observations.
 
@@ -105,23 +115,72 @@ class CDL(Recommender):
             as well as some useful attributes such as mappings to the original user/item ids.\
             Please refer to the class TrainSet in the "data" module for details.
         """
-
         Recommender.fit(self, train_set)
-        if not self.trainable:
-            print('%s is trained already (trainable = False)' % (self.name))
-            return
+
+        from ...utils.init_utils import xavier_uniform
+        self.U = self.init_params.get('U', xavier_uniform((self.train_set.num_users, self.k), self.seed))
+        self.V = self.init_params.get('V', xavier_uniform((self.train_set.num_items, self.k), self.seed))
+
+        if self.trainable:
+            self._cdl()
+
+    def _cdl(self):
+        import tensorflow as tf
+        from .cdl import Model
+        from tqdm import trange
+
+        np.random.seed(self.seed)
+
+        R = self.train_set.csc_matrix  # csc for efficient slicing over items
+        n_users, n_items, = self.train_set.num_users, self.train_set.num_items
+
+        text_feature = self.train_set.item_text.batch_bow(np.arange(n_items))  # bag of word feature
+        text_feature = (text_feature - text_feature.min()) / (text_feature.max() - text_feature.min())  # normalization
+
+        layer_sizes = [self.vocab_size] + self.autoencoder_structure + [self.k] \
+                      + self.autoencoder_structure + [self.vocab_size]
+
+        # Build model
+        model = Model(n_users=n_users, n_items=n_items, n_vocab=self.vocab_size, k=self.k, layers=layer_sizes,
+                      lambda_u=self.lambda_u, lambda_v=self.lambda_v, lambda_w=self.lambda_w, lambda_n=self.lambda_n,
+                      lr=self.learning_rate, dropout_rate=self.dropout_rate, U_init=self.U, V_init=self.V)
+
+        # Training model
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            loop = trange(self.max_iter, disable=not self.verbose)
+            for _ in loop:
+                corruption_mask = np.random.binomial(1, 1 - self.corruption_rate,
+                                                     size=(n_items, self.vocab_size))
+                sum_loss = 0
+                count = 0
+                for i, batch_ids in enumerate(self.train_set.item_iter(self.batch_size, shuffle=True)):
+                    batch_R = R[:, batch_ids]
+                    batch_C = np.ones(batch_R.shape) * self.b
+                    batch_C[batch_R.nonzero()] = self.a
+
+                    feed_dict = {
+                        model.text_mask: corruption_mask[batch_ids, :],
+                        model.text_input: text_feature[batch_ids],
+                        model.ratings: batch_R.A,
+                        model.C: batch_C,
+                        model.item_ids: batch_ids
+                    }
+                    sess.run(model.opt1, feed_dict)  # train U, V
+                    _, _loss = sess.run([model.opt2, model.loss], feed_dict)  # train SDAE
+
+                    sum_loss += _loss
+                    count += len(batch_ids)
+                    if i % 10 == 0:
+                        loop.set_postfix(loss=(sum_loss / count))
+
+            self.U, self.V = sess.run([model.U, model.V])
+
+        tf.reset_default_graph()
 
         if self.verbose:
-            print('Learning...')
-
-        self.U, self.V = cdl(train_set, self.autoencoder_structure, k=self.k, lambda_u=self.lambda_u,
-                             lambda_v=self.lambda_v, lambda_w=self.lambda_w, lambda_n=self.lambda_n,
-                             a=self.a, b=self.b, corruption_rate=self.autoencoder_corruption,
-                             n_epochs=self.max_iter, lr=self.learning_rate, dropout_rate=self.dropout_rate,
-                             batch_size=self.batch_size, init_params=self.init_params, verbose=self.verbose)
-
-        if self.verbose:
-            print('\nLearning completed')
+            print('Learning completed!')
 
     def score(self, user_id, item_id=None):
         """Predict the scores/ratings of a user for an item.
@@ -139,9 +198,7 @@ class CDL(Recommender):
         -------
         res : A scalar or a Numpy array
             Relative scores that the user gives to the item or to all known items
-
         """
-
         if item_id is None:
             if self.train_set.is_unk_user(user_id):
                 raise ScoreException("Can't make score prediction for (user_id=%d)" % user_id)
