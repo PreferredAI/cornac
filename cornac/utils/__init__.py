52678ac0799242268f26c954e790fbd503dbac43
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-04-12 23:28:38 +0800

    Add get_rng() function in utils (#121)

--- cornac/utils/__init__.py
+++ cornac/utils/__init__.py
@@ -1,9 +1,11 @@
 from .common import validate_format
 from .common import estimate_batches
+from .common import get_rng
 from .download import cache
-from cornac.utils.fast_dot import fast_dot
+from .fast_dot import fast_dot
 
 __all__ = ['validate_format',
            'estimate_batches',
+           'get_rng',
            'cache',
            'fast_dot']
