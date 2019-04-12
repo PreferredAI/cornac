3e87822986670b5183fff3a8eb1dc98d7f6f999b
Aghiles <salahaghiles@gmail.com>
2019-04-12 17:37:01 +0800

    Update graph module

--- cornac/data/graph.py
+++ cornac/data/graph.py
@@ -18,25 +18,36 @@ class GraphModule(FeatureModule):
         self.raw_data = kwargs.get('data', None)
         self.matrix = None
         self.map_data = []
+        self.map_rid = []
+        self.map_cid = []
+        self.val = []
 
     def _build_triplet(self, global_id_map):
         """Build adjacency matrix in sparse triplet format using mapped ids
         """
 
-        for i, j, val in self.raw_data:
-            self.map_data.append([global_id_map[i], global_id_map[j], val])
-        self.map_data = np.asanyarray(self.map_data)
-        #self.raw_data = None
+        for i, j, v in self.raw_data:
+            self.map_rid.append(global_id_map[i])
+            self.map_cid.append(global_id_map[j])
+            self.val.append(v)
+            
+            #self.map_data = (np.asarray(self.map_rid, dtype=np.int),
+            #                 np.asarray(self.map_cid, dtype=np.int),
+            #                 np.asarray(self.val, dtype=np.float))
+            
+        self.map_rid = np.asarray(self.map_rid, dtype=np.int)
+        self.map_cid = np.asarray(self.map_cid, dtype=np.int)
+        self.val = np.asarray(self.val, dtype=np.float)
+
 
     def _build_sparse_matrix(self, triplet):
         """Build sparse adjacency matrix
         """
+        (rid, cid, val) = triplet
+        n_rows = int(max(rid) + 1)
+        n_cols = int(max(cid) + 1)
 
-        n_rows = max(triplet[:, 0]) + 1
-        n_cols = max(triplet[:, 1]) + 1
-
-        # TODO: csr_matrix is more efficient for row slicing in batch function
-        self.matrix = sp.csc_matrix((triplet[:, 2], (triplet[:, 0], triplet[:, 1])), shape=(n_rows, n_cols))
+        self.matrix = sp.csr_matrix((val, (rid, cid)), shape=(n_rows, n_cols))
 
     def get_train_triplet(self, train_row_ids, train_col_ids):
         """Get the training tuples
@@ -48,7 +59,7 @@ class GraphModule(FeatureModule):
         # this makes operations much more efficient
         train_row_ids = np.asanyarray(list(train_row_ids))
         train_col_ids = np.asanyarray(list(train_col_ids))
-        for i, j, v in self.map_data:
+        for i, j, v in zip(self.map_rid, self.map_cid, self.val):
             if (i not in train_row_ids) or (j not in train_col_ids):
                 continue
             rid.append(i)
@@ -64,7 +75,7 @@ class GraphModule(FeatureModule):
     # TODO: id_map can be None to support GraphModule as an independent component
     def build(self, id_map=None):
         self._build_triplet(id_map)
-        self._build_sparse_matrix(self.map_data)
+        self._build_sparse_matrix((self.map_rid, self.map_cid, self.val))
 
     # TODO: add feature_fallback decorator and rename the API more meaningful
     def batch(self, batch_ids):
