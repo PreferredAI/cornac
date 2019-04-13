fcd6f67c3bc9882b40edf855e6a84234d521c144
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-04-13 23:24:44 +0800

    Refactor Graph module (#130)

--- cornac/data/graph.py
+++ cornac/data/graph.py
@@ -4,9 +4,10 @@
 @author: Aghiles Salah <asalah@smu.edu.sg>
 """
 
-from . import FeatureModule
 import scipy.sparse as sp
 import numpy as np
+
+from . import FeatureModule
 
 
 class GraphModule(FeatureModule):
@@ -16,60 +17,53 @@ class GraphModule(FeatureModule):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.raw_data = kwargs.get('data', None)
-        self.matrix = None
+        self.__matrix = None
+
+    @property
+    def matrix(self):
+        if self.__matrix is None:
+            n_rows = int(max(self.map_rid) + 1)
+            n_cols = int(max(self.map_cid) + 1)
+            self.__matrix = sp.csr_matrix((self.val, (self.map_cid, self.map_cid)),
+                                          shape=(n_rows, n_cols))
+        return self.__matrix
+
+    def _build_triplet(self, id_map):
+        """Build adjacency matrix in sparse triplet format using mapped ids
+        """
         self.map_rid = []
         self.map_cid = []
         self.val = []
-
-    def _build_triplet(self, global_id_map):
-        """Build adjacency matrix in sparse triplet format using mapped ids
-        """
-
         for i, j, v in self.raw_data:
-            self.map_rid.append(global_id_map[i])
-            self.map_cid.append(global_id_map[j])
+            if (i not in id_map) or (j not in id_map):
+                continue
+            self.map_rid.append(id_map[i])
+            self.map_cid.append(id_map[j])
             self.val.append(v)
-            
+
         self.map_rid = np.asarray(self.map_rid, dtype=np.int)
         self.map_cid = np.asarray(self.map_cid, dtype=np.int)
         self.val = np.asarray(self.val, dtype=np.float)
 
-
-    def _build_sparse_matrix(self, triplet):
-        """Build sparse adjacency matrix
-        """
-        (rid, cid, val) = triplet
-        n_rows = int(max(rid) + 1)
-        n_cols = int(max(cid) + 1)
-
-        self.matrix = sp.csr_matrix((val, (rid, cid)), shape=(n_rows, n_cols))
+    def build(self, id_map=None):
+        if id_map is None:
+            raise ValueError('id_map is required but None!')
+        self._build_triplet(id_map)
 
     def get_train_triplet(self, train_row_ids, train_col_ids):
         """Get the training tuples
         """
-        rid = []
-        cid = []
-        val = []
-        train_triplet = []
-        # this makes operations much more efficient
-        train_row_ids = np.asanyarray(list(train_row_ids))
-        train_col_ids = np.asanyarray(list(train_col_ids))
-        for i, j, v in zip(self.map_rid, self.map_cid, self.val):
+        picked_idx = []
+        train_row_ids = set(train_row_ids) if not isinstance(train_row_ids, set) else train_row_ids
+        train_col_ids = set(train_col_ids) if not isinstance(train_col_ids, set) else train_col_ids
+        for idx, (i, j, v) in enumerate(zip(self.map_rid, self.map_cid, self.val)):
             if (i not in train_row_ids) or (j not in train_col_ids):
                 continue
-            rid.append(i)
-            cid.append(j)
-            val.append(v)
-            
-        train_triplet = (np.asarray(rid, dtype=np.int),
-        np.asarray(cid, dtype=np.int),
-        np.asarray(val, dtype=np.float))
+            picked_idx.append(idx)
 
-        return train_triplet
-
-    def build(self, id_map):
-        self._build_triplet(id_map)
-        self._build_sparse_matrix((self.map_rid, self.map_cid, self.val))
+        return self.map_cid[picked_idx], \
+               self.map_rid[picked_idx], \
+               self.val[picked_idx]
 
     # TODO: add feature_fallback decorator and rename the API more meaningful
     def batch(self, batch_ids):
--- tests/cornac/data/test_graph.py
+++ tests/cornac/data/test_graph.py
@@ -17,10 +17,6 @@ class TestGraphModule(unittest.TestCase):
         gmd = GraphModule(data=data)
 
         self.assertEqual(len(gmd.raw_data), 7)
-        self.assertEqual(len(gmd.map_rid), 0)
-        self.assertEqual(len(gmd.map_cid), 0)
-        self.assertEqual(len(gmd.val), 0)
-        self.assertIsNone(gmd.matrix)
 
     def test_build(self):
         data = Reader().read('./tests/graph_data.txt', sep=' ')
@@ -28,7 +24,7 @@ class TestGraphModule(unittest.TestCase):
 
         global_iid_map = OrderedDict()
         for raw_iid, raw_jid, val in data:
-            mapped_iid = global_iid_map.setdefault(raw_iid, len(global_iid_map))
+            global_iid_map.setdefault(raw_iid, len(global_iid_map))
 
         gmd.build(id_map=global_iid_map)
 
@@ -37,15 +33,18 @@ class TestGraphModule(unittest.TestCase):
         self.assertEqual(len(gmd.val), 7)
         self.assertEqual(gmd.matrix.shape, (7, 3))
 
+        try:
+            GraphModule().build()
+        except ValueError:
+            assert True
 
     def test_get_train_triplet(self):
-
         data = Reader().read('./tests/graph_data.txt', sep=' ')
         gmd = GraphModule(data=data)
 
         global_iid_map = OrderedDict()
         for raw_iid, raw_jid, val in data:
-            mapped_iid = global_iid_map.setdefault(raw_iid, len(global_iid_map))
+            global_iid_map.setdefault(raw_iid, len(global_iid_map))
 
         gmd.build(id_map=global_iid_map)
         rid, cid, val = gmd.get_train_triplet([0, 1, 2], [0, 1, 2])
@@ -53,7 +52,10 @@ class TestGraphModule(unittest.TestCase):
         self.assertEqual(len(cid), 3)
         self.assertEqual(len(val), 3)
 
-
+        rid, cid, val = gmd.get_train_triplet([0, 2], [0, 1])
+        self.assertEqual(len(rid), 1)
+        self.assertEqual(len(cid), 1)
+        self.assertEqual(len(val), 1)
 
 
 if __name__ == '__main__':
