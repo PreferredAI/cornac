c0fb754c69d93c61e97ab449df11d5009ec4a7d7
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2019-04-13 17:31:33 +0800

    Update init_utils functions

--- cornac/utils/init_utils.py
+++ cornac/utils/init_utils.py
@@ -21,17 +21,75 @@ def constant(shape, val, dtype=np.float32):
     return ones(shape, dtype=dtype) * val
 
 
-def uniform(shape, low=0.0, high=1.0, seed=None, dtype=np.float32):
-    return get_rng(seed).uniform(low, high, shape).astype(dtype)
+def uniform(shape=None, low=0.0, high=1.0, random_state=None, dtype=np.float32):
+    """
+    Draw samples from a uniform distribution.
+
+    Parameters
+    ----------
+    shape : int or tuple of ints, optional
+        Output shape. If shape is ``None`` (default), a single value is returned.
+    low : float or array_like of floats, optional
+        Lower boundary of the output interval.  All values generated will be
+        greater than or equal to low.  The default value is 0.
+    high : float or array_like of floats
+        Upper boundary of the output interval.  All values generated will be
+        less than high.  The default value is 1.0.
+    random_state : int or np.random.RandomState, optional
+        If an integer is given, it will be used as seed value for creating a RandomState.
+    dtype : str or dtype
+        Returned data-type for the output array.
+
+    Returns
+    -------
+    out : ndarray or scalar
+        Drawn samples from the parameterized uniform distribution.
+    """
+    return get_rng(random_state).uniform(low, high, shape).astype(dtype)
 
 
-def normal(shape, mean=0.0, std=1.0, seed=None, dtype=np.float32):
-    return get_rng(seed).normal(mean, std, shape).astype(dtype)
+def normal(shape=None, mean=0.0, std=1.0, random_state=None, dtype=np.float32):
+    """
+    Draw random samples from a normal (Gaussian) distribution.
+
+    Parameters
+    ----------
+    shape : int or tuple of ints, optional
+        Output shape. If shape is ``None`` (default), a single value is returned.
+    mean : float or array_like of floats
+        Mean of the distribution.
+    std : float or array_like of floats
+        Standard deviation of the distribution.
+    random_state : int or np.random.RandomState, optional
+        If an integer is given, it will be used as seed value for creating a RandomState.
+    dtype : str or dtype
+        Returned data-type for the output array.
+
+    Returns
+    -------
+    out : ndarray or scalar
+        Drawn samples from the parameterized normal distribution.
+    """
+    return get_rng(random_state).normal(mean, std, shape).astype(dtype)
 
 
-def xavier_uniform(shape, seed=None, dtype=np.float32):
+def xavier_uniform(shape, random_state=None, dtype=np.float32):
     """Return a numpy array by performing 'Xavier' initializer
     also known as 'Glorot' initializer on Uniform distribution.
+
+    Parameters
+    ----------
+    shape : int or tuple of ints
+        Output shape.
+    random_state : int or np.random.RandomState, optional
+        If an integer is given, it will be used as seed value for creating a RandomState.
+    dtype : str or dtype
+        Returned data-type for the output array.
+
+    Returns
+    -------
+    out : ndarray
+        Output matrix.
 
     References
     ----------
@@ -43,12 +101,26 @@ def xavier_uniform(shape, seed=None, dtype=np.float32):
     assert len(shape) == 2  # only support matrix
     std = np.sqrt(2.0 / np.sum(shape))
     limit = np.sqrt(3.0) * std
-    return uniform(shape, -limit, limit, seed, dtype)
+    return uniform(shape, -limit, limit, random_state, dtype)
 
 
-def xavier_normal(shape, seed=None, dtype=np.float32):
+def xavier_normal(shape, random_state=None, dtype=np.float32):
     """Return a numpy array by performing 'Xavier' initializer
     also known as 'Glorot' initializer on Normal distribution.
+
+    Parameters
+    ----------
+    shape : int or tuple of ints
+        Output shape.
+    random_state : int or np.random.RandomState, optional
+        If an integer is given, it will be used as seed value for creating a RandomState.
+    dtype : str or dtype
+        Returned data-type for the output array.
+
+    Returns
+    -------
+    out : ndarray
+        Output matrix.
 
     References
     ----------
@@ -59,4 +131,4 @@ def xavier_normal(shape, seed=None, dtype=np.float32):
     """
     assert len(shape) == 2  # only support matrix
     std = np.sqrt(2.0 / np.sum(shape))
-    return normal(shape, 0, std, seed, dtype)
+    return normal(shape, 0, std, random_state, dtype)
