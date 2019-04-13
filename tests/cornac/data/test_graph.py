76b9791243537b31cbca6ce8da7952c3e5d0ef57
Aghiles <salahaghiles@gmail.com>
2019-04-13 12:08:38 +0800

    Change map_data format inside graph module (#120)
    * Update get_train_triplet func
    
    * Update recom_mcf due to change in graph module
    
    * Update c2pf due to change in graph module
    
    * Update graph module
    
    * change data format in graph module
    
    * Update tests for graph due to change in this module
    
    * Make id_map in build() fun for graph a required parameter
    
    * Fix issue with val attribute
    
    * Update toy dataset to test graph module
    
    * Add test for build in graph module
    
    * Add tests for get_train_triplet in graph module
    
    * Fix issue in test get_train_triplet

--- tests/cornac/data/test_graph.py
+++ tests/cornac/data/test_graph.py
@@ -7,6 +7,7 @@
 import unittest
 from cornac.data import GraphModule
 from cornac.data import Reader
+from collections import OrderedDict
 
 
 class TestGraphModule(unittest.TestCase):
@@ -14,10 +15,45 @@ class TestGraphModule(unittest.TestCase):
     def test_init(self):
         data = Reader().read('./tests/graph_data.txt', sep=' ')
         gmd = GraphModule(data=data)
-        # gmd.build(global_id_map=None)
-        self.assertEqual(len(gmd.raw_data), 12)
-        self.assertEqual(len(gmd.map_data), 0)
+
+        self.assertEqual(len(gmd.raw_data), 7)
+        self.assertEqual(len(gmd.map_rid), 0)
+        self.assertEqual(len(gmd.map_cid), 0)
+        self.assertEqual(len(gmd.val), 0)
         self.assertIsNone(gmd.matrix)
+
+    def test_build(self):
+        data = Reader().read('./tests/graph_data.txt', sep=' ')
+        gmd = GraphModule(data=data)
+
+        global_iid_map = OrderedDict()
+        for raw_iid, raw_jid, val in data:
+            mapped_iid = global_iid_map.setdefault(raw_iid, len(global_iid_map))
+
+        gmd.build(id_map=global_iid_map)
+
+        self.assertEqual(len(gmd.map_rid), 7)
+        self.assertEqual(len(gmd.map_cid), 7)
+        self.assertEqual(len(gmd.val), 7)
+        self.assertEqual(gmd.matrix.shape, (7, 3))
+
+
+    def test_get_train_triplet(self):
+
+        data = Reader().read('./tests/graph_data.txt', sep=' ')
+        gmd = GraphModule(data=data)
+
+        global_iid_map = OrderedDict()
+        for raw_iid, raw_jid, val in data:
+            mapped_iid = global_iid_map.setdefault(raw_iid, len(global_iid_map))
+
+        gmd.build(id_map=global_iid_map)
+        rid, cid, val = gmd.get_train_triplet([0, 1, 2], [0, 1, 2])
+        self.assertEqual(len(rid), 3)
+        self.assertEqual(len(cid), 3)
+        self.assertEqual(len(val), 3)
+
+
 
 
 if __name__ == '__main__':
