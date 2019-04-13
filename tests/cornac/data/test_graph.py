4a9a5ac6477eea9e2a8916ba3812173842750bcf
Aghiles <salahaghiles@gmail.com>
2019-04-13 11:26:45 +0800

    Add test for build in graph module

--- tests/cornac/data/test_graph.py
+++ tests/cornac/data/test_graph.py
@@ -7,6 +7,7 @@
 import unittest
 from cornac.data import GraphModule
 from cornac.data import Reader
+from collections import OrderedDict
 
 
 class TestGraphModule(unittest.TestCase):
@@ -14,12 +15,29 @@ class TestGraphModule(unittest.TestCase):
     def test_init(self):
         data = Reader().read('./tests/graph_data.txt', sep=' ')
         gmd = GraphModule(data=data)
-        # gmd.build(global_id_map=None)
-        self.assertEqual(len(gmd.raw_data), 12)
+
+        self.assertEqual(len(gmd.raw_data), 7)
         self.assertEqual(len(gmd.map_rid), 0)
         self.assertEqual(len(gmd.map_cid), 0)
         self.assertEqual(len(gmd.val), 0)
         self.assertIsNone(gmd.matrix)
+
+    def test_build(self):
+        data = Reader().read('./tests/graph_data.txt', sep=' ')
+        gmd = GraphModule(data=data)
+
+        global_iid_map = OrderedDict()
+        for raw_iid, raw_jid, val in data:
+            mapped_iid = global_iid_map.setdefault(raw_iid, len(global_iid_map))
+
+        gmd.build(id_map=global_iid_map)
+
+        self.assertEqual(len(gmd.map_rid), 7)
+        self.assertEqual(len(gmd.map_cid), 7)
+        self.assertEqual(len(gmd.val), 7)
+        self.assertEqual(gmd.matrix.shape, (7, 3))
+
+
 
 
 if __name__ == '__main__':
