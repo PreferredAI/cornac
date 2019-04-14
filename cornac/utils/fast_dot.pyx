49a4fdb88cf37f9dfef520cfd811f5eb4b8fd769
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2019-04-15 03:32:27 +0800

    Update fast_dot

--- cornac/utils/fast_dot.pyx
+++ cornac/utils/fast_dot.pyx
@@ -13,6 +13,6 @@ from cython.parallel import prange
 @cython.wraparound(False)
 cpdef void fast_dot(float[:] vec, float[:, :] mat, float[:] output) nogil:
     cdef size_t i, j, d0 = mat.shape[0], d1 = mat.shape[1]
-    for i in prange(d0):
-        for j in prange(d1):
+    for i in prange(d0, schedule='guided'):
+        for j in range(d1):
             output[i] += vec[j] * mat[i, j]
