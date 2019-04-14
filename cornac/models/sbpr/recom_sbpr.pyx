da23bfaab316428ab81679935cac6043dcdc3722
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-04-15 03:15:16 +0800

    Add Social Bayesian Personalized Ranking (SBPR) model (#133)

--- cornac/models/sbpr/recom_sbpr.pyx
+++ cornac/models/sbpr/recom_sbpr.pyx
@@ -8,44 +8,25 @@
 cimport cython
 from cython cimport floating, integral
 from cython.parallel import parallel, prange
-from libc.math cimport exp
-from libcpp cimport bool
-from libcpp.algorithm cimport binary_search
+from libc.math cimport exp, floor
 
 import numpy as np
 cimport numpy as np
+from scipy.sparse import csr_matrix
 
 from ..recommender import Recommender
 from ...exception import ScoreException
 from ...utils import fast_dot
 from ...utils.common import scale
+from ..bpr.recom_bpr cimport RNGVector, has_non_zero
 
 
-cdef extern from "recom_bpr.h" namespace "recom_bpr" nogil:
+cdef extern from "../bpr/recom_bpr.h" namespace "recom_bpr" nogil:
     cdef int get_thread_num()
 
 
-@cython.boundscheck(False)
-cdef bool has_non_zero(integral[:] indptr, integral[:] indices,
-                       integral rowid, integral colid) nogil:
-    """Given a CSR matrix, returns whether the [rowid, colid] contains a non zero.
-    Assumes the CSR matrix has sorted indices"""
-    return binary_search(&indices[indptr[rowid]], &indices[indptr[rowid + 1]], colid)
-
-
-cdef class RNGVector(object):
-    def __init__(self, int num_threads, long rows):
-        for i in range(num_threads):
-            self.rng.push_back(mt19937(np.random.randint(2 ** 31)))
-            self.dist.push_back(uniform_int_distribution[long](0, rows))
-
-    cdef inline long generate(self, int thread_id) nogil:
-        return self.dist[thread_id](self.rng[thread_id])
-
-
-
-class BPR(Recommender):
-    """Bayesian Personalized Ranking.
+class SBPR(Recommender):
+    """Social Bayesian Personalized Ranking.
 
     Parameters
     ----------
@@ -79,17 +60,20 @@ class BPR(Recommender):
 
     References
     ----------
-    * Rendle, Steffen, Christoph Freudenthaler, Zeno Gantner, and Lars Schmidt-Thieme. \
-    BPR: Bayesian personalized ranking from implicit feedback. In UAI, pp. 452-461. 2009.
+    * Zhao, T., McAuley, J., & King, I. (2014, November). Leveraging social connections to improve \
+    personalized ranking for collaborative filtering. CIKM 2014 (pp. 261-270).
     """
 
-    def __init__(self, name='BPR', k=10, max_iter=100, learning_rate=0.001, lambda_reg=0.01,
+    def __init__(self, name='SBPR', k=10, max_iter=100, learning_rate=0.001,
+                 lambda_u=0.01, lambda_v=0.01, lambda_b=0.01,
                  num_threads=0, trainable=True, verbose=False, init_params=None, seed=None):
         super().__init__(name=name, trainable=trainable, verbose=verbose)
         self.k = k
         self.max_iter = max_iter
         self.learning_rate = learning_rate
-        self.lambda_reg = lambda_reg
+        self.lambda_u = lambda_u
+        self.lambda_v = lambda_v
+        self.lambda_b = lambda_b
         self.init_params = {} if init_params is None else init_params
         self.seed = seed
 
@@ -111,11 +95,11 @@ class BPR(Recommender):
         """
         Recommender.fit(self, train_set)
 
+        n_users, n_items = train_set.num_users, train_set.num_items
+
         from tqdm import trange
         from ...utils import get_rng
         from ...utils.init_utils import zeros, uniform
-
-        n_users, n_items = train_set.num_users, train_set.num_items
 
         rng = get_rng(self.seed)
         self.u_factors = self.init_params.get('U', (uniform((n_users, self.k), random_state=rng) - 0.5) / self.k)
@@ -125,24 +109,47 @@ class BPR(Recommender):
         if not self.trainable:
             return
 
+        # construct implicit feedback
         X = train_set.matrix # csr_matrix
         # this basically calculates the 'row' attribute of a COO matrix
         # without requiring us to get the whole COO matrix
         user_counts = np.ediff1d(X.indptr)
         user_ids = np.repeat(np.arange(n_users), user_counts).astype(X.indices.dtype)
 
+        # construct social feedback
+        (rid, cid, val) = train_set.user_graph.get_train_triplet(train_set.uid_list,
+                                                                     train_set.uid_list)
+        Y = csr_matrix((val, (rid, cid)), shape=(n_users, n_users))
+        social_item_ids = []
+        social_item_counts = []
+        social_indptr = [0]
+        for uid in trange(n_users, disable=not self.verbose, desc='Social'):
+            real_pos_items = np.unique(X[uid].indices)
+            social_pos_items, counts = np.unique(X[Y[uid].indices].indices,
+                                                     return_counts=True)
+            mask = np.in1d(social_pos_items, real_pos_items, assume_unique=True)
+            social_item_ids.extend(social_pos_items[~mask])
+            social_item_counts.extend(counts[~mask])
+            social_indptr.append(len(social_item_ids))
+
+        social_item_ids = np.asarray(social_item_ids).astype(X.indices.dtype)
+        social_item_counts = np.asarray(social_item_counts).astype(X.indices.dtype)
+        social_indptr = np.asarray(social_indptr).astype(X.indices.dtype)
+
+        # construct random generators
         cdef:
             int num_threads = self.num_threads
             RNGVector rng_pos = RNGVector(num_threads, len(user_ids) - 1)
             RNGVector rng_neg = RNGVector(num_threads, n_items - 1)
 
+        # start training
         with trange(self.max_iter, disable=not self.verbose) as progress:
             for epoch in progress:
-                correct, skipped = self._fit_sgd(rng_pos, rng_neg, num_threads,
+                skipped = self._fit_sgd(rng_pos, rng_neg, num_threads,
                                                  user_ids, X.indices, X.indptr,
+                                                 social_item_ids, social_item_counts, social_indptr,
                                                  self.u_factors, self.i_factors, self.i_biases)
-                progress.set_postfix({"correct": "%.2f%%" % (100.0 * correct / (len(user_ids) - skipped)),
-                                      "skipped": "%.2f%%" % (100.0 * skipped / n_items)})
+                progress.set_postfix({"skipped": "%.2f%%" % (100.0 * skipped / len(user_ids))})
         if self.verbose:
             print('Optimization finished!')
 
@@ -151,61 +158,104 @@ class BPR(Recommender):
     @cython.wraparound(False)
     def _fit_sgd(self, RNGVector rng_pos, RNGVector rng_neg, int num_threads,
                  integral[:] user_ids, integral[:] item_ids, integral[:] indptr,
+                 integral[:] social_item_ids, integral[:] social_item_counts, integral[:] social_indptr,
                  floating[:, :] U, floating[:, :] V, floating[:] B):
         """Fit the model parameters (U, V, B) with SGD
         """
         cdef:
-            long num_samples = len(user_ids), s, i_index, j_index, correct = 0, skipped = 0
+            long num_samples = len(user_ids)
             long num_items = self.train_set.num_items
-            integral f, i_id, j_id, thread_id
-            floating z, score, temp
+            long s, i_index, k_index, skipped = 0
+            int f, u_id, i_id, j_id, k_id, n_social_items, thread_id
+            floating u_temp, k_rand
+            floating z, score # for BPR formula
+            floating z_ik, z_kj, score_ik, score_kj, s_uk # for SBPR-2 formula
 
             floating lr = self.learning_rate
-            floating reg = self.lambda_reg
+            floating lbd_u = self.lambda_u
+            floating lbd_v = self.lambda_v
+            floating lbd_b = self.lambda_b
             int factors = self.k
 
             floating * user
             floating * item_i
             floating * item_j
+            floating * item_k
 
         with nogil, parallel(num_threads=num_threads):
             thread_id = get_thread_num()
 
             for s in prange(num_samples, schedule='guided'):
                 i_index = rng_pos.generate(thread_id)
+                u_id = user_ids[i_index]
                 i_id = item_ids[i_index]
                 j_id = rng_neg.generate(thread_id)
 
-                # if the user has liked the item j, skip this for now
-                if has_non_zero(indptr, item_ids, user_ids[i_index], j_id):
+                # sample social item k_id for given user u_id
+                n_social_items = social_indptr[u_id + 1] - social_indptr[u_id]
+                k_rand = <float>rng_neg.generate(thread_id) / num_items # uniform between [0.0, 1.0)
+                k_index = social_indptr[u_id] + <int>floor(k_rand * n_social_items)
+                k_id = social_item_ids[k_index]
+
+                # if the user has liked the item j,
+                # else if item j is also a social item,
+                # skip this for now
+                if has_non_zero(indptr, item_ids, u_id, j_id) or (j_id == k_id):
                     skipped += 1
                     continue
 
                 # get pointers to the relevant factors
-                user, item_i, item_j = &U[user_ids[i_index], 0], &V[i_id, 0], &V[j_id, 0]
+                user = &U[u_id, 0]
+                item_i, item_j, item_k = &V[i_id, 0], &V[j_id, 0], &V[k_id, 0]
 
-                # compute the score
-                score = B[i_id] - B[j_id]
+                # if no social item for given user uid, update factors based on BPR formula
+                if n_social_items == 0:
+                    # compute the score
+                    score = B[i_id] - B[j_id]
+                    for f in range(factors):
+                        score = score + user[f] * (item_i[f] - item_j[f])
+                    z = 1.0 / (1.0 + exp(score))
+
+                    # update the factors via sgd.
+                    for f in range(factors):
+                        u_temp = user[f]
+                        user[f] += lr * (z * (item_i[f] - item_j[f]) - lbd_u* user[f])
+                        item_i[f] += lr * (z * u_temp - lbd_v * item_i[f])
+                        item_j[f] += lr * (-z * u_temp - lbd_v * item_j[f])
+
+                    # update item biases
+                    B[i_id] += lr * (z - lbd_b * B[i_id])
+                    B[j_id] += lr * (-z - lbd_b * B[j_id])
+
+                    continue
+
+                # found social feedback, update factors based on SBPR-2 formula
+                # compute the scores
+                score_ik = B[i_id] - B[k_id]
+                score_kj = B[k_id] - B[j_id]
                 for f in range(factors):
-                    score = score + user[f] * (item_i[f] - item_j[f])
-                z = 1.0 / (1.0 + exp(score))
-
-                if z < .5:
-                    correct += 1
+                    score_ik = score_ik + user[f] * (item_i[f] - item_k[f])
+                    score_kj = score_kj + user[f] * (item_k[f] - item_j[f])
+                s_uk = 1.0 / (1.0 + social_item_counts[k_index])
+                z_ik = 1.0 / (1.0 + exp(score_ik * s_uk))
+                z_kj = 1.0 / (1.0 + exp(score_kj))
 
                 # update the factors via sgd.
                 for f in range(factors):
-                    temp = user[f]
-                    user[f] += lr * (z * (item_i[f] - item_j[f]) - reg * user[f])
-                    item_i[f] += lr * (z * temp - reg * item_i[f])
-                    item_j[f] += lr * (-z * temp - reg * item_j[f])
+                    u_temp = user[f]
+                    user[f] += lr * (z_ik * (item_i[f] - item_k[f]) * s_uk +
+                                         z_kj * (item_k[f] - item_j[f]) -
+                                         lbd_u * user[f])
+                    item_i[f] += lr * (z_ik * u_temp * s_uk - lbd_v * item_i[f])
+                    item_j[f] += lr * (-z_kj * u_temp - lbd_v * item_j[f])
+                    item_k[f] += lr * (z_kj * u_temp - z_ik * u_temp * s_uk - lbd_v * item_k[f])
 
                 # update item biases
-                B[i_id] += lr * (z - reg * B[i_id])
-                B[j_id] += lr * (-z - reg * B[j_id])
+                B[i_id] += lr * (z_ik * s_uk - lbd_b * B[i_id])
+                B[j_id] += lr * (-z_kj - lbd_b * B[j_id])
+                B[k_id] += lr * (z_kj - z_ik * s_uk - lbd_b * B[j_id])
 
-        return correct, skipped
-
+        return skipped
 
     def score(self, user_id, item_id=None):
         """Predict the scores/ratings of a user for an item.
