c06d3d619ac7e48c3f923a1a73fa57ca3de63353
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-05-22 15:23:00 +0800

    Update CVAE loss weighting (#155)

--- examples/cvae_example.py
+++ examples/cvae_example.py
@@ -24,8 +24,10 @@ ratio_split = RatioSplit(data=data, test_size=0.2, exclude_unknowns=True,
                          rating_threshold=0.5, verbose=True, seed=123,
                          item_text=item_text_module)
 
-cvae = cornac.models.CVAE(z_dim=50, lr=0.001, batch_size=128,
-                          input_dim=8000, seed=123, verbose=True)
+cvae = cornac.models.CVAE(z_dim=50, vae_layers=[200, 100], act_fn='sigmoid',
+                          input_dim=8000, lr=0.001, batch_size=128, n_epochs=100,
+                          lambda_u=1e-4, lambda_v=0.001, lambda_r=10, lambda_w=1e-4,
+                          seed=123, verbose=True)
 
 rec_300 = cornac.metrics.Recall(k=300)
 
