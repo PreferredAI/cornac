3a91762540a379f30c5c4a3ca5ad131d6dd69d86
Aghiles <salahaghiles@gmail.com>
2019-06-14 14:07:02 +0800

    Change recom_vaecf

--- cornac/models/vaecf/recom_vaecf.py
+++ cornac/models/vaecf/recom_vaecf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-@author: Aghiles Salah
+@author: Aghiles Salah <asalah@smu.edu.sg>
 """
 
 import numpy as np
@@ -25,6 +25,9 @@ class VAECF(Recommender):
 
     n_epochs: int, optional, default: 100
         The number of epochs for SGD.
+
+    batch_size: int, optional, default: 100
+        The batch size.
 
     learning_rate: float, optional, default: 0.001
         The learning rate for SGD_RMSProp.
@@ -54,20 +57,17 @@ class VAECF(Recommender):
 	In Proceedings of the 2018 World Wide Web Conference on World Wide Web, pp. 689-698.
     """
 
-    def __init__(self, name="PVAE", k=10, h_dim=20, n_epochs=100, batch_size=100, learning_rate=0.001, gamma=0.9,
-                 trainable=True, verbose=False, use_gpu=False, init_params={}, seed=None):
+    def __init__(self, name="VAECF", k=10, h=20, n_epochs=100, batch_size=100, learning_rate=0.001, gamma=0.9,
+                 trainable=True, verbose=False, seed=None, use_gpu=False):
         Recommender.__init__(self, name=name, trainable=trainable, verbose=verbose)
         self.k = k
-        self.h_dim = h_dim
+        self.h = h
         self.batch_size = batch_size
-        self.init_params = init_params
         self.n_epochs = n_epochs
         self.learning_rate = learning_rate
         self.gamma = gamma
-        self.use_gpu = use_gpu
-        self.loss = np.full(n_epochs, 0)
-        self.eps = 1e-10
         self.seed = seed
+        self.use_gpu = use_gpu
 
     # fit the recommender model to the traning data
     def fit(self, train_set):
@@ -76,7 +76,7 @@ class VAECF(Recommender):
         Parameters
         ----------
         train_set: object of type TrainSet, required
-            An object contraining the user-item preference in csr scipy sparse format,\
+            An object containing the user-item preference in csr scipy sparse format,\
             as well as some useful attributes such as mappings to the original user/item ids.\
             Please refer to the class TrainSet in the "data" module for details.
         """
@@ -88,21 +88,13 @@ class VAECF(Recommender):
             if self.verbose:
                 print('Learning...')
 
-            from .pvae import learn
+            from .vaecf import learn
             
-            self.features = train_set.user_graph.matrix[:self.train_set.num_users, :self.train_set.num_users]
-            
-            #self.features = torch.tensor(self.features.A,dtype=torch.double, requires_grad=False)
-            #self.features = torch.randn_like(self.features)*1.
-            
-            res = learn(self.train_set, self.features, k=self.k, h_dim=self.h_dim, n_epochs=self.n_epochs,
+            res = learn(self.train_set, k=self.k, h_dim=self.h_dim, n_epochs=self.n_epochs,
                       batch_size=self.batch_size, learn_rate=self.learning_rate, gamma=self.gamma,
                       init_params=self.init_params, use_gpu=self.use_gpu, verbose=self.verbose, seed=self.seed)
 
-            self.vae, self.disc, self.kl_vals, self.f_vals = res
-
-            z_u, _ = self.vae.encode(torch.tensor(self.train_set.matrix.A, dtype=torch.double))
-            self.theta = z_u.data
+            self.vae = res
 
             if self.verbose:
                 print('Learning completed')
@@ -130,20 +122,17 @@ class VAECF(Recommender):
         if item_id is None:
             if self.train_set.is_unk_user(user_id):
                 raise ScoreException("Can't make score prediction for (user_id=%d)" % user_id)
-            r_u = self.train_set.matrix[user_id].copy()
-            r_u.data = np.ones(len(r_u.data))
+            x_u = self.train_set.matrix[user_id].copy()
+            x_u.data = np.ones(len(r_u.data))
             z_u, _ = self.vae.encode(torch.tensor(r_u.A,dtype=torch.double))
             known_item_scores = self.vae.decode(z_u).data.cpu().numpy().flatten()
-            # known_item_scores = np.asarray(np.zeros(self.V.shape[0]),dtype='float32')
-            # fast_dot(self.U[user_id], self.V, known_item_scores)
-            # fast_dot(self.P[user_id], self.Q, known_item_scores)
             return known_item_scores
         else:
             if self.train_set.is_unk_user(user_id) or self.train_set.is_unk_item(item_id):
                 raise ScoreException("Can't make score prediction for (user_id=%d, item_id=%d)" % (user_id, item_id))
-            user_pred = self.V[item_id, :].dot(self.U[user_id, :]) + self.Q[item_id, :].dot(self.P[user_id, :])
-            user_pred = sigmoid(user_pred)
-
-            user_pred = scale(user_pred, self.train_set.min_rating, self.train_set.max_rating, 0., 1.)
+            x_u = self.train_set.matrix[user_id].copy()
+            x_u.data = np.ones(len(r_u.data))
+            z_u, _ = self.vae.encode(torch.tensor(r_u.A,dtype=torch.double))
+            user_pred = self.vae.decode(z_u).data.cpu().numpy().flatten()[item_id] # Fix me I am not efficient
 
             return user_pred
