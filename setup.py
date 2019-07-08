3a9bcaae00c206ffecef1714c65bd573c67c50c9
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-07-08 23:02:29 +0700

    Move author list to a separated file (#173)

--- setup.py
+++ setup.py
@@ -154,8 +154,6 @@ if USE_CYTHON:
 setup(
     name='cornac',
     version='0.3.0',
-    author='Aghiles Salah, Quoc-Tuan Truong',
-    author_email='asalah@smu.edu.sg, tuantq.vnu@gmail.com',
     description='A Python library for using, building, evaluating and comparing recommendation algorithms',
     long_description=long_description,
     long_description_content_type='text/markdown',
