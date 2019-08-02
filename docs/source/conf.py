502b6598ec3234a76b6d2eb550c11f606fd7033a
tqtg <tuantq.vnu@gmail.com>
2019-08-02 20:41:17 +0800

    Release v0.3.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Preferred.AI'
 # The short X.Y version
 version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.3.2'
 
 
 # -- General configuration ---------------------------------------------------
