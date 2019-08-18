278b9f88b172bf53f58e34b970be8fdc261c9fe0
lthoang <lthoang@apcs.vn>
2019-08-18 23:24:06 +0800

    update example with new setting

--- examples/efm_example.py
+++ examples/efm_example.py
@@ -28,7 +28,7 @@ split_data = RatioSplit(data=rating,
                         exclude_unknowns=True, verbose=True,
                         sentiment=md, seed=123)
 
-efm = cornac.models.EFM(num_explicit_factors=50, num_latent_factors=50, num_most_cared_aspects=15,
+efm = cornac.models.EFM(num_explicit_factors=40, num_latent_factors=60, num_most_cared_aspects=15,
                         rating_scale=5.0, alpha=0.85,
                         lambda_x=1, lambda_y=1, lambda_u=0.01, lambda_h=0.01, lambda_v=0.01,
                         max_iter=100, num_threads=1,
