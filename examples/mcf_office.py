a451387de64e63f94f3affc0649e1cebae8e4dde
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-08-22 17:10:23 +0800

    Remove shuffle option in RatioSplit (#209)

--- examples/mcf_office.py
+++ examples/mcf_office.py
@@ -29,8 +29,8 @@ item_graph_modality = GraphModality(data=contexts)
 
 ratio_split = RatioSplit(data=ratings,
                          test_size=0.2, rating_threshold=3.5,
-                         shuffle=True, exclude_unknowns=True,
-                         verbose=True, item_graph=item_graph_modality)
+                         exclude_unknowns=True, verbose=True,
+                         item_graph=item_graph_modality)
 
 mcf = MCF(k=10, max_iter=40, learning_rate=0.001, verbose=True)
 
