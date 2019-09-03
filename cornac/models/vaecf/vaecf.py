b19a5a57a71e0db9be6aede03cdaa1b171b7074c
Aghiles <salahaghiles@gmail.com>
2019-09-03 19:40:45 +0800

    remove useless parameter gamma

--- cornac/models/vaecf/vaecf.py
+++ cornac/models/vaecf/vaecf.py
@@ -81,7 +81,7 @@ class VAE(nn.Module):
         return torch.mean(beta * kld - ll)
 
 
-def learn(train_set, k, h, n_epochs, batch_size, learn_rate, beta, gamma, verbose, seed, use_gpu):
+def learn(train_set, k, h, n_epochs, batch_size, learn_rate, beta, verbose, seed, use_gpu):
     if seed is not None:
         torch.manual_seed(seed)
 
