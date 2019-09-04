69ab5bb04fcb03bec1fd9b03324d562a03e1488b
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2019-09-04 17:10:09 +0800

    Add Neural Matrix Factorization (NeuMF) model

---
+++ cornac/models/neu_cf/recom_neumf.py
@@ -0,0 +1,208 @@
+# Copyright 2018 The Cornac Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+import numpy as np
+
+from ..recommender import Recommender
+from ...exception import ScoreException
+
+
+class NeuMF(Recommender):
+    """Neural Matrix Factorization.
+
+    Parameters
+    ----------
+    num_factors: int, optional, default: 8
+        Embedding size of MF model.
+
+    layers: list, optional, default: [64, 32, 16, 8]
+        MLP layers. Note that the first layer is the concatenation of
+        user and item embeddings. So layers[0]/2 is the embedding size.
+
+    act_fn: str, default: 'relu'
+        Name of the activation function used for the MLP layers.
+        Supported functions: ['sigmoid', 'tanh', 'elu', 'relu', 'selu, 'relu6', 'leaky_relu']
+
+    reg_mf: float, optional, default: 0.
+        Regularization for MF embeddings.
+
+    reg_layers: list, optional, default: [0., 0., 0., 0.]
+        Regularization for each MLP layer,
+        reg_layers[0] is the regularization for embeddings.
+
+    num_epochs: int, optional, default: 20
+        Number of epochs.
+
+    batch_size: int, optional, default: 256
+        Batch size.
+
+    num_neg: int, optional, default: 4
+        Number of negative instances to pair with a positive instance.
+
+    lr: float, optional, default: 0.001
+        Learning rate.
+
+    learner: str, optional, default: 'adam'
+        Specify an optimizer: adagrad, adam, rmsprop, sgd
+
+    name: string, optional, default: 'NeuMF'
+        Name of the recommender model.
+
+    trainable: boolean, optional, default: True
+        When False, the model is not trained and Cornac assumes that the model is already \
+        pre-trained.
+
+    verbose: boolean, optional, default: False
+        When True, some running logs are displayed.
+
+    seed: int, optional, default: None
+        Random seed for parameters initialization.
+
+    References
+    ----------
+    * He, X., Liao, L., Zhang, H., Nie, L., Hu, X., & Chua, T. S. (2017, April). Neural collaborative filtering. \
+    In Proceedings of the 26th international conference on world wide web (pp. 173-182).
+    """
+
+    def __init__(self, name='NeuMF',
+                 num_factors=8, layers=[64, 32, 16, 8], act_fn='relu',
+                 reg_mf=0., reg_layers=[0., 0., 0., 0.], num_epochs=20,
+                 batch_size=256, num_neg=4, lr=0.001, learner='adam',
+                 trainable=True, verbose=True, seed=None):
+        super().__init__(name=name, trainable=trainable, verbose=verbose)
+
+        self.num_factors = num_factors
+        self.layers = layers
+        self.act_fn = act_fn
+        self.reg_mf = reg_mf
+        self.reg_layers = reg_layers
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.num_neg = num_neg
+        self.learning_rate = lr
+        self.learner = learner
+        self.seed = seed
+
+    def fit(self, train_set):
+        """Fit the model to observations.
+
+        Parameters
+        ----------
+        train_set: object of type TrainSet, required
+            An object containing the user-item preference in csr scipy sparse format,\
+            as well as some useful attributes such as mappings to the original user/item ids.\
+            Please refer to the class TrainSet in the "data" module for details.
+        """
+        Recommender.fit(self, train_set)
+
+        if self.trainable:
+            self._fit_neumf()
+
+    def _fit_neumf(self):
+        import os
+        import tensorflow as tf
+        from tqdm import trange
+
+        np.random.seed(self.seed)
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+        tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+        from .ops import gmf, mlp, loss_fn, train_fn
+
+        graph = tf.Graph()
+        with graph.as_default():
+            tf.set_random_seed(self.seed)
+
+            self.gmf_user_id = tf.placeholder(shape=[None, ], dtype=tf.int32, name='gmf_user_id')
+            self.mlp_user_id = tf.placeholder(shape=[None, ], dtype=tf.int32, name='mlp_user_id')
+            self.item_id = tf.placeholder(shape=[None, ], dtype=tf.int32, name='item_id')
+            self.labels = tf.placeholder(shape=[None, 1], dtype=tf.float32, name='labels')
+
+            gmf_feat = gmf(uid=self.gmf_user_id, iid=self.item_id, num_users=self.train_set.num_users,
+                           num_items=self.train_set.num_items, emb_size=self.num_factors,
+                           reg_user=self.reg_mf, reg_item=self.reg_mf, seed=self.seed)
+            mlp_feat = mlp(uid=self.mlp_user_id, iid=self.item_id, num_users=self.train_set.num_users,
+                           num_items=self.train_set.num_items, layers=self.layers,
+                           reg_layers=self.reg_layers, act_fn=self.act_fn, seed=self.seed)
+
+            interaction = tf.concat([gmf_feat, mlp_feat], axis=-1)
+            logits = tf.layers.dense(interaction, units=1, name='logits',
+                                     kernel_initializer=tf.initializers.lecun_uniform(self.seed))
+            self.prediction = tf.nn.sigmoid(logits)
+
+            loss = loss_fn(labels=self.labels, logits=logits)
+            train_op = train_fn(loss, learning_rate=self.learning_rate, learner=self.learner)
+
+            initializer = tf.global_variables_initializer()
+
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(graph=graph, config=config)
+        self.sess.run(initializer)
+
+        loop = trange(self.num_epochs, disable=not self.verbose)
+        for _ in loop:
+            count = 0
+            sum_loss = 0
+            for i, (batch_users, batch_items, batch_ratings) in enumerate(
+                    self.train_set.uir_iter(self.batch_size, shuffle=True, num_zeros=self.num_neg)):
+                _, _loss = self.sess.run([train_op, loss],
+                                         feed_dict={
+                                             self.gmf_user_id: batch_users,
+                                             self.mlp_user_id: batch_users,
+                                             self.item_id: batch_items,
+                                             self.labels: batch_ratings.reshape(-1, 1)
+                                         })
+
+                count += len(batch_ratings)
+                sum_loss += _loss
+                if i % 10 == 0:
+                    loop.set_postfix(loss=(sum_loss / count))
+
+    def score(self, user_id, item_id=None):
+        """Predict the scores/ratings of a user for an item.
+
+        Parameters
+        ----------
+        user_id: int, required
+            The index of the user for whom to perform score prediction.
+
+        item_id: int, optional, default: None
+            The index of the item for that to perform score prediction.
+            If None, scores for all known items will be returned.
+
+        Returns
+        -------
+        res : A scalar or a Numpy array
+            Relative scores that the user gives to the item or to all known items
+        """
+        if item_id is None:
+            if self.train_set.is_unk_user(user_id):
+                raise ScoreException("Can't make score prediction for (user_id=%d)" % user_id)
+
+            known_item_scores = self.sess.run(self.prediction, feed_dict={
+                self.gmf_user_id: [user_id],
+                self.mlp_user_id: np.ones(self.train_set.num_items) * user_id,
+                self.item_id: np.arange(self.train_set.num_items)
+            })
+            return known_item_scores.ravel()
+        else:
+            if self.train_set.is_unk_user(user_id) or self.train_set.is_unk_item(item_id):
+                raise ScoreException("Can't make score prediction for (user_id=%d, item_id=%d)" % (user_id, item_id))
+
+            user_pred = self.sess.run(self.prediction, feed_dict={
+                self.gmf_user_id: [user_id], self.mlp_user_id: [user_id], self.item_id: [item_id]
+            })
+            return user_pred.ravel()
