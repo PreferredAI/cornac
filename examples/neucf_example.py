02ea14b21052845421afd9074c48e0c489a11aa0
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-09-04 18:09:46 +0800

    Add Generalized Matrix Factorization model (#216)

--- examples/neucf_example.py
+++ examples/neucf_example.py
@@ -23,12 +23,14 @@ ratio_split = RatioSplit(data=amazon_clothing.load_rating(Reader(bin_threshold=1
                          test_size=0.2, rating_threshold=1.0, seed=123,
                          exclude_unknowns=True, verbose=True)
 
-neumf = cornac.models.NeuMF(num_factors=8, layers=[16, 8], act_fn='tanh', learner='adam',
-                            num_epochs=10, batch_size=256, lr=0.001, seed=123)
+gmf = cornac.models.GMF(num_factors=8, num_epochs=20, learner='adam',
+                        batch_size=256, lr=0.001, num_neg=10, seed=123)
+neumf = cornac.models.NeuMF(num_factors=8, layers=[64, 32, 16, 8], act_fn='tanh', learner='adam',
+                            num_epochs=20, batch_size=256, lr=0.001, num_neg=10, seed=123)
 
 ndcg_50 = cornac.metrics.NDCG(k=50)
 rec_50 = cornac.metrics.Recall(k=50)
 
 cornac.Experiment(eval_method=ratio_split,
-                  models=[neumf],
+                  models=[gmf, neumf],
                   metrics=[ndcg_50, rec_50]).run()
