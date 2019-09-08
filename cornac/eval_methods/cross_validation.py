8899798fe689525af65bc9d07c82183fcb6bb13b
Quoc-Tuan Truong <tqtg@users.noreply.github.com>
2019-09-08 17:59:45 +0800

    Check duplicates within each Dataset (#224)

--- cornac/eval_methods/cross_validation.py
+++ cornac/eval_methods/cross_validation.py
@@ -102,8 +102,7 @@ class CrossValidation(BaseMethod):
 
         train_data = safe_indexing(self._data, train_idx)
         test_data = safe_indexing(self._data, test_idx)
-        self.build(train_data=train_data, test_data=test_data)
-        self.val_set = self.test_set
+        self.build(train_data=train_data, test_data=test_data, val_data=test_data)
 
         if self.verbose:
             print('---')
