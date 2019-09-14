edd4795776979c22ad4e3cd85b2f9d5745c069a8
tqtg <tuantq.vnu@gmail.com>
2019-09-14 12:23:01 +0800

    Release v1.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Cornac Authors'
 author = 'Preferred.AI'
 
 # The short X.Y version
-version = '1.0'
+version = '1.1'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
