f6fe7cf0a1e939b2c5cc6ad28ec2de8aeb792e3b
tqtg <tuantq.vnu@gmail.com>
2019-09-18 22:34:41 +0800

    Release v1.1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Preferred.AI'
 # The short X.Y version
 version = '1.1'
 # The full version, including alpha/beta/rc tags
-release = '1.1.1'
+release = '1.1.2'
 
 
 # -- General configuration ---------------------------------------------------
