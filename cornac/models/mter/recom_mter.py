c923dc6188572206f6edeaa928a8c27835698ad7
lthoang <lthoang@apcs.vn>
2019-10-10 19:27:57 +0800

    Add init_params for MTER

--- cornac/models/mter/recom_mter.py
+++ cornac/models/mter/recom_mter.py
@@ -163,9 +163,6 @@ class MTER(Recommender):
 
         Recommender.fit(self, train_set, val_set)
 
-        if not self.trainable:
-            return
-
         from ...utils import get_rng
         from ...utils.init_utils import uniform
         rng = get_rng(self.seed)
@@ -185,20 +182,27 @@ class MTER(Recommender):
         G3_shape = (self.n_item_factors, self.n_aspect_factors,
                     self.n_opinion_factors)
 
-        mp_U = mp.Array(c.c_double, uniform(
-            np.product(U_shape), random_state=rng))
-        mp_I = mp.Array(c.c_double, uniform(
-            np.product(I_shape), random_state=rng))
-        mp_A = mp.Array(c.c_double, uniform(
-            np.product(A_shape), random_state=rng))
-        mp_O = mp.Array(c.c_double, uniform(
-            np.product(O_shape), random_state=rng))
-        mp_G1 = mp.Array(c.c_double, uniform(
-            np.product(G1_shape), random_state=rng))
-        mp_G2 = mp.Array(c.c_double, uniform(
-            np.product(G2_shape), random_state=rng))
-        mp_G3 = mp.Array(c.c_double, uniform(
-            np.product(G3_shape), random_state=rng))
+        mp_U = mp.Array(
+            c.c_double, self.init_params.get(
+                'U', uniform(np.product(U_shape), random_state=rng)).flatten())
+        mp_I = mp.Array(
+            c.c_double, self.init_params.get(
+                'I', uniform(np.product(I_shape), random_state=rng)).flatten())
+        mp_A = mp.Array(
+            c.c_double, self.init_params.get(
+                'A', uniform(np.product(A_shape), random_state=rng)).flatten())
+        mp_O = mp.Array(
+            c.c_double, self.init_params.get(
+                'O', uniform(np.product(O_shape), random_state=rng)).flatten())
+        mp_G1 = mp.Array(
+            c.c_double, self.init_params.get(
+                'G1', uniform(np.product(G1_shape), random_state=rng)).flatten())
+        mp_G2 = mp.Array(
+            c.c_double, self.init_params.get(
+                'G2', uniform(np.product(G2_shape), random_state=rng)).flatten())
+        mp_G3 = mp.Array(
+            c.c_double, self.init_params.get(
+                'G3', uniform(np.product(G3_shape), random_state=rng)).flatten())
 
         self.G1 = np.frombuffer(mp_G1.get_obj()).reshape(G1_shape)
         self.G2 = np.frombuffer(mp_G2.get_obj()).reshape(G2_shape)
@@ -208,36 +212,37 @@ class MTER(Recommender):
         self.A = np.frombuffer(mp_A.get_obj()).reshape(A_shape)
         self.O = np.frombuffer(mp_O.get_obj()).reshape(O_shape)
 
-        mp_del_g1_arr = mp.Array(c.c_double, int(np.product(G1_shape)))
-        mp_del_g2_arr = mp.Array(c.c_double, int(np.product(G2_shape)))
-        mp_del_g3_arr = mp.Array(c.c_double, int(np.product(G3_shape)))
-        mp_del_u_arr = mp.Array(c.c_double, int(np.product(U_shape)))
-        mp_del_i_arr = mp.Array(c.c_double, int(np.product(I_shape)))
-        mp_del_a_arr = mp.Array(c.c_double, int(np.product(A_shape)))
-        mp_del_o_arr = mp.Array(c.c_double, int(np.product(O_shape)))
-
-        del_g1 = np.frombuffer(mp_del_g1_arr.get_obj()).reshape(G1_shape)
-        del_g2 = np.frombuffer(mp_del_g2_arr.get_obj()).reshape(G2_shape)
-        del_g3 = np.frombuffer(mp_del_g3_arr.get_obj()).reshape(G3_shape)
-        del_u = np.frombuffer(mp_del_u_arr.get_obj()).reshape(U_shape)
-        del_i = np.frombuffer(mp_del_i_arr.get_obj()).reshape(I_shape)
-        del_a = np.frombuffer(mp_del_a_arr.get_obj()).reshape(A_shape)
-        del_o = np.frombuffer(mp_del_o_arr.get_obj()).reshape(O_shape)
+        if not self.trainable:
+            return
 
         self.G1, self.G2, self.G3, self.U, self.I, self.A, self.O = self._fit_mter(
             self.n_epochs, self.n_threads, self.lr, self.n_element_samples, self.n_bpr_samples,
             self.lambda_bpr, self.lambda_reg,
             rating_matrix, user_item_aspect, user_aspect_opinion, item_aspect_opinion, user_item_pairs,
-            self.G1, self.G2, self.G3, self.U, self.I, self.A, self.O,
-            del_g1, del_g2, del_g3, del_u, del_i, del_a, del_o)
+            self.G1, self.G2, self.G3, self.U, self.I, self.A, self.O)
 
     def _fit_mter(self,
                   n_epochs, n_threads, lr, n_element_samples, n_bpr_samples,
                   lambda_bpr, lambda_reg,
                   rating_matrix, user_item_aspect, user_aspect_opinion, item_aspect_opinion, user_item_pairs,
-                  G1, G2, G3, U, I, A, O,
-                  del_g1, del_g2, del_g3, del_u, del_i, del_a, del_o):
+                  G1, G2, G3, U, I, A, O):
         from .mter import paraserver, grad_worker_mse, grad_worker_bpr
+
+        mp_del_g1_arr = mp.Array(c.c_double, int(np.product(G1.shape)))
+        mp_del_g2_arr = mp.Array(c.c_double, int(np.product(G2.shape)))
+        mp_del_g3_arr = mp.Array(c.c_double, int(np.product(G3.shape)))
+        mp_del_u_arr = mp.Array(c.c_double, int(np.product(U.shape)))
+        mp_del_i_arr = mp.Array(c.c_double, int(np.product(I.shape)))
+        mp_del_a_arr = mp.Array(c.c_double, int(np.product(A.shape)))
+        mp_del_o_arr = mp.Array(c.c_double, int(np.product(O.shape)))
+
+        del_g1 = np.frombuffer(mp_del_g1_arr.get_obj()).reshape(G1.shape)
+        del_g2 = np.frombuffer(mp_del_g2_arr.get_obj()).reshape(G2.shape)
+        del_g3 = np.frombuffer(mp_del_g3_arr.get_obj()).reshape(G3.shape)
+        del_u = np.frombuffer(mp_del_u_arr.get_obj()).reshape(U.shape)
+        del_i = np.frombuffer(mp_del_i_arr.get_obj()).reshape(I.shape)
+        del_a = np.frombuffer(mp_del_a_arr.get_obj()).reshape(A.shape)
+        del_o = np.frombuffer(mp_del_o_arr.get_obj()).reshape(O.shape)
 
         lock = mp.Lock()
         q_samples_mse = mp.Queue()
