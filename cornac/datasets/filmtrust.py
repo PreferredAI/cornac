f161f5df602b347692f1f2968342e305b0d8247f
Aghiles <salahaghiles@gmail.com>
2019-11-28 11:46:40 +0800

    Add access to filmtrust dataset

---
+++ cornac/datasets/filmtrust.py
@@ -0,0 +1,60 @@
+# Copyright 2018 The Cornac Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+Source: https://www.librec.net/datasets.html
+"""
+
+from typing import List
+
+from ..utils import cache
+from ..data import Reader
+
+
+def load_feedback(reader: Reader = None) -> List:
+    """Load the user-item ratings, scale: [0.5,4]
+
+    Parameters
+    ----------
+    reader: `obj:cornac.data.Reader`, default: None
+        Reader object used to read the data.
+
+    Returns
+    -------
+    data: array-like
+        Data in the form of a list of tuples (user, item, rating).
+    """
+    fpath = cache(url='https://www.librec.net/datasets/filmtrust.zip',
+                  unzip=True, relative_path='filmtrust/ratings.txt')
+    reader = Reader() if reader is None else reader
+    return reader.read(fpath, sep=' ')
+
+
+def load_user_trust(reader: Reader = None) -> List:
+    """Load the user-user trust information (undirected network)
+
+    Parameters
+    ----------
+    reader: `obj:cornac.data.Reader`, default: None
+        Reader object used to read the data.
+
+    Returns
+    -------
+    data: array-like
+        Data in the form of a list of tuples (user_id, user_id, 1).
+    """
+    fpath = cache(url='https://www.librec.net/datasets/filmtrust.zip',
+                  unzip=True, relative_path='filmtrust/trust.txt')
+    reader = Reader() if reader is None else reader
+    return reader.read(fpath, sep=' ')
