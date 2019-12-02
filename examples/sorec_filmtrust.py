6d90f868271de7212b608df4e97f09efd9f61980
Aghiles <salahaghiles@gmail.com>
2019-12-02 19:04:30 +0800

    Add SoRec example on FilmTrust

---
+++ examples/sorec_filmtrust.py
@@ -0,0 +1,58 @@
+# Copyright 2018 The Cornac Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Fit to and evaluate SoRec on the FilmTrust dataset"""
+
+from cornac.data import GraphModality
+from cornac.eval_methods import RatioSplit
+from cornac.experiment import Experiment
+from cornac import metrics
+from cornac.models import SoRec
+from cornac.datasets import filmtrust
+
+# Load filmtrust user feedback and trust
+ratings = filmtrust.load_feedback()
+trust = filmtrust.load_trust()
+
+# Instantiate a GraphModality with the trust information
+user_graph_modality = GraphModality(data=trust)
+
+# Instantiate a data splitting method
+ratio_split = RatioSplit(data=ratings,
+                         test_size=0.2, rating_threshold=2.5,
+                         exclude_unknowns=True, verbose=True,
+                         user_graph=user_graph_modality, seed=123)
+
+# Instantiate SoRec
+sorec = SoRec(k=10, max_iter=50, learning_rate=0.001, verbose=False, seed=123)
+
+# Evaluation metrics
+ndcg = metrics.NDCG(k=-1)
+rmse = metrics.RMSE()
+rec = metrics.Recall(k=20)
+pre = metrics.Precision(k=20)
+
+# Instantiate and run an experiment
+exp = Experiment(eval_method=ratio_split,
+                 models=[sorec],
+                 metrics=[rmse, ndcg, pre, rec])
+exp.run()
+
+"""
+Output:
+      |   RMSE | NDCG@-1 | Precision@20 | Recall@20 | Train (s) | Test (s)
+----- + ------ + ------- + ------------ + --------- + --------- + --------
+SoRec | 0.7574 |  0.3707 |       0.0756 |    0.3736 |    0.8198 |   0.8037
+
+"""
