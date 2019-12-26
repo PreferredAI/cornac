20f20ead1b3768c5efa406c2ce8c2784255b5395
Aghiles <salahaghiles@gmail.com>
2019-12-26 11:02:43 +0800

    change load_rating to load_feedback for office dataset

--- tests/cornac/datasets/test_amazon_office.py
+++ tests/cornac/datasets/test_amazon_office.py
@@ -25,7 +25,7 @@ class TestAmazonOffice(unittest.TestCase):
     def test_amazon_office(self):
         random.seed(time.time())
         if random.random() > 0.8:
-            ratings = office.load_rating()
+            ratings = office.load_feedback()
             contexts = office.load_context()
             self.assertEqual(len(ratings), 53282)
             self.assertEqual(len(contexts), 108466)
