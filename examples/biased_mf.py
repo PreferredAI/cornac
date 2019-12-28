e39d4e85bc17e4adba47c9bc1b8165f37e537dd7
Aghiles <salahaghiles@gmail.com>
2019-12-28 13:09:43 +0800

    Improve biased MF example with comments

--- examples/biased_mf.py
+++ examples/biased_mf.py
@@ -18,18 +18,25 @@ import cornac
 from cornac.datasets import movielens
 from cornac.eval_methods import RatioSplit
 
-ratio_split = RatioSplit(data=movielens.load_feedback(variant='1M'),
+# Load MovieLens 1M ratings
+ml_1m = movielens.load_feedback(variant='1M')
+
+# Define an evaluation method to split feedback into train and test sets
+ratio_split = RatioSplit(data=ml_1m,
                          test_size=0.2,
                          exclude_unknowns=False,
                          verbose=True)
 
+# Instantiate the global average baseline and MF model
 global_avg = cornac.models.GlobalAvg()
 mf = cornac.models.MF(k=10, max_iter=25, learning_rate=0.01, lambda_reg=0.02,
                       use_bias=True, early_stop=True, verbose=True)
 
+# Instantiate MAE and RMSE for evaluation
 mae = cornac.metrics.MAE()
 rmse = cornac.metrics.RMSE()
 
+# Put everything together into an experiment and run it
 exp = cornac.Experiment(eval_method=ratio_split,
                         models=[global_avg, mf],
                         metrics=[mae, rmse],
