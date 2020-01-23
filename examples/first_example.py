b6b091e26c7bc0c87304f5671682c55d99b02860
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2020-01-23 16:00:38 +0800

    Update first example

--- examples/first_example.py
+++ examples/first_example.py
@@ -14,29 +14,29 @@
 # ============================================================================
 """Your very first example with Cornac"""
 
-import cornac as cn
+import cornac
 
 # Load MovieLens 100K dataset
-ml_100k = cn.datasets.movielens.load_feedback()
+ml_100k = cornac.datasets.movielens.load_feedback()
 
 # Split data based on ratio
-ratio_split = cn.eval_methods.RatioSplit(data=ml_100k, test_size=0.2, rating_threshold=4.0, seed=123)
+rs = cornac.eval_methods.RatioSplit(data=ml_100k, test_size=0.2, rating_threshold=4.0, seed=123)
 
 # Here we are comparing biased MF, PMF, and BPR
-mf = cn.models.MF(k=10, max_iter=25, learning_rate=0.01, lambda_reg=0.02, use_bias=True, seed=123)
-pmf = cn.models.PMF(k=10, max_iter=100, learning_rate=0.001, lamda=0.001, seed=123)
-bpr = cn.models.BPR(k=10, max_iter=200, learning_rate=0.001, lambda_reg=0.01, seed=123)
+mf = cornac.models.MF(k=10, max_iter=25, learning_rate=0.01, lambda_reg=0.02, use_bias=True, seed=123)
+pmf = cornac.models.PMF(k=10, max_iter=100, learning_rate=0.001, lamda=0.001, seed=123)
+bpr = cornac.models.BPR(k=10, max_iter=200, learning_rate=0.001, lambda_reg=0.01, seed=123)
 
 # Define metrics used to evaluate the models
-mae = cn.metrics.MAE()
-rmse = cn.metrics.RMSE()
-rec_20 = cn.metrics.Recall(k=20)
-ndcg_20 = cn.metrics.NDCG(k=20)
-auc = cn.metrics.AUC()
+mae = cornac.metrics.MAE()
+rmse = cornac.metrics.RMSE()
+recall = cornac.metrics.Recall(k=[10, 20])
+ndcg = cornac.metrics.NDCG(k=[10, 20])
+auc = cornac.metrics.AUC()
 
 # Put it together into an experiment and run
-exp = cn.Experiment(eval_method=ratio_split,
-                    models=[mf, pmf, bpr],
-                    metrics=[mae, rmse, rec_20, ndcg_20, auc],
-                    user_based=True)
-exp.run()
+cornac.Experiment(eval_method=rs,
+                  models=[mf, pmf, bpr],
+                  metrics=[mae, rmse, recall, ndcg, auc],
+                  user_based=True).run()
+
