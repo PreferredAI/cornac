513f3d3d869bc34fb0aedd9a19d70763b7f6809d
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2020-05-05 23:31:57 +0800

    Update KNN example

--- examples/knn_movielens.py
+++ examples/knn_movielens.py
@@ -19,58 +19,53 @@ from cornac.datasets import movielens
 from cornac.eval_methods import RatioSplit
 
 
+K = 50  # number of nearest neighbors
+
+
 # Load ML-100K dataset
 feedback = movielens.load_feedback(variant="100K")
 
 # Define an evaluation method to split feedback into train and test sets
 ratio_split = RatioSplit(
-    data=feedback,
-    test_size=0.2,
-    rating_threshold=4.0,
-    exclude_unknowns=True,
-    verbose=True,
-    seed=123,
+    data=feedback, test_size=0.2, exclude_unknowns=True, verbose=True, seed=123
 )
 
-# Comparing a few variants of KNN methods
-user_knn_cosine = cornac.models.UserKNN(
-    k=20, similarity="cosine", amplify=1.0, name="UserKNN-Cosine"
-)
+# UserKNN methods
+user_knn_cosine = cornac.models.UserKNN(k=K, similarity="cosine", name="UserKNN-Cosine")
 user_knn_pearson = cornac.models.UserKNN(
-    k=20, similarity="pearson", amplify=1.0, name="UserKNN-Pearson"
+    k=K, similarity="pearson", name="UserKNN-Pearson"
 )
-user_knn_tfidf = cornac.models.UserKNN(
-    k=20, similarity="cosine", weighting="tf-idf", amplify=1.0, name="UserKNN-TFIDF"
+user_knn_amp = cornac.models.UserKNN(
+    k=K, similarity="cosine", amplify=2.0, name="UserKNN-Amplified"
+)
+user_knn_idf = cornac.models.UserKNN(
+    k=K, similarity="cosine", weighting="idf", name="UserKNN-IDF"
 )
 user_knn_bm25 = cornac.models.UserKNN(
-    k=20, similarity="cosine", weighting="bm25", amplify=1.0, name="UserKNN-BM25"
+    k=K, similarity="cosine", weighting="bm25", name="UserKNN-BM25"
 )
-item_knn_cosine = cornac.models.ItemKNN(
-    k=20, similarity="cosine", amplify=1.0, name="ItemKNN-Cosine"
-)
-item_knn_adjusted_cosine = cornac.models.ItemKNN(
-    k=20, similarity="adjusted", amplify=1.0, name="ItemKNN-AdjustedCosine"
-)
+# ItemKNN methods
+item_knn_cosine = cornac.models.ItemKNN(k=K, similarity="cosine", name="ItemKNN-Cosine")
 item_knn_pearson = cornac.models.ItemKNN(
-    k=20, similarity="pearson", amplify=1.0, name="ItemKNN-Pearson"
+    k=K, similarity="pearson", name="ItemKNN-Pearson"
+)
+item_knn_adjusted = cornac.models.ItemKNN(
+    k=K, similarity="cosine", mean_centered=True, name="ItemKNN-AdjustedCosine"
 )
 
-# Evaluation metrics
-rmse = cornac.metrics.RMSE()
-rec_20 = cornac.metrics.Recall(k=20)
-
-# Put everything together into an experiment and run it
+# Put everything together into an experiment
 cornac.Experiment(
     eval_method=ratio_split,
     models=[
         user_knn_cosine,
         user_knn_pearson,
-        user_knn_tfidf,
+        user_knn_amp,
+        user_knn_idf,
         user_knn_bm25,
         item_knn_cosine,
-        item_knn_adjusted_cosine,
         item_knn_pearson,
+        item_knn_adjusted,
     ],
-    metrics=[rmse, rec_20],
+    metrics=[cornac.metrics.RMSE()],
     user_based=True,
 ).run()
