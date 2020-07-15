2a2c1736cf09e9d3cb6c763378f740433cb2bbee
tqtg <tuantq.vnu@gmail.com>
2020-07-15 19:18:27 +0800

    Release v1.7.1

--- setup.py
+++ setup.py
@@ -252,7 +252,7 @@ if USE_CYTHON:
 
 setup(
     name="cornac",
-    version="1.7.0",
+    version="1.7.1",
     description="A Comparative Framework for Multimodal Recommender Systems",
     long_description=long_description,
     long_description_content_type="text/markdown",
