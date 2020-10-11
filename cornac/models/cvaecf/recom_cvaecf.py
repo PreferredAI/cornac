ec07d256e55007e40e9888620525f4d81a24d51e
Aghiles <salahaghiles@gmail.com>
2020-10-11 14:00:18 +0800

    Fix typo in Recommender's score function doc

--- cornac/models/cvaecf/recom_cvaecf.py
+++ cornac/models/cvaecf/recom_cvaecf.py
@@ -196,7 +196,7 @@ class CVAECF(Recommender):
             The index of the user for whom to perform score prediction.
 
         item_idx: int, optional, default: None
-            The index of the item for that to perform score prediction.
+            The index of the item for which to perform score prediction.
             If None, scores for all known items will be returned.
 
         Returns
--- cornac/models/recommender.py
+++ cornac/models/recommender.py
@@ -185,7 +185,7 @@ class Recommender:
             The index of the user for whom to perform score prediction.
             
         item_idx: int, optional, default: None
-            The index of the item for that to perform score prediction.
+            The index of the item for which to perform score prediction.
             If None, scores for all known items will be returned.
 
         Returns
