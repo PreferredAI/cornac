1f941aebbe59f765cfb3eba09169851d4ef7b335
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2020-11-04 12:20:40 +0800

    Refactor code

--- tests/cornac/eval_methods/test_stratified_split.py
+++ tests/cornac/eval_methods/test_stratified_split.py
@@ -25,56 +25,101 @@ from cornac.metrics import MAE, Recall
 
 
 class TestStratifiedSplit(unittest.TestCase):
-
     def setUp(self):
-        self.data = [(u, i, random.randint(1, 5), random.randint(0, 100))
-                for (u, i) in itertools.product(['u1', 'u2', 'u3', 'u4', 'u5', 'u6'],
-                                                ['i1', 'i2', 'i3', 'i4', 'i5', 'i6'])]
+        self.data = [
+            (u, i, random.randint(1, 5), random.randint(0, 100))
+            for (u, i) in itertools.product(
+                ["u1", "u2", "u3", "u4", "u5", "u6"],
+                ["i1", "i2", "i3", "i4", "i5", "i6"],
+            )
+        ]
 
     def test_group_by(self):
-        StratifiedSplit(self.data, fmt='UIR', group_by='user', test_size=0.4, verbose=True)
-        StratifiedSplit(self.data, fmt='UIR', group_by='item', test_size=0.4, verbose=True)
+        StratifiedSplit(
+            self.data, fmt="UIR", group_by="user", test_size=0.4, verbose=True
+        )
+        StratifiedSplit(
+            self.data, fmt="UIR", group_by="item", test_size=0.4, verbose=True
+        )
 
         try:
-            StratifiedSplit(self.data, fmt='UIR', group_by=None, verbose=True)
+            StratifiedSplit(self.data, fmt="UIR", group_by=None, verbose=True)
         except ValueError:
             assert True
 
     def test_data_fmt(self):
         try:
-            StratifiedSplit(self.data, fmt='UIR', chrono=True, verbose=True)
+            StratifiedSplit(self.data, fmt="UIR", chrono=True, verbose=True)
         except ValueError:
             assert True
 
         try:
-            data = Reader().read('./tests/data.txt', fmt='UIR', sep='\t')
+            data = Reader().read("./tests/data.txt", fmt="UIR", sep="\t")
             StratifiedSplit(data, chrono=True, verbose=True)
         except ValueError:
             assert True
 
     def test_splits(self):
         try:
-            StratifiedSplit(self.data, fmt='UIRT', chrono=True, test_size=0.1, val_size=0.1, verbose=True)
-        except ValueError: # test_data and val_data are empty
+            StratifiedSplit(
+                self.data,
+                fmt="UIRT",
+                chrono=True,
+                test_size=0.1,
+                val_size=0.1,
+                verbose=True,
+            )
+        except ValueError:  # test_data and val_data are empty
             assert True
 
-        eval_method = StratifiedSplit(self.data, fmt='UIRT', group_by='user', chrono=True, test_size=0.2, val_size=0.2, verbose=True)
+        eval_method = StratifiedSplit(
+            self.data,
+            fmt="UIRT",
+            group_by="user",
+            chrono=True,
+            test_size=0.2,
+            val_size=0.2,
+            verbose=True,
+        )
         self.assertTrue(isclose(0.2, eval_method.test_size))
         self.assertTrue(isclose(0.2, eval_method.val_size))
 
-        eval_method = StratifiedSplit(self.data, fmt='UIRT', group_by='item', chrono=True, test_size=0.2, val_size=0.2, verbose=True)
+        eval_method = StratifiedSplit(
+            self.data,
+            fmt="UIRT",
+            group_by="item",
+            chrono=True,
+            test_size=0.2,
+            val_size=0.2,
+            verbose=True,
+        )
         self.assertTrue(isclose(0.2, eval_method.test_size))
         self.assertTrue(isclose(0.2, eval_method.val_size))
 
-        eval_method = StratifiedSplit(self.data, fmt='UIR', group_by='user', chrono=False, test_size=0.2, val_size=0.2, verbose=True)
+        eval_method = StratifiedSplit(
+            self.data,
+            fmt="UIR",
+            group_by="user",
+            chrono=False,
+            test_size=0.2,
+            val_size=0.2,
+            verbose=True,
+        )
         self.assertTrue(isclose(0.2, eval_method.test_size))
         self.assertTrue(isclose(0.2, eval_method.val_size))
 
-        eval_method = StratifiedSplit(self.data, fmt='UIR', group_by='item', chrono=False, test_size=0.2, val_size=0.2, verbose=True)
+        eval_method = StratifiedSplit(
+            self.data,
+            fmt="UIR",
+            group_by="item",
+            chrono=False,
+            test_size=0.2,
+            val_size=0.2,
+            verbose=True,
+        )
         self.assertTrue(isclose(0.2, eval_method.test_size))
         self.assertTrue(isclose(0.2, eval_method.val_size))
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
