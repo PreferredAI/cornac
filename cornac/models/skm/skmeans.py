4d383e8ab8881033ad232a59ef7aea5ae7554788
Aghiles <salahaghiles@gmail.com>
2020-12-28 12:13:41 +0800

    Update SKMeans to account for max_iter and seed (#377)

--- cornac/models/skm/skmeans.py
+++ cornac/models/skm/skmeans.py
@@ -17,40 +17,39 @@ import numpy as np
 import scipy.sparse as sp
 
 
-def skmeans(X, k=5, max_iter=100, tol=1e-6, verbose=True, init_par=None):
+def skmeans(X, k=5, max_iter=100, tol=1e-6, verbose=True, seed=None, init_par=None):
     # The Spherical k-means clustering algorithm
 
     n = X.shape[0]
-    print(n)
-    print(k)
     # normalize rows of X so as they lie on a unit hypersphere
     X = X.multiply(sp.csc_matrix(1.0 / (np.sqrt(X.multiply(X).sum(1).A1) + 1e-20)).T)
     if init_par is None:
-        par = np.random.randint(k, size=n)
+        prng = np.random.RandomState(seed)
+        par = prng.randint(k, size=n)
     else:
         par = init_par
 
     # Initialisation of the classification matrix Z
-    Z = sp.csc_matrix((n, k))
+    Z = sp.lil_matrix((n, k))
     Z[np.arange(n), par] = 1
 
     change = True
     l_init = -1e1000
     l = []
     iter_ = 0
-    while change:
+    while change and iter_ < max_iter:
         change = False
         # Update centroids
         MU = Z.T * X
         # project centroids to the unit hypersphere
-        MU = MU.multiply(sp.csc_matrix(1.0 / np.sqrt(MU.multiply(MU).sum(1).A1)).T)
+        MU = MU.multiply(sp.lil_matrix(1.0 / np.sqrt(MU.multiply(MU).sum(1).A1)).T)
         # MU = sp.csc_matrix(MU)
 
         # Object Assignements
         Z1 = X * MU.T
         par = Z1.argmax(1).A1  # The object partition in k clusters
         # update the classification matrix
-        Z = sp.csc_matrix((n, k))
+        Z = sp.lil_matrix((n, k))
         Z[np.arange(len(par)), par] = 1
 
         # Skmeans criteria (likelihood)
@@ -58,7 +57,7 @@ def skmeans(X, k=5, max_iter=100, tol=1e-6, verbose=True, init_par=None):
 
         if np.abs(l_t - l_init) > tol:
             if verbose:
-                print("Iter %i, likelihood: %f" % (iter_, l_t))
+                print("Iter %i, likelihood: %f" % (iter_ + 1, l_t))
             l_init = l_t
             change = True
             l.append(l_t)
