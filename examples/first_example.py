cd90351317fc3f4fabd3313fd43374806f89d600
Quoc-Tuan Truong <tuantq.vnu@gmail.com>
2021-01-03 23:23:12 +0800

    typos

--- examples/first_example.py
+++ examples/first_example.py
@@ -23,14 +23,14 @@ from cornac.metrics import MAE, RMSE, Precision, Recall, NDCG, AUC, MAP
 ml_100k = cornac.datasets.movielens.load_feedback()
 rs = RatioSplit(data=ml_100k, test_size=0.2, rating_threshold=4.0, seed=123)
 
-# initialized models, here we are comparing biased MF, PMF, and BPR
+# initialize models, here we are comparing: Biased MF, PMF, and BPR
 models = [
     MF(k=10, max_iter=25, learning_rate=0.01, lambda_reg=0.02, use_bias=True, seed=123),
     PMF(k=10, max_iter=100, learning_rate=0.001, lambda_reg=0.001, seed=123),
     BPR(k=10, max_iter=200, learning_rate=0.001, lambda_reg=0.01, seed=123),
 ]
 
-# define metrics used to evaluate the models
+# define metrics to evaluate the models
 metrics = [MAE(), RMSE(), Precision(k=10), Recall(k=10), NDCG(k=10), AUC(), MAP()]
 
 # put it together in an experiment, voilà!
