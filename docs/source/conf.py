673402fe9a6f69c5c493d25e6e307fc5d1a2a044
tqtg <tuantq.vnu@gmail.com>
2021-01-29 15:00:14 +0800

    Release v1.11.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Cornac Authors'
 author = 'Preferred.AI'
 
 # The short X.Y version
-version = '1.10'
+version = '1.11'
 # The full version, including alpha/beta/rc tags
-release = '1.10.0'
+release = '1.11.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -269,7 +269,7 @@ if USE_CYTHON:
 
 setup(
     name="cornac",
-    version="1.10.0",
+    version="1.11.0",
     description="A Comparative Framework for Multimodal Recommender Systems",
     long_description=long_description,
     long_description_content_type="text/markdown",
