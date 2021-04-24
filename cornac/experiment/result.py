1170c88e4102c4ba338cdbf8aadc29c6a068be08
Amir H. Jadidinejad <amir.jadidi@yahoo.com>
2021-04-24 23:14:39 +0100

    add STResult class for stratified evaluation method

--- cornac/experiment/result.py
+++ cornac/experiment/result.py
@@ -26,10 +26,12 @@ def _table_format(data, headers=None, index=None, extra_spaces=0, h_bars=None):
         for idx, row in zip(index, data):
             row.insert(0, idx)
 
-    column_widths = np.asarray([[len(str(v)) for v in row] for row in data]).max(axis=0)
+    column_widths = np.asarray([[len(str(v)) for v in row]
+                                for row in data]).max(axis=0)
 
     row_fmt = (
-        " | ".join(["{:>%d}" % (w + extra_spaces) for w in column_widths][1:]) + "\n"
+        " | ".join(["{:>%d}" % (w + extra_spaces)
+                    for w in column_widths][1:]) + "\n"
     )
     if index is not None:
         row_fmt = "{:<%d} | " % (column_widths[0] + extra_spaces) + row_fmt
@@ -74,7 +76,7 @@ class Result:
 class CVResult(list):
     """
     Cross Validation Result Class for a single model. A list of :obj:`cornac.experiment.Result`.
-    
+
     Parameters
     ----------
     model_name: string, required
@@ -100,7 +102,71 @@ class CVResult(list):
         data = np.vstack([data, mean, std])
         data = [[NUM_FMT.format(v) for v in row] for row in data]
         index.extend(["Mean", "Std"])
-        self.table = _table_format(data, headers, index, h_bars=[1, len(data) - 1])
+        self.table = _table_format(
+            data, headers, index, h_bars=[1, len(data) - 1])
+
+
+class STResult(list):
+    """
+    Stratified Result Class for a single model
+
+    Parameters
+    ----------
+    model_name: string, required
+        The name of the recommender model.
+    """
+
+    def __init__(self, model_name):
+        super().__init__()
+        self.model_name = model_name
+
+    def __str__(self):
+        return '[{}]\n{}'.format(self.model_name, self.table)
+
+    def organize(self):
+
+        headers = list(self[0].metric_avg_results.keys())
+
+        data, index, sizes = [], [], []
+        for f, r in enumerate(self):
+            data.append([r.metric_avg_results[m] for m in headers])
+            if f == 0:
+                index.append('Closed')
+            elif f == 1:
+                index.append('IPS')
+            elif f == 2:
+                index.append('SNIPS')
+            else:
+                index.append('Q%d' % (f - 2))
+            sizes.append(r.metric_avg_results['SIZE'])
+
+        # add mean and std rows (total accumulative)
+        data = np.asarray(data)
+        mean, std = data.mean(axis=0), data.std(axis=0)
+
+        # add unbiased stratified evaluation
+        weights = np.asarray(sizes) / sizes[0]
+        unbiased = np.average(
+            data[3:], axis=0, weights=weights[3:]) * sum(weights[3:])
+
+        # weighted average does not meaningful for size
+        for idx, header in enumerate(headers):
+            if header == 'SIZE':
+                unbiased[idx] = sizes[0]
+
+        # update the table
+        data = np.vstack([data, unbiased])
+        data = [[NUM_FMT.format(v) for v in row] for row in data]
+        index.extend(['Unbiased'])
+
+        # add unbiased to the list
+        self.append(Result(model_name=self[0].model_name,
+                           metric_avg_results=OrderedDict(
+                               zip(headers, unbiased)),
+                           metric_user_results=None))
+
+        self.table = _table_format(
+            data, headers, index, h_bars=[1, 2, 4, len(data)])
 
 
 class ExperimentResult(list):
@@ -112,7 +178,8 @@ class ExperimentResult(list):
         headers = list(self[0].metric_avg_results.keys())
         data, index = [], []
         for r in self:
-            data.append([NUM_FMT.format(r.metric_avg_results[m]) for m in headers])
+            data.append([NUM_FMT.format(r.metric_avg_results[m])
+                         for m in headers])
             index.append(r.model_name)
         return _table_format(data, headers, index, h_bars=[1])
 
