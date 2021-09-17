2a23e7321a4803d7945ba01d4445dda42bdcfaf7
tqtg <tuantq.vnu@gmail.com>
2021-09-17 11:12:01 +0800

    Release v1.14.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Cornac Authors'
 author = 'Preferred.AI'
 
 # The short X.Y version
-version = '1.13'
+version = '1.14'
 # The full version, including alpha/beta/rc tags
-release = '1.13.5'
+release = '1.14.0'
 
 
 # -- General configuration ---------------------------------------------------
