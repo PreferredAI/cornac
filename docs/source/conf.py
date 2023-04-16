1b0515dc108cf36fbbe00b32a0fa256ab5a323c2
tqtg <tuantq.vnu@gmail.com>
2023-04-15 20:21:41 -0700

    Release v1.15.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Preferred.AI'
 # The short X.Y version
 version = '1.15'
 # The full version, including alpha/beta/rc tags
-release = '1.15.0'
+release = '1.15.1'
 
 
 # -- General configuration ---------------------------------------------------
