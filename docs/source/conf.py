3b7fb035d890c884b99966e24e214209c00fc6b2
Tuan Truong <tuantq.vnu@gmail.com>
2023-05-12 22:50:09 -0700

    Release v1.15.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Preferred.AI'
 # The short X.Y version
 version = '1.15'
 # The full version, including alpha/beta/rc tags
-release = '1.15.2'
+release = '1.15.3'
 
 
 # -- General configuration ---------------------------------------------------
