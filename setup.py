7654454b4e3f8196d954c7daf41814f4b90b0a96
Tuan Truong <tuantq.vnu@gmail.com>
2023-07-22 11:17:09 -0700

    Release v1.16.0

--- setup.py
+++ setup.py
@@ -288,7 +288,7 @@ if USE_CYTHON:
 
 setup(
     name="cornac",
-    version="1.15.4",
+    version="1.16.0",
     description="A Comparative Framework for Multimodal Recommender Systems",
     long_description=long_description,
     long_description_content_type="text/markdown",
