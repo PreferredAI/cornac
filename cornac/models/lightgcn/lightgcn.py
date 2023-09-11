6866c3c1b36bf1f24492e987079808e41ca24c02
tqtg <tuantq.vnu@gmail.com>
2023-09-11 22:20:18 +0000

    refactor code

--- cornac/models/lightgcn/lightgcn.py
+++ cornac/models/lightgcn/lightgcn.py
@@ -1,238 +1,105 @@
-import logging
-import time
 import torch
-import numpy as np
-from .data import construct_graph
-from .nn_modules import NeuralNetwork
-from tqdm import tqdm
-from torch.utils.tensorboard import SummaryWriter
+import torch.nn as nn
+import torch
+import dgl
+import dgl.function as fn
 
 
-class Model:
-    def __init__(
-        self,
-        num_users,
-        num_items,
-        hidden_dim,
-        num_layers,
-        learning_rate,
-        batch_size,
-        verbose,
-        seed,
-    ):
-        self.learning_rate = learning_rate
-        self.batch_size = batch_size
-        
-        self.device = torch.device(
-            "cuda" if torch.cuda.is_available() else "cpu"
+def construct_graph(data_set):
+    """
+    Generates graph given a cornac data set
+
+    Parameters
+    ----------
+    data_set : cornac.data.dataset.Dataset
+        The data set as provided by cornac
+    """
+    user_indices, item_indices, _ = data_set.uir_tuple
+    user_nodes, item_nodes = (
+        torch.from_numpy(user_indices),
+        torch.from_numpy(
+            item_indices + data_set.total_users
+        ),  # increment item node idx by num users
+    )
+
+    u, v = torch.cat([user_nodes, item_nodes], dim=0), torch.cat(
+        [item_nodes, user_nodes], dim=0
+    )
+
+    g = dgl.graph((u, v), num_nodes=(data_set.total_users + data_set.total_items))
+    return g
+
+
+class GCNLayer(nn.Module):
+    def __init__(self):
+        super(GCNLayer, self).__init__()
+
+    def forward(self, graph, src_embedding, dst_embedding):
+        with graph.local_scope():
+            inner_product = torch.cat((src_embedding, dst_embedding), dim=0)
+
+            out_degs = graph.out_degrees().to(src_embedding.device).float().clamp(min=1)
+            norm_out_degs = torch.pow(out_degs, -0.5).view(-1, 1)  # D^-1/2
+
+            inner_product = inner_product * norm_out_degs
+
+            # graph.ndata["h"] = {"user": inner_product, "item": inner_product}
+            graph.ndata["h"] = inner_product
+            graph.update_all(
+                message_func=fn.copy_u("h", "m"), reduce_func=fn.sum("m", "h")
+            )
+
+            res = graph.ndata["h"]
+
+            in_degs = graph.in_degrees().to(src_embedding.device).float().clamp(min=1)
+            norm_in_degs = torch.pow(in_degs, -0.5).view(-1, 1)  # D^-1/2
+
+            res = res * norm_in_degs
+            return res
+
+
+class Model(nn.Module):
+    def __init__(self, user_size, item_size, hidden_size, num_layers=3, device=None):
+        super(Model, self).__init__()
+        self.user_size = user_size
+        self.item_size = item_size
+        self.hidden_size = hidden_size
+        self.embedding_weights = self._init_weights()
+        self.layers = nn.ModuleList([GCNLayer() for _ in range(num_layers)])
+        self.device = device
+
+    def forward(self, graph):
+        user_embedding = self.embedding_weights["user_embedding"]
+        item_embedding = self.embedding_weights["item_embedding"]
+
+        for i, layer in enumerate(self.layers, start=1):
+            if i == 1:
+                embeddings = layer(graph, user_embedding, item_embedding)
+            else:
+                embeddings = layer(
+                    graph, embeddings[: self.user_size], embeddings[self.user_size :]
+                )
+
+            user_embedding = user_embedding + embeddings[: self.user_size] * (
+                1 / (i + 1)
+            )
+            item_embedding = item_embedding + embeddings[self.user_size :] * (
+                1 / (i + 1)
+            )
+
+        return user_embedding, item_embedding
+
+    def _init_weights(self):
+        initializer = nn.init.xavier_uniform_
+
+        weights_dict = nn.ParameterDict(
+            {
+                "user_embedding": nn.Parameter(
+                    initializer(torch.empty(self.user_size, self.hidden_size))
+                ),
+                "item_embedding": nn.Parameter(
+                    initializer(torch.empty(self.item_size, self.hidden_size))
+                ),
+            }
         )
-
-        self.model = NeuralNetwork(
-            num_users,
-            num_items,
-            hidden_dim,
-            num_layers,
-            self.device
-        ).to(self.device)
-
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate, weight_decay=0)
-
-        self.verbose = verbose
-        if verbose:
-            logging.basicConfig(level=logging.INFO)
-
-        self.seed = seed
-        if seed is not None:
-            torch.manual_seed(seed)
-            if torch.cuda.is_available():
-                torch.cuda.manual_seed_all(seed)
-
-        self.best_valid_loss = np.inf
-        self.writer = SummaryWriter()
-
-    def _calculate_loss(self, batch_u, batch_i, batch_j, user_embeddings, item_embeddings):        
-        user_embed = user_embeddings[batch_u]
-        positive_item_embed = item_embeddings[batch_i]
-        negative_item_embed = item_embeddings[batch_j]
-
-        pred_i = torch.sum(
-            torch.multiply(user_embed, positive_item_embed), dim=1
-        )
-        pred_j = torch.sum(
-            torch.multiply(user_embed, negative_item_embed), dim=1
-        )
-
-        # bpr_loss = - (pred_j.view(-1) - pred_i.view(-1)).sigmoid().log().sum()
-        # bpr_loss = torch.mean(torch.nn.functional.softplus(pred_j - pred_i))
-
-        # reg_loss = (
-        #     torch.norm(user_embed) ** 2 +
-        #     torch.norm(positive_item_embed) ** 2 +
-        #     torch.norm(negative_item_embed) ** 2
-        # )
-
-        reg_loss = (1/2) * (
-            user_embed.norm(2).pow(2) +
-            positive_item_embed.norm(2).pow(2) +
-            negative_item_embed.norm(2).pow(2)
-        ) / float(len(batch_u))
-
-        loss = torch.mean(torch.nn.functional.softplus(pred_j - pred_i))
-        # loss = 0.5 * (bpr_loss + self.learning_rate * reg_loss) / self.batch_size
-
-        return loss, reg_loss
-
-    def _train_model(self, train_set, val_set, model, graph, weight_decay):
-        epoch_loss_test = 0
-        epoch_loss_val = None
-
-        # Train set
-        for batch_u, batch_i, batch_j in tqdm(
-            train_set.uij_iter(
-                batch_size=self.batch_size,
-                shuffle=True,
-            ),
-            desc="Batch",
-            total=train_set.num_batches(self.batch_size),
-            leave=False,
-            position=1
-        ):
-            batch_u = torch.from_numpy(batch_u).long().to(self.device)
-            batch_i = torch.from_numpy(batch_i).long().to(self.device)
-            batch_j = torch.from_numpy(batch_j).long().to(self.device)
-
-            user_embeddings, item_embeddings = model(graph)
-            loss, reg_loss = self._calculate_loss(batch_u, batch_i, batch_j, user_embeddings, item_embeddings)
-            reg_loss = reg_loss * weight_decay
-            loss = loss + reg_loss
-            epoch_loss_test += loss.cpu().item()
-
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-            # user_embed, pos_item_embed, neg_item_embed = _get_embedding(self, users, )
-
-            # for u, i, j in zip(batch_u, batch_i, batch_j):
-            #     user_embeddings, item_embeddings = model(graph)
-            #     bpr_loss, loss = self._calculate_loss(u, i, j, user_embeddings, item_embeddings)
-            #     epoch_loss_test += bpr_loss.item()
-                
-            #     self.optimizer.zero_grad()
-            #     loss.backward()
-            #     self.optimizer.step()
-        
-        # # Val set
-        # if val_set is not None:
-        #     epoch_loss_val = 0
-
-        #     for batch_u, batch_i, batch_j in tqdm(
-        #         val_set.uij_iter(
-        #             batch_size=self.batch_size,
-        #             shuffle=True,
-        #         ),
-        #         desc="Val Set",
-        #         total=val_set.num_batches(self.batch_size),
-        #         leave=False,
-        #         position=1
-        #     ):
-        #         batch_u = torch.from_numpy(batch_u).long().to(self.device)
-        #         batch_i = torch.from_numpy(batch_i).long().to(self.device)
-        #         batch_j = torch.from_numpy(batch_j).long().to(self.device)
-
-        #         user_embeddings, item_embeddings = model(graph)
-        #         bpr_loss, _ = self._calculate_loss(batch_u, batch_i, batch_j, user_embeddings, item_embeddings)
-        #         epoch_loss_val += bpr_loss.item()
-
-        #     if epoch_loss_val < self.best_valid_loss:
-        #         self.best_valid_loss = epoch_loss_val
-        #         self.no_better_valid_count = 0
-        #         self.best_model_state_dict = model.state_dict()
-
-        epoch_loss_test = epoch_loss_test/self.batch_size
-
-        return epoch_loss_test, epoch_loss_val
-
-    def train(
-        self,
-        train_set,
-        val_set,
-        max_iter,
-        weight_decay
-    ):
-        self.graph = construct_graph(train_set, self.device)
-        # progress_bar = trange(1, max_iter + 1, disable=self.verbose)
-        for iter in tqdm(
-            range(1, max_iter),
-            desc="Training",
-            unit="iter",
-            position=0,
-            leave=False,
-            # disable=self.verbose,
-        ):
-            loss_test, loss_val = self._train_model(train_set, val_set, self.model, self.graph, weight_decay)
-
-            log_str = f"Epoch: {iter}\t loss: {loss_test:.4f}"
-            if loss_val is not None:
-                log_str += f"\t val_loss: {loss_val:.4f}"
-            logging.info(log_str)
-
-            self.writer.add_scalar("Loss/train", loss_test, iter)
-
-    def predict(
-        self,
-        test_set
-    ):
-        graph = construct_graph(test_set, self.device)
-        user_embeddings, item_embeddings = self.model(graph)
-        return user_embeddings, item_embeddings
-
-    def score(
-        self,
-        user_embeddings,
-        item_embeddings,
-        user_idx,
-        item_idx
-    ):
-        if item_idx is None:
-            return torch.matmul(item_embeddings, torch.t(user_embeddings[user_idx])).cpu().detach().numpy()
-        else:
-            return torch.sum(torch.multiply(user_embeddings[user_idx], item_embeddings[item_idx])).cpu().detach().numpy()
-        
-    def monitor_value(self):
-        """Calculating monitored value used for early stopping on validation set (`val_set`).
-        This function will be called by `early_stop()` function.
-
-        Returns
-        -------
-        res : float
-            Monitored value on validation set.
-            Return `None` if `val_set` is `None`.
-        """
-        if self.val_set is None:
-            return None
-        
-        epoch_loss_val = 0
-        for batch_u, batch_i, batch_j in tqdm(
-            self.val_set.uij_iter(
-                batch_size=self.batch_size,
-                shuffle=True,
-            ),
-            desc="Val Set",
-            total=self.val_set.num_batches(self.batch_size),
-            leave=False,
-            position=1
-        ):
-            batch_u = torch.from_numpy(batch_u).long().to(self.device)
-            batch_i = torch.from_numpy(batch_i).long().to(self.device)
-            batch_j = torch.from_numpy(batch_j).long().to(self.device)
-
-            user_embeddings, item_embeddings = self.model(self.graph)
-            bpr_loss, _ = self._calculate_loss(batch_u, batch_i, batch_j, user_embeddings, item_embeddings)
-            epoch_loss_val += bpr_loss.item()
-
-        logging.info(f"loss_val: {epoch_loss_val}")
-
-        return epoch_loss_val
-
-        
-
+        return weights_dict
--- cornac/models/lightgcn/recom_lightgcn.py
+++ cornac/models/lightgcn/recom_lightgcn.py
@@ -14,21 +14,23 @@
 # ============================================================================
 
 from ..recommender import Recommender
-# from ...exception import ScoreException
+from ...exception import ScoreException
+
+from tqdm.auto import tqdm
 
 
 class LightGCN(Recommender):
     def __init__(
         self,
         name="LightGCN",
-        max_iter=1000,
+        num_epochs=1000,
         learning_rate=0.001,
         train_batch_size=1024,
         test_batch_size=100,
         hidden_dim=16,
         num_layers=3,
-        early_stopping_patience=5,
-        weight_decay=1e-4,
+        early_stopping=None,
+        lambda_reg=1e-4,
         top_k=10,
         trainable=True,
         verbose=False,
@@ -36,53 +38,194 @@ class LightGCN(Recommender):
     ):
         super().__init__(name=name, trainable=trainable, verbose=verbose)
 
-        self.max_iter = max_iter
+        self.num_epochs = num_epochs
         self.learning_rate = learning_rate
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.train_batch_size = train_batch_size
         self.test_batch_size = test_batch_size
-        self.early_stopping_patience = early_stopping_patience
-        self.weight_decay = weight_decay
+        self.early_stopping = early_stopping
+        self.lambda_reg = lambda_reg
         self.top_k = top_k
         self.seed = seed
 
     def fit(self, train_set, val_set=None):
         Recommender.fit(self, train_set, val_set)
 
-        if self.trainable:
-            from .lightgcn import Model
+        if not self.trainable:
+            return self
 
-            self.model = Model(
-                train_set.total_users,
-                train_set.total_items,
-                self.hidden_dim,
-                self.num_layers,
-                self.learning_rate,
-                self.train_batch_size,
-                verbose=True,
-                seed=None
+        # model setup
+        import torch
+        from .lightgcn import Model
+        from .lightgcn import construct_graph
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if self.seed is not None:
+            torch.manual_seed(self.seed)
+            if torch.cuda.is_available():
+                torch.cuda.manual_seed_all(self.seed)
+
+        model = Model(
+            train_set.total_users,
+            train_set.total_items,
+            self.hidden_dim,
+            self.num_layers,
+        ).to(self.device)
+
+        graph = self.graph = construct_graph(train_set).to(self.device)
+
+        optimizer = torch.optim.Adam(
+            model.parameters(), lr=self.learning_rate, weight_decay=self.lambda_reg
+        )
+        loss_fn = torch.nn.BCELoss(reduction="sum")
+
+        # model training
+        pbar = tqdm(
+            range(1, self.num_epochs),
+            desc="Training",
+            unit="iter",
+            position=0,
+            leave=False,
+            disable=not self.verbose,
+        )
+        for epoch in pbar:
+            model.train()
+            accum_loss = 0.0
+            for batch_u, batch_i, batch_j in tqdm(
+                train_set.uij_iter(
+                    batch_size=self.train_batch_size,
+                    shuffle=True,
+                ),
+                desc="Batch",
+                total=train_set.num_batches(self.train_batch_size),
+                leave=False,
+                position=1,
+                disable=not self.verbose,
+            ):
+                user_embeddings, item_embeddings = model(graph)
+
+                batch_u = torch.from_numpy(batch_u).long().to(self.device)
+                batch_i = torch.from_numpy(batch_i).long().to(self.device)
+                batch_j = torch.from_numpy(batch_j).long().to(self.device)
+
+                user_embed = user_embeddings[batch_u]
+                positive_item_embed = item_embeddings[batch_i]
+                negative_item_embed = item_embeddings[batch_j]
+
+                ui_scores = torch.sum(
+                    torch.multiply(user_embed, positive_item_embed), dim=1
+                )
+                uj_scores = torch.sum(
+                    torch.multiply(user_embed, negative_item_embed), dim=1
+                )
+
+                loss = loss_fn(
+                    torch.sigmoid(ui_scores - uj_scores), torch.ones_like(ui_scores)
+                )
+                accum_loss += loss.cpu().item()
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            accum_loss /= len(train_set.uir_tuple[0])  # normalize over all observations
+            pbar.set_postfix(loss=accum_loss)
+
+            # store user and item embedding matrices for prediction
+            model.eval()
+            self.U, self.V = model(graph)
+
+            if self.early_stopping is not None and self.early_stop(
+                **self.early_stopping
+            ):
+                break
+
+        # we will use numpy for faster prediction in the score function, no need torch
+        self.U = user_embeddings.cpu().detach().numpy()
+        self.V = item_embeddings.cpu().detach().numpy()
+
+    def monitor_value(self):
+        """Calculating monitored value used for early stopping on validation set (`val_set`).
+        This function will be called by `early_stop()` function.
+
+        Returns
+        -------
+        res : float
+            Monitored value on validation set.
+            Return `None` if `val_set` is `None`.
+        """
+        if self.val_set is None:
+            return None
+
+        import torch
+
+        loss_fn = torch.nn.BCELoss(reduction="sum")
+        accum_loss = 0.0
+        pbar = tqdm(
+            self.val_set.uij_iter(batch_size=self.test_batch_size),
+            desc="Validation",
+            total=self.val_set.num_batches(self.test_batch_size),
+            leave=False,
+            position=2,
+            disable=not self.verbose,
+        )
+        for batch_u, batch_i, batch_j in pbar:
+            batch_u = torch.from_numpy(batch_u).long().to(self.device)
+            batch_i = torch.from_numpy(batch_i).long().to(self.device)
+            batch_j = torch.from_numpy(batch_j).long().to(self.device)
+
+            user_embed = self.U[batch_u]
+            positive_item_embed = self.V[batch_i]
+            negative_item_embed = self.V[batch_j]
+
+            ui_scores = torch.sum(
+                torch.multiply(user_embed, positive_item_embed), dim=1
+            )
+            uj_scores = torch.sum(
+                torch.multiply(user_embed, negative_item_embed), dim=1
             )
 
-            self.model.train(
-                train_set,
-                val_set,
-                self.max_iter,
-                self.weight_decay
+            loss = loss_fn(
+                torch.sigmoid(ui_scores - uj_scores), torch.ones_like(ui_scores)
             )
+            accum_loss += loss.cpu().item()
+            pbar.set_postfix(val_loss=accum_loss)
 
-    def transform(self, test_set):
-        self.user_embeddings, self.item_embeddings = self.model.predict(test_set)
+        accum_loss /= len(self.val_set.uir_tuple[0])
+        return -accum_loss  # higher is better -> smaller loss is better
 
     def score(self, user_idx, item_idx=None):
-        # if item_idx is None:
-        #     if self.train_set.is_unk_user(user_idx):
-        #         raise ScoreException(
-        #              "Can't make score prediction for (user_id=%d)" % user_idx
-        #         )
-        return self.model.score(
-            self.user_embeddings,
-            self.item_embeddings,
-            user_idx,
-            item_idx
-        )
+        """Predict the scores/ratings of a user for an item.
+
+        Parameters
+        ----------
+        user_idx: int, required
+            The index of the user for whom to perform score prediction.
+
+        item_idx: int, optional, default: None
+            The index of the item for which to perform score prediction.
+            If None, scores for all known items will be returned.
+
+        Returns
+        -------
+        res : A scalar or a Numpy array
+            Relative scores that the user gives to the item or to all known items
+
+        """
+        if item_idx is None:
+            if self.train_set.is_unk_user(user_idx):
+                raise ScoreException(
+                    "Can't make score prediction for (user_id=%d)" % user_idx
+                )
+            known_item_scores = self.V.dot(self.U[user_idx, :])
+            return known_item_scores
+        else:
+            if self.train_set.is_unk_user(user_idx) or self.train_set.is_unk_item(
+                item_idx
+            ):
+                raise ScoreException(
+                    "Can't make score prediction for (user_id=%d, item_id=%d)"
+                    % (user_idx, item_idx)
+                )
+            return self.V[item_idx, :].dot(self.U[user_idx, :])
