2d430dcb65d5c081e156cbef1489364d2a53abac
Darryl Ong <ongrongsheng@gmail.com>
2023-10-20 09:48:35 +0800

    Release v1.17.0

--- setup.py
+++ setup.py
@@ -296,7 +296,7 @@ if USE_CYTHON:
 
 setup(
     name="cornac",
-    version="1.16.0",
+    version="1.17.0",
     description="A Comparative Framework for Multimodal Recommender Systems",
     long_description=long_description,
     long_description_content_type="text/markdown",
