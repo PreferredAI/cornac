e7ce06eb33a66afe47473a0cc709f40933231f4a
darrylong <darrylong@users.noreply.github.com>
2023-11-16 15:15:13 +0800

    Updated styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,15 +67,22 @@ html_theme = 'pydata_sphinx_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 html_title = "Cornac"
+html_logo = "_static/logo.png"
 
 html_theme_options = {
     # "switcher": {
     #     "json_url": "https://cornac.readthedocs.io/latest/_static/switcher.json",
     # }
     "announcement": "Welcome to our new documentation site! Please report any issues <a href='https://github.com/PreferredAI/cornac/issues/new?assignees=&labels=&projects=&title=%5BDOCS%5D'>here</a>.",
+    "logo": {
+        "text": "Cornac",
+    },
+    "pygment_light_style": "default",
+    "pygment_dark_style": "github-dark"
 }
 
 # -- Options for intersphinx extension ---------------------------------------
