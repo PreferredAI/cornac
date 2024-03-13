91fda338aed819663159f8144673c02b49a5600b
tj45794 <tjendal@cs.aau.dk>
2024-03-13 13:31:25 +0100

    Path fix

--- examples/hypar_example.py
+++ examples/hypar_example.py
@@ -27,16 +27,16 @@ from cornac.metrics import NDCG, AUC, MAP, MRR, Recall, Precision
 
 # Download datsets from: https://drive.google.com/drive/folders/1DZAUJ4aO5O4SLxCFGS3MYBQhn9DlZ7qE
 # Extract the datasets to the current working directory
-def dataset_converter(dataset_name):
+def dataset_converter(dataset_name, path='seer-ijcai2020'):
     import pandas as pd
     print(f'--- Loading {dataset_name} ---')
-    df = pd.read_csv(os.path.join('seer-ijcai2020', dataset_name, 'profile.csv'), sep=',')
+    df = pd.read_csv(os.path.join(path, dataset_name, 'profile.csv'), sep=',')
     # df.drop(columns=['sentire_aspect', 'aspect_pos', 'opinion_pos', 'sentence_len', 'sentence_count'], inplace=True)
 
     # reviewerID,asin,overall,unixReviewTime,aspect_pos,aspect,opinion_pos,opinion,sentence,sentence_len,sentence_count,sentiment
 
     # Creating rating file
-    ra_path = os.path.join('seer-ijcai2020', dataset_name, 'ratings.txt')
+    ra_path = os.path.join(path, dataset_name, 'ratings.txt')
     if not os.path.isfile(ra_path):
         print('Removing duplicates')
         ratings = df[['reviewerID', 'asin', 'overall', 'unixReviewTime']].drop_duplicates()
@@ -50,7 +50,7 @@ def dataset_converter(dataset_name):
     df.drop(columns=['overall', 'unixReviewTime'], inplace=True)  # space efficiency
 
     # Creating review file
-    re_path = os.path.join('seer-ijcai2020', dataset_name, 'review.txt')
+    re_path = os.path.join(path, dataset_name, 'review.txt')
     if not os.path.isfile(re_path):
         print('Getting sentences')
         review = df.groupby(['reviewerID', 'asin'], as_index=False).agg({'sentence': '.'.join})
@@ -67,7 +67,7 @@ def dataset_converter(dataset_name):
     df.drop(columns=['sentence'], inplace=True)  # space efficiency
 
     # Creating sentiment file
-    s_path = os.path.join('seer-ijcai2020', dataset_name, 'sentiment.txt')
+    s_path = os.path.join(path, dataset_name, 'sentiment.txt')
     if not os.path.isfile(s_path):
         print('Joining aspects, opinions and sentiments')
         df['aspect:opinion:sentiment'] = df.apply(lambda x: f"{x['aspect']}:{x['opinion']}:{x['sentiment']}", axis=1)
@@ -84,7 +84,7 @@ def dataset_converter(dataset_name):
         print('Warning: sentiment.txt already exists, skipping.')
 
 
-def load_feedback(fmt="UIR", reader: Reader = None) -> List:
+def load_feedback(fpath='seer-ijcai2020/cellphone/ratings.txt', fmt="UIR", reader: Reader = None) -> List:
     """Load the user-item ratings, scale: [1,5]
 
     Parameters
@@ -97,12 +97,11 @@ def load_feedback(fmt="UIR", reader: Reader = None) -> List:
     data: array-like
         Data in the form of a list of tuples (user, item, rating).
     """
-    fpath = 'seer-ijcai2020/cellphone/ratings.txt'
     reader = Reader() if reader is None else reader
     return reader.read(fpath, fmt=fmt, sep=",")
 
 
-def load_review(reader: Reader = None) -> List:
+def load_review(fpath='seer-ijcai2020/cellphone/review.txt', reader: Reader = None) -> List:
     """Load the user-item-review list
 
     Parameters
@@ -115,12 +114,11 @@ def load_review(reader: Reader = None) -> List:
     data: array-like
         Data in the form of a list of tuples (user, item, review).
     """
-    fpath = 'seer-ijcai2020/cellphone/review.txt'
     reader = Reader() if reader is None else reader
     return reader.read(fpath, fmt="UIReview", sep="\t")
 
 
-def load_sentiment(reader: Reader = None) -> List:
+def load_sentiment(fpath='seer-ijcai2020/cellphone/sentiment.txt', reader: Reader = None) -> List:
     """Load the user-item-sentiments
     The dataset was constructed by the method described in the reference paper.
 
@@ -138,7 +136,6 @@ def load_sentiment(reader: Reader = None) -> List:
     ----------
     https://github.com/evison/Sentires
     """
-    fpath = 'seer-ijcai2020/cellphone/sentiment.txt'
     reader = Reader() if reader is None else reader
     return reader.read(fpath, fmt='UITup', sep=',', tup_sep=':')
 
