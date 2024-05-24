78de86c4fea24ef6284e56b5fe9e282025e01122
darrylong <darrylong@users.noreply.github.com>
2024-05-24 17:53:21 +0800

    Release v2.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ author = 'Preferred.AI'
 # The short X.Y version
 version = '2.2'
 # The full version, including alpha/beta/rc tags
-release = '2.2.0'
+release = '2.2.1'
 
 
 # -- General configuration ---------------------------------------------------
