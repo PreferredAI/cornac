ca580f630f067e9c843e6ea084a5c081d8112c14
tqtg <tuantq.vnu@gmail.com>
2025-04-14 21:49:12 +0000

    Fix dtype for windows build

--- cornac/models/mf/backend_cpu.pyx
+++ cornac/models/mf/backend_cpu.pyx
@@ -33,14 +33,13 @@ from tqdm.auto import trange
 def fit_sgd(integral[:] rid, integral[:] cid, floating[:] val,
             floating[:, :] U, floating[:, :] V, 
             floating[:] Bu, floating[:] Bi,
-            integral num_users, integral num_items,
             floating lr, floating reg, floating mu,
             int max_iter, int num_threads,
             bool use_bias, bool early_stop, bool verbose):
     """Fit the model parameters (U, V, Bu, Bi) with SGD"""
     cdef:
-        integral num_ratings = val.shape[0]
-        integral num_factors = U.shape[1]
+        long num_ratings = val.shape[0]
+        int num_factors = U.shape[1]
 
         floating loss = 0
         floating last_loss = 0
