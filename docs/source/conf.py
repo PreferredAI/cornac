480da8c0547a6533f868424cd77d0ce2aac97aba
qtuantruong <tuantq.vnu@gmail.com>
2026-05-27 09:27:00 -0700

    Release v2.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,9 +23,9 @@ copyright = "2023, Preferred.AI"
 author = "Preferred.AI"
 
 # The short X.Y version
-version = "2.3"
+version = "2.4"
 # The full version, including alpha/beta/rc tags
-release = "2.3.5"
+release = "2.4.0"
 
 
 # -- General configuration ---------------------------------------------------
