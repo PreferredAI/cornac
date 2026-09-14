1b7a95d3ce88ef6b6b358104dc7f91776abf4613
qtuantruong <tuantq.vnu@gmail.com>
2026-09-13 18:13:42 -0700

    chore: bump version to 3.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ author = "Preferred.AI"
 # The short X.Y version
 version = "3.0"
 # The full version, including alpha/beta/rc tags
-release = "3.0.0"
+release = "3.0.1"
 
 
 # -- General configuration ---------------------------------------------------
